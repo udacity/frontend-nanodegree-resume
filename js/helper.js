4b669209615b5d348409a9210f87e83879c20c0e
Peter Chon <peter.chon@gmail.com>
2014-10-21 00:05:25 -0700

    Update helper.js
    uncommented code to show google map

--- js/helper.js
+++ js/helper.js
@@ -87,7 +87,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.clientX, loc.clientY);
 });
 
 
@@ -235,11 +235,11 @@ Uncomment all the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window 
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
