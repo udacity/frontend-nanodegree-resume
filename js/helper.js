917f7dd4e2edbf91a41ca44ba3b3b66baea19131
cameronwp <cameron.w.pittman@gmail.com>
2014-11-29 22:03:33 -0800

    bug fixes. nicer css.

--- js/helper.js
+++ js/helper.js
@@ -64,12 +64,10 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+    var iName = inName(name) || function(){};
     $('#name').html(iName);  
   });
 })
-
-
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
@@ -231,7 +229,7 @@ function initializeMap() {
 };
 
 /*
-Uncomment all the code below when you're ready to implement a Google Map!
+Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
