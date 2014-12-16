c35022f0ee66ff3ad048bb43e83b712247d55e02
misaacs <michael.isaacs@gmail.com>
2014-12-16 14:24:19 -0800

    Added changes for initial education & work Hacked it to work with existing javascript conditional (display:none)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -63,3 +63,25 @@ for (var index=0; index< bio.skills.length; ++index)
 	$("#skills").append(HTMLnextSkill);    //adding next skill
 }
 
+// "work" object
+var work= {};
+work.job_position= "Software Architect";
+work.employer= "Hawthorne Medical Analytics";
+work.years_worked= 11;
+work.location= "Portland OR";
+
+//"education" object
+var education= {};
+education["school attended"]= "UC Santa Barbara";
+education["years in school"]= [2000,2001,2002,2003];
+education["location"]= "Santa Barbara CA";
+
+
+$("#workExperience").append(HTMLworkStart);		//new division class="work-entry"
+$(".work-entry").append(work["job_position"]);	//
+
+$("#education").append(HTMLschoolStart);    // Add a new <div> "education-entry"
+$(".education-entry").append(education["school attended"]);
+
+console.log(work);
+console.log(education);
