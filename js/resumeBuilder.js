d087fa7ed840c30b8fb0b4ab8376d1086937d25d
phonofidelic <chris_clemons85@hotmail.com>
2014-12-21 23:18:23 -0500

    Remove 'hello world' from index.html. Add var firstName, var age, var awsomeThoughts, var funThoughts.replace(), print funThoughts to #main

---
+++ js/resumeBuilder.js
@@ -0,0 +1,14 @@
+var firstName = "chris";
+
+var age = 29;
+
+console.log(firstName);
+
+var awsomeThoughts = "I am Phonofidelic and I am AWESOME!";
+
+console.log(awsomeThoughts);
+
+var funThoughts =
+	awsomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
