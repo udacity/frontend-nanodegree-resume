47930db21faf92de334ab7da1efe4826a5584b28
Juan Martin Marco <jmmarco@gmail.com>
2015-01-16 10:58:15 -0300

    Add first changes of my online resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,32 @@
+var work = {
+	"jobs": [
+	{
+		"employer": "Udacity",
+		"title": "Code Reviewer",
+		"location": "Buenos Aires, ARG",
+		"dates": "2014 - Present",
+		"description": "Evaluate and review code for Programming Foundations with Python"
+	}
+	{
+		"employer": "Avaya",
+		"title": "Backbone Engineer",
+		"location": "Buenos Aires, ARG",
+		"dates": "2010 - 2013",
+		"description": "Break and fix support for Avaya Call Manager"
+	}
+	{
+		"employer": "IBM",
+		"title": "Master Trainer",
+		"location": "Buenos Aires, ARG",
+		"dates": "2006 - 2010",
+		"description": "Teach and train program fundamentals for new hires and existing staff"
+	}
+	]
+}
+
+
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+
