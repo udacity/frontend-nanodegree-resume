f29ecfb7351e4b3d16b5760eb6b15df5c1399cf9
phonofidelic <chris_clemons85@hotmail.com>
2015-01-25 11:48:20 -0500

    Added complete resume structure

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,33 +1,239 @@
-// var firstName = "Phonofidelic";
+/*
+work object
+*/
+var work = [
+		{
+			"employer" : "Phonofidelic",
+			"title" : "Graphic designer",
+			"location" : "Stockholm, Sweden & Ft Lauderdale, FL",
+			"dates" : "2006-2015",
+			"description" : "describe"
+		},
+		{
+			"employer" : "Imperial Recordings",
+			"title" : "Graphic artist, printer",
+			"location" : "Stockholm, Sweden",
+			"dates" : "2012",
+			"description" : "describe"
+		}
+];
 
-//var name = "Phonofidelic";
+/*
+projects object
+*/
+var projects = {
+	"projects" : [
+		{
+			"title" : "This Works For Now",
+			"dates" : 2012,
+			"description" : "Graphic prints and instalation shown att Grafiska Sälskapet.",
+			"images" : ["images/197x148.gif", "images/197x148.gif"]
+		},
+		{
+			"title" : "Something",
+			"dates" : 2012,
+			"description" : "Graphic prints for live event.",
+			"images" : ["images/197x148.gif", "images/197x148.gif", "images/197x148.gif"]
+		}
+	]
+};
 
-// var role = "Design & Printing";
+/*
+bio object
+*/
+var bio = {
+	"name" : "Chris Clemons",
+	"role" : "Visual Designer and Web Developer",
+	"contact" : {
+		"email" : "info@phonofidelic.com",
+		"mobile" : "555-555-5555"
+	},
+	"welcomeMessage" : "Hello bla bla bla",
+	"skills" : ["design", "printing", "art"]
+};
 
-// var age = 29;
+/*
+education object
+*/
+var education = {
+	"schools" : [
+		{
+			"name" : "Stockholms Universitet",
+			"location" : "Stockholm, Sweden",
+			"area" : "Philosophy and linguistics",
+			"years" : "2011-2013"
+		},
+		{
+			"name" : "Grafikskolan",
+			"location" : "Stockholm, Sweden",
+			"area" : "Print making",
+			"years" : "2009-2011"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"name" : "Udacity",
+			"title" : "Front-end web development",
+			"dates" : "2014-2015",
+			"URL" : "https://www.udacity.com"
+		}
+	]
+};
 
-// console.log(firstName);
+/*
+name start
+*/
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
 
-// var awsomeThoughts = "I am Phonofidelic and I am AWESOME!";
+/*
+role start
+*/
+var formattedRole = HTMLheaderRole.replace("%data%", "Design & Printing");
+$("#header").append(formattedRole);
 
-// console.log(awsomeThoughts);
+/*
+biopic start
+*/
+var formattedBioPic = HTMLbioPic.replace("%data%", "images/me.jpg");
+$("#header").append(formattedBioPic);
 
-// var funThoughts =
-	// awsomeThoughts.replace("AWESOME", "FUN");
+/*
+contact start
+*/
+var formattedContact = HTMLemail.replace("%data%", bio.contact.email);
+$("#header").append(formattedContact);
 
-// $("#main").append(funThoughts);
+var formattedContact = HTMLmobile.replace("%data%", bio.contact.mobile);
+$("#header").append(formattedContact);
 
-// var formattedName = HTMLheaderName.replace("%data%", name);
+if (bio.skills.length > 0) {
 
-// $("#header").append(formattedName);
+	$("#header").append(HTMLskillsStart);
 
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skillsH3").append(formattedSkill);
 
-// $("#header").append(formattedRole);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skillsH3").append(formattedSkill);
 
-var skills =
-["design", "printing", "art"];
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skillsH3").append(formattedSkill);
+}
 
-// $("#main").append(skills);
+/*
+work experience start
+*/
+function displayWork() {
+	for (job in work) {
+		$("#workExperience").append(HTMLworkStart);
 
-$("#main").append(skills[0]);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+		var formattedWorkDates = HTMLworkDates.replace("%data%", work[job].dates);
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work[job].location);
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%", work[job].description);
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedWorkDescription);
+	}
+}
+
+displayWork();
+
+/*
+click tracker
+*/
+// $(document).click(function(loc) {
+// 	var x = loc.pageX;
+// 	var y = loc.pageY;
+
+// 	logClicks(x,y);
+// });
+
+/*
+international name changer (!NOT WORKING!)
+*/
+// function inName(name) {
+// 	name = name.trim().split(" ");
+// 	console.log(name);
+// 	name[1] = name[1].toUpperCase();
+// 	name[0] = name[0].slice(0,1).toUpperCase(); + name[0].slice(1).toLowercase();
+
+// 	return name[0] + " " + name[1];
+// }
+// $("#main").append(internationalizeButton);
+
+/* 
+projects start 
+*/
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+			$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+$("#projects").append(projects.display);
+
+/*
+education start
+*/
+education.display = function() {
+	for (school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		$(".education-entry:last").append(formattedSchoolName);
+
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedSchoolLocation);
+
+		var formattedSchoolArea = HTMLschoolDegree.replace("%data%", education.schools[school].area);
+		$(".education-entry:last").append(formattedSchoolArea);
+
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].years);
+		$(".education-entry:last").append(formattedSchoolDates);
+	}
+	for (course in education.onlineCourses) {
+		$("#education").append(HTMLonlineClasses);
+
+		var formattedOnlineName = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		$(".education-entry:last").append(formattedOnlineName);
+
+		var formattedOnlineName = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].name);
+		$(".education-entry:last").append(formattedOnlineName);
+
+		var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+		$(".education-entry:last").append(formattedOnlineDates);
+
+		var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].URL);
+		$(".education-entry:last").append(formattedOnlineURL);
+	}
+}
+
+$("#education").append(education.display);
+
+/*
+map start
+*/
+//$("#mapDiv").append(googleMap);
