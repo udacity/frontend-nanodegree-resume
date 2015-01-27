b316a2c19d7f22ef0c047ae2b8b30e3d63cbe5a9
Juan Martin Marco <jmmarco@gmail.com>
2015-01-26 21:14:31 -0300

    Add displayBio, needs improovement

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -26,6 +26,22 @@ function displayBio() {
 	$("#header").append(formattedName);
 	$("#header").append(formattedRole);
 	$("#header").append(formattedPicture);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedMobile);
+	$("#footerContacts").append(formattedMobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").append(formattedEmail);
+	$("#footerContacts").append(formattedEmail);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").append(formattedGithub);
+	$("#footerContacts").append(formattedGithub);
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#topContacts").append(formattedTwitter);
+	$("#footerContacts").append(formattedTwitter);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").append(formattedLocation);
+	$("#footerContacts").append(formattedLocation);
+
 }
 displayBio();
 
