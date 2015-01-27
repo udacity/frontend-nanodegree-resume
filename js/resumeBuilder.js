171a90dab0715c11037adaf0b02e8851c159be9e
spaetsch <spaetsch@gmail.com>
2015-01-27 11:11:51 -0800

    Start final project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -70,158 +70,6 @@ $("#education").append(formattedschoolName);
 
 */
 
-//--------------------
-
-
-var bio = {
-	"name" : "Sarah Paetsch",
-	"role" : "Web Developer",
-	"contacts" : {
-		"mobile" : "123-456-7890",
-		"email" : "foo@example.com",
-		"twitter" : "@twitterName" ,
-		"github" : "githubName",
-		"location" : "Seattle"
-	},
-
-	"picURL" : "images/userpic1.jpg",
-	"welcomeMsg" : "Welcome to my awesome website",
-	"skills" : [
-		"skill1 " , "skill2 ", "skill3 "
-	]
-};
-
-var education = {
-	"schools" : [
-	{
-		"name":"Univerity of Texas at Austin",
-		"city": "Austin, TX",
-		"major" : "English",
-		"gradyear" : "2000"
-	},
-	{
-	"name":"The Ohio State Univerity",
-	"city": "Columbus, OH",
-	"major" : "English",
-	"minor" : "Computer Science",
-	"gradyear" : "1998"
-	}
-	],
-
-	"onlineclasses" : [
-	{
-	"courseTitle" : "Javascript Basics",
-	"school" : "Udacity",
-	"dates" : "January 2015",
-	"siteURL" : "www.udacity.com"
-	},
-	{
-	"courseTitle" : "Class 2",
-	"school" : "moo",
-	"dates" : "December 2014",
-	"siteURL" : "www.somewhere.com"
-	}
-]
-};
-
-
-var workList = {
-	"jobs" : [
-	{	
-	"employer" : "Devilbunny Dyeworks (self)",
-	"position" : "Indie Yarn Dyer",
-	"dates" : "2012 - present",
-	"location" : "Campbell, CA",
-	"description" : "Dolcelatte emmental cheese triangles. Cottage cheese lancashire cheesy grin roquefort fromage pepper jack cheese slices fromage."
-	},
-	{	
-	"employer" : "National Instruments",
-	"position" : "Technical Writer / Course Developer",
-	"dates" : "2007-2009",
-	"location" : "Austin, TX",
-	"description" : "Dolcelatte emmental cheese triangles. Cottage cheese lancashire cheesy grin roquefort fromage pepper jack cheese slices fromage."
-	}
-	]
-};
-
-var projectList = {
-	"projects" : [
-	{
-	"projectTitle" : "Sample Project 1",
-	"projectDates" : "2014",
-	"projectDesc" : "I love cheese, especially port-salut macaroni cheese. Cheesecake everyone loves red leicester gouda jarlsberg gouda the big cheese. Fondue halloumi bocconcini babybel bocconcini the big cheese cheeseburger.",
-	"projectImages" : [
-		"http://www.fillmurray.com/200/300", "http://www.fillmurray.com/200/300"
-	]
-	},
-	{
-	"projectTitle" : "Sample Project 2",
-	"projectDates" : "2015",
-	"projectDesc" : "I love cheese, especially port-salut macaroni cheese. Cheesecake everyone loves red leicester gouda jarlsberg gouda the big cheese. Fondue halloumi bocconcini babybel bocconcini the big cheese cheeseburger.",
-	"projectImages" : [
-		"http://www.fillmurray.com/250/250", "http://www.fillmurray.com/250/250", "http://www.fillmurray.com/250/250"
-	]
-	}
-	]
-};
-
-
-var name = bio.name;
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var role = bio.role;
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var formattedPic = HTMLbioPic.replace("%data%", bio.picURL);
-$("#header").append(formattedPic);
-
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-	$("#skills").append(formattedSkill);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-	$("#skills").append(formattedSkill);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-	$("#skills").append(formattedSkill);
-}
-
-
-
-function displayWork(){
-	for (job in workList.jobs){
-		var formattedjobTitle = HTMLworkTitle.replace("%data%", workList.jobs[job].position);
-		var formattedjobEmployer = HTMLworkEmployer.replace("%data%", workList.jobs[job].employer);
-
-		var formattedDates = HTMLworkDates.replace("%data%", workList.jobs[job].dates);
-		var formattedLocation = HTMLworkLocation.replace("%data%", workList.jobs[job].location);
-		var formattedDesc = HTMLworkDescription.replace("%data%", workList.jobs[job].description);
-
-		$("#workExperience").append(HTMLworkStart);
-		$(".work-entry:last").append(formattedjobEmployer + formattedjobTitle);		
-		$(".work-entry:last").append(formattedDates);
-		$(".work-entry:last").append(formattedLocation);
-		$(".work-entry:last").append(formattedDesc);
-
-	}
-}
-
-displayWork();
-
-$(document).click(function(loc){
-
-	var x = loc.pageX;
-	var y = loc.pageY;
-
-	logClicks(x,y);
-
-});
-
 /* internationalize button code
 function inName(name){
 	console.log("here");
@@ -234,41 +82,245 @@ function inName(name){
 
 $("#main").append(internationalizeButton);
 */
-console.log("here");
+//console.log("here");
 
-		console.log(projectList.projects[1].projectTitle);
+		//console.log(projectList.projects[1].projectTitle);
 
+
+//--------------------
+
+
+var bio = {
+	"name" : "Sarah Paetsch",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "512-293-7337",
+		"email" : "spaetsch@gmail.com",
+		"twitter" : "@twitterName" ,
+		"github" : "spaetsch",
+		"location" : "Seattle, WA"
+	},
+	"welcomeMessage" : "Welcome to my awesome website.",
+	"skills" : [
+		"Javascript" , "jQuery", "HTML", "CSS"
+	],
+	"biopic" : "images/userpic1.jpg",
+};
+
+bio.display = function(){
+
+	var name = bio.name;
+	var formattedName = HTMLheaderName.replace("%data%", name);
+
+	var role = bio.role;
+	var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+
+	var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+	$("#header").append(formattedPic);
+
+	var welcomeMsg = bio.welcomeMessage;
+	var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", welcomeMsg);
+	$("#header").append(formattedWelcomeMsg);
+
+	var mobile = bio.contacts.mobile;
+	var formattedMobile = HTMLmobile.replace("%data%", mobile);
+	$("#topContacts").append(formattedMobile);
+
+	var twitter = bio.contacts.twitter;
+	var formattedTwitter = HTMLtwitter.replace("%data%", twitter);
+	$("#topContacts").append(formattedTwitter);
+
+	var email = bio.contacts.email;
+	var formattedEmail = HTMLemail.replace("%data%", email);
+	$("#topContacts").append(formattedEmail);
+
+	var github = bio.contacts.github;
+	var formattedGithub = HTMLgithub.replace("%data%", github);
+	$("#topContacts").append(formattedGithub);
+
+	var location = bio.contacts.location;
+	var formattedLocation = HTMLlocation.replace("%data%", location);
+	$("#topContacts").append(formattedLocation);
+
+	$("#header").append(HTMLskillsStart);
+
+	//iterates through skill list to display all of them
+	for (skill in bio.skills){
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);	
+		$("#skills").append(formattedSkill);
+	}
+}
+
+
+var education = {
+	"schools" : [
+	{
+		"name":"University of Texas at Austin",
+		"location": "Austin, TX",
+		"degree" : "M.A.",
+		"majors" : [
+			"English with Computers and Literature concentration", "other major"
+			],
+		"dates" : "2000",
+		"url" : "http://www.utexas.edu/"
+	},
+	{
+		"name":"The Ohio State Univerity",
+		"location" : "Columbus, OH",
+		"degree" : "B.A.",
+		"majors" : [
+			"English", "minor in Computer Science"
+			],
+		"dates" : "1998",
+		"url" : "http://www.osu.edu/"
+	}
+	],
+
+	"onlineclasses" : [
+	{
+		"title" : "Javascript Basics",
+		"school" : "Udacity",
+		"date" : "January 2015",
+		"url" : "https://www.udacity.com/"
+	},
+	{
+		"title" : "Intro to jQuery",
+		"school" : "Udacity",
+		"date" : "February 2015",
+		"url" : "https://www.udacity.com/"
+	}
+	]
+};
+
+education.display = function(){
+	for (school in education.schools){
+
+		var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+
+		$("#education").append(HTMLschoolStart);
+
+		$(".education-entry:last").append(formattedschoolName + formattedschoolDegree);
+		$(".education-entry:last").append(formattedschoolDates);
+		$(".education-entry:last").append(formattedschoolLocation);
+		$(".education-entry:last").append(formattedschoolMajor);
+	}
+}
+
+var work = {
+	"jobs" : [
+	{	
+		"employer" : "Devilbunny Dyeworks",
+		"title" : "Sole Proprietor / Independent Yarn Dyer",
+		"location" : "Campbell, CA",
+		"dates" : "2012 - present",
+		"description" : "Dolcelatte emmental cheese triangles. Cottage cheese lancashire cheesy grin roquefort fromage pepper jack cheese slices fromage."
+	},
+	{	
+		"employer" : "National Instruments",
+		"title" : "Technical Writer / Course Developer",
+		"location" : "Austin, TX",
+		"dates" : "2007-2009",
+		"description" : "Dolcelatte emmental cheese triangles. Cottage cheese lancashire cheesy grin roquefort fromage pepper jack cheese slices fromage."
+	}, 
+	{	
+		"employer" : "Enspire Learning",
+		"title" : "Content Developer",
+		"location" : "Austin, TX",
+		"dates" : "2005-2006",
+		"description" : "Dolcelatte emmental cheese triangles. Cottage cheese lancashire cheesy grin roquefort fromage pepper jack cheese slices fromage."
+	}
+	]
+};
+
+work.display = function(){
+	for (job in work.jobs){
+		var formattedjobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedjobEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDesc = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		$("#workExperience").append(HTMLworkStart);
+		$(".work-entry:last").append(formattedjobEmployer + formattedjobTitle);		
+		$(".work-entry:last").append(formattedDates);
+		$(".work-entry:last").append(formattedLocation);
+		$(".work-entry:last").append(formattedDesc);
+
+	}
+}
+
+var projects = {
+	"projects" : [
+	{
+	"title" : "Sample Project 1",
+	"dates" : "2014",
+	"description" : "I love cheese, especially port-salut macaroni cheese. Cheesecake everyone loves red leicester gouda jarlsberg gouda the big cheese. Fondue halloumi bocconcini babybel bocconcini the big cheese cheeseburger.",
+	"images" : [
+		"http://www.fillmurray.com/200/300", "http://www.fillmurray.com/200/300"
+	]
+	},
+	{
+	"title" : "Sample Project 2",
+	"dates" : "2015",
+	"description" : "I love cheese, especially port-salut macaroni cheese. Cheesecake everyone loves red leicester gouda jarlsberg gouda the big cheese. Fondue halloumi bocconcini babybel bocconcini the big cheese cheeseburger.",
+	"images" : [
+		"http://www.fillmurray.com/250/250", "http://www.fillmurray.com/250/250", "http://www.fillmurray.com/250/250"
+	]
+	}
+	]
+};
 
 projects.display = function(){
-	for (project in projectList.projects) {
+	for (project in projects.projects) {
 
-		console.log("here2");
-
-		console.log(projectList.projects[1].projectTitle);
 		$("#projects").append(HTMLprojectStart);
 
-		console.log(projectList.projects[project].projectTitle);
-
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projectList.projects[project].projectTitle);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
 		$(".project-entry:last").append(formattedTitle);
 
-		var formattedDates = HTMLprojectDates.replace("%data%", projectList.projects[project].projectDates);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
 		$(".project-entry:last").append(formattedDates);
 
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projectList.projects[project].projectDesc);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
 		$(".project-entry:last").append(formattedDescription);
 
-		if (projectList.projects[project].projectImages.length > 0){
-			for (image in projectList.projects[project].projectImages) {
-				var formattedImage = HTMLprojectImage.replace("%data%", projectList.projects[project].projectImages[image]);
+		if (projects.projects[project].images.length > 0){
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
 				$(".project-entry:last").append(formattedImage);
 			}
 		}
 	}
 }
 
+
+
+bio.display();
+
+work.display();
+
 projects.display();
 
+education.display();
+
+$(document).click(function(loc){
+
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+	logClicks(x,y);
+
+});
+
+console.log("map stuff");
 $("#mapDiv").append(googleMap);
 
 
