a0f7fd1dee13ed79ed776df032d29b4b3d8c6d23
phonofidelic <chris_clemons85@hotmail.com>
2015-02-04 03:29:28 -0500

    Changed class name 'white-text' to 'contactLink-text'.

--- js/helper.js
+++ js/helper.js
@@ -15,19 +15,19 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="black-text">%contact%</span><span class="contactLink-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="black-text">mobile</span><span class="contactLink-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="black-text">email</span><span class="contactLink-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="black-text">twitter</span><span class="contactLink-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="black-text">github</span><span class="contactLink-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="black-text">blog</span><span class="contactLink-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="black-text">location</span><span class="contactLink-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="contactLink-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
