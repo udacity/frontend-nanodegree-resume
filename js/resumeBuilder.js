68b65695aeeb5542b8dcf883ddf48861c1e04a70
Daniel Stuart <dan@bidvine.co.uk>
2015-03-08 10:52:25 -0400

    conflict change

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,7 @@ var bio = {
     "location": "Guelph, Ontario, Canada"
   },
   "welcomeMessage": "This is a little bit about me. I am always learning and this is part of building up skills in parallel to my design and product planning background. My goal here is to go a bit deeper into the stack.",
-  "skills": ["design", "HTML", "Sketch", "SASS"],
+  "skills": ["Design", "HTML", "Sketch", "SASS"],
   "biopic": "images/mypic.jpg",
 
 	"displayHeader": function() {
