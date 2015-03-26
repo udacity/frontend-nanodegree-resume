5f2587e37d1fff6a2250009b3e99bd617d4324df
Jason Goldfarb <yosegold@gmail.com>
2015-03-25 23:14:03 -0400

    commented out formattedName and formattedRole. Added bio object and .append  to display values but not working properly

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-var name = "Jason Goldfarb";
+/*var name = "Jason Goldfarb";
 var formattedName =
     HTMLheaderName.replace("%data%", name);
 
@@ -8,3 +8,31 @@ var formattedRole =
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+*/
+
+var skills =
+  ["awesomeness", "programming", "teaching", "JS" ];
+
+//$("#main").append(skills.length);
+
+var bio = {
+  "name": "Jason",
+  "role": "Web Developer",
+  "contacts": {
+     "mobile": "(917) 207-1982",
+     "email": "jason.r.goldfarb@gmail.com",
+     "gitHub": "jgoldfarb",
+   },
+  "welcomeMessage": "This is my online resume",
+  "skills": ["awesomeness", "programming", "teaching", "JS"],
+  "bioPic": "images/headshot.jpg"
+};
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contacts.mobile);
+$("#main").append(bio.contacts.email);
+$("#main").append(bio.contacts.gitHub);
+$("#main").append(bio.welcomeMessage);
+$("#main").append(bio.skills);
+$("#main").append(bio.bioPic);
