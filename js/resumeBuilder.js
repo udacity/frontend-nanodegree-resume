e2c6203675470967c64fff45998aa2e239cdb6f9
Jason Goldfarb <yosegold@gmail.com>
2015-03-26 23:32:52 -0400

    cleaned up bio object and added formatting for bio info to appear on the page. skills at a glance heading not appearing. skills not appearing in top to bottom order. let's connect appears on page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,38 +1,61 @@
-/*var name = "Jason Goldfarb";
-var formattedName =
-    HTMLheaderName.replace("%data%", name);
+//var name = "Jason Goldfarb";
+//var formattedName =
+  //  HTMLheaderName.replace("%data%", name);
 
-var role = "Web Developer";
-var formattedRole =
-    HTMLheaderRole.replace("%data%", role);
+//var role = "Web Developer";
+//var formattedRole =
+//    HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-*/
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
-var skills =
-  ["awesomeness", "programming", "teaching", "JS" ];
+
+//var skills =
+//  ["awesomeness", "programming", "teaching", "JS" ];
 
 //$("#main").append(skills.length);
 
 var bio = {
-  "name": "Jason",
+  "name": "Jason Goldfarb",
   "role": "Web Developer",
   "contacts": {
      "mobile": "(917) 207-1982",
      "email": "jason.r.goldfarb@gmail.com",
-     "gitHub": "jgoldfarb",
+     "github": "jgoldfarb",
    },
-  "welcomeMessage": "This is my online resume",
+  "welcomeMessage": "This is my Front-End Web Development resume",
   "skills": ["awesomeness", "programming", "teaching", "JS"],
   "bioPic": "images/headshot.jpg"
 };
 
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contacts.mobile);
-$("#main").append(bio.contacts.email);
-$("#main").append(bio.contacts.gitHub);
-$("#main").append(bio.welcomeMessage);
-$("#main").append(bio.skills);
-$("#main").append(bio.bioPic);
+    var formattedRole =
+        HTMLheaderRole.replace("%data%", bio.role);
+        $("#header").prepend(formattedRole);
+
+    var formattedName =
+        HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(formattedName);
+
+    var formattedMobile =
+        HTMLmobile.replace("%data%", bio.contacts.mobile);
+        $("#topContacts").append(formattedMobile);
+
+    var formattedEmail =
+        HTMLemail.replace("%data%", bio.contacts.email);
+        $("#topContacts").append(formattedEmail);
+
+    var formattedGithub =
+        HTMLgithub.replace("%data%", bio.contacts.github);
+        $("#topContacts").append(formattedGithub);
+
+    var formattedPic =
+        HTMLbioPic.replace("%data%", bio.bioPic);
+        $("#header").append(formattedPic);
+
+    var formattedWelcomeMessage =
+        HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        $("#header").append(formattedWelcomeMessage);
+
+    var formattedSkills =
+        HTMLskills.replace("%data%", bio.skills);
+        $("#header").append(formattedSkills);
