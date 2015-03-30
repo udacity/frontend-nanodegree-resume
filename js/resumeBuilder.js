45278bc9ee6715b7c878f99c689ca4a666efffa8
Jason Goldfarb <yosegold@gmail.com>
2015-03-29 22:40:14 -0400

    added work and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,6 +13,41 @@ var bio = {
   "bioPic": "images/headshot.jpg"
 };
 
+var work = {
+  "jobs": [
+    {
+    "employer": "AT&T",
+    "title": "Senior Real Estate and Construction Manager - LTE Project Manager",
+    "location": "New York City Metropolitan Area",
+    "dates": "October 2011 - Current",
+    "description": "Completed 100% of the 2014 LTE upgrade projects for 800+ locations, ahead of time, and within budget. Lead weekly project development and deployment meetings with multiple site acquisition and civil project vendors.	Provide guidance for complicated, legal, technical, business, and contractual issues that impact project timelines. Create market-specific master trackers, and provide in depth reporting of all milestone progress to senior level executive teams. Responsible for maintaining project schedules and for hitting and exceeding goals linked to planned, forecasted, and completion objectives. On board new vendor teams, and liaison with all internal and external groups accountable for LTE deployment in the market.",
+    }
+      ],
+  };
+
+
+var education = {
+  "schools": [
+	   {
+		  "name": "Brooklyn Law School",
+	    "location": "Brooklyn, New York",
+	    "degree" : "Juris Doctor",
+	    "majors": "Law",
+	    "dates": 1994,
+	    "url": "http://www.brooklaw.edu/",
+	   }
+      ],
+
+  "onlineCourses": [
+	   {
+		"title": "Front End Web Developer",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "http://www.udacity.com"
+	   }
+   ],
+};
+
     var formattedRole =
         HTMLheaderRole.replace("%data%", bio.role);
         $("#header").prepend(formattedRole);
@@ -39,7 +74,7 @@ var bio = {
 
     var formattedLocation =
         HTMLlocation.replace("%data%", bio.contacts.location);
-        $("#topContacts").append(formattedLocation);        
+        $("#topContacts").append(formattedLocation);
 
     var formattedPic =
         HTMLbioPic.replace("%data%", bio.bioPic);
