acaa3154e70a74b7e56f463de16e4e6bddb346b7
Jason Goldfarb <yosegold@gmail.com>
2015-03-30 21:43:46 -0400

    add dates, location, description, for most recent job. all appears properly.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -71,10 +71,27 @@ var bio = {
       work.display = function () {
         for (var job in work.jobs) {
             $("#workExperience").append(HTMLworkStart);
-            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+            var formattedEmployer =
+            HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+            var formattedTitle =
+            HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
             var formattedEmployerTitle = formattedEmployer + formattedTitle;
             $(".work-entry:last").append(formattedEmployerTitle);
+
+            var formattedDates =
+            HTMLworkDates.replace("%data%", work.jobs[job].dates);
+            $(".work-entry:last").append(formattedDates);
+
+            var formattedLocation =
+            HTMLworkLocation.replace("%data%", work.jobs[job].location);
+            $(".work-entry:last").append(formattedLocation);
+
+            var formattedDescription =
+            HTMLworkDescription.replace("%data%", work.jobs[job].description);
+            $(".work-entry:last").append(formattedDescription);
       }
     };
     work.display();
