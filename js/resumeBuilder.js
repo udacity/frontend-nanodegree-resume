95be65597af79bf023d3391038ad847004fdbc87
Jan Dahl-Madsen <jandahl-madsen@Jans-MacBook-Air.local>
2015-04-09 14:53:33 +0545

    update education
    update education area and optimized bio area

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,21 +5,21 @@ var work = {
     "title": "Technician",
     "location": "Herlev",
     "year": "2010",
-    "description": "Cash register software with warehouse inclusion"
+    "description": "Cash register software with warehouse inclusion Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae libero nisi, culpa illum a dolorum iste quaerat atque aspernatur rem, sapiente. Vitae fuga ipsa sunt cumque adipisci quibusdam et, impedit!"
   },
   {
     "employer": "Aadalens Privatskole",
     "title": "IT Ninja",
     "location": "Ishøj",
     "year": "2012",
-    "description": "Anything remotely technical, i was responsible"
+    "description": "Anything remotely technical, i was responsible Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quis reiciendis itaque officia recusandae quisquam possimus totam nisi, voluptate eligendi voluptates, autem aspernatur soluta id aperiam saepe, rem, reprehenderit architecto."
   },
   {
     "employer": "Self Employed",
     "title": "Web Developer",
     "location": "Nepal",
     "year": "2015",
-    "description": "Starting out as front-end developer"
+    "description": "Starting out as front-end developer Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus provident sed quas, voluptate voluptas quis atque officiis? Ex quis illo quam eveniet dolore dolores eligendi, hic labore, necessitatibus, porro voluptas?"
   }
   ]
 };
@@ -28,16 +28,23 @@ var projects = {
   {
     "title": "High rise building in Gibraltar",
     "year": "2006",
-    "images":["http://placehold.it/100x100", "http://placehold.it/100x100", "http://placehold.it/100x100"]
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem optio doloribus blanditiis ea asperiores voluptatum repellat labore placeat, aliquid libero! orem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem optio doloribus blanditiis ea asperiores voluptatum repellat labore placeat, aliquid libero!",
+    "images":["http://placehold.it/150x100", "http://placehold.it/150x100", "http://placehold.it/150x100"]
+  },
+  {
+    "title": "TestProject",
+    "year": "2008",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem optio doloribus blanditiis ea asperiores voluptatum repellat labore placeat, aliquid libero! orem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem optio doloribus blanditiis ea asperiores voluptatum repellat labore placeat, aliquid libero!",
+    "images":["http://placehold.it/150x100", "http://placehold.it/150x100", "http://placehold.it/150x100"]
   }]
 };
 var bio = {
     "name": "Jan Dahl-Madsen",
     "role": "Web Developer",
-    "welcomeMessage": "Welcome to my story",
+    "welcomeMessage": "Welcome to my story Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, illum!",
     "contacts": {"mobile": "+977 9813018651", "email": "161076@gmail.com", "github": "jsdm", "twitter": "@janmadsen", "location": "Kathmandu"},
-    "skills": ["HTML5", "JS", "CSS", "Design"],
-    "biopic": "images/me.png"
+    "skills": ["HTML5", "Javascript", "CSS", "Design"],
+    "biopic": "images/fry.jpg"
   };
 var education = {
   "schools": [
@@ -46,31 +53,30 @@ var education = {
   "location": "Copenhagen N",
   "degree": "Building Technician",
   "year": "2000",
-  "url": "www.bth.dk"
+  "major": "Buildings"
   },
   {
     "name": "TEC copenhagen",
     "location": "Frederiksberg",
     "degree": "IT Engineer",
     "year": "2008",
-    "url": "www.tec.dk"
+    "major": "IT"
   }
-  ]};
-  // var "onlineCourses" = {
-  //   "onlineCourse": [
-  //   {
-  //     "title": "HTML & CSS",
-  //     "school": "codecademy",
-  //     "Year": "2014",
-  //     "url": "www.codecademy.com"
-  //   },
-  //   {
-  //     "title": "Front-end Web Development nano degree",
-  //     "school": "Udacity",
-  //     "Year": "2015",
-  //     "url": "www.udacity.com"
-  //   }]
-  // }
+  ],
+    "onlineCourse": [
+    {
+      "title": "HTML & CSS",
+      "school": "codecademy",
+      "year": "2014",
+      "url": "www.codecademy.com"
+    },
+    {
+      "title": "Front-end Web Development nano degree",
+      "school": "Udacity",
+      "year": "2015",
+      "url": "www.udacity.com"
+    }]
+};
 var workArray = [];
 function locationizer(work) {
     for (var loc in work.jobs){
@@ -101,16 +107,25 @@ function displayEducation(){
   for (var edu in education.schools) {
     $("#education").append(HTMLschoolStart);
     var formattedName = HTMLschoolName.replace("%data%", education.schools[edu].name);
-    var formattedCity = HTMLschoolLocation.replace("%data%", education.schools[edu].city);
+    var formattedCity = HTMLschoolLocation.replace("%data%", education.schools[edu].location);
     var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[edu].degree);
     var formattedYear = HTMLschoolDates.replace("%data%", education.schools[edu].year);
-    var formattedURL = HTMLschoolMajor.replace("%data%", education.schools[edu].url);
-    $(".education-entry:last").append(formattedName);
+    var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[edu].major);
+    $(".education-entry:last").append(formattedName + formattedDegree);
     $(".education-entry:last").append(formattedCity);
-    $(".education-entry:last").append(formattedDegree);
     $(".education-entry:last").append(formattedYear);
-    $(".education-entry:last").append(formattedURL);
+    $(".education-entry:last").append(formattedMajor);
 
+  }
+  $(".education-entry:last").append(HTMLonlineClasses);
+  for (var onl in education.onlineCourse) {
+    var onlTitle = HTMLonlineTitle.replace("%data%", education.onlineCourse[onl].title);
+    var onlSchool = HTMLonlineSchool.replace("%data%", education.onlineCourse[onl].school);
+    var onlYear = HTMLonlineDates.replace("%data%", education.onlineCourse[onl].year);
+    var onlUrl = HTMLonlineURL.replace("%data%", education.onlineCourse[onl].url);
+    $(".education-entry:last").append(onlTitle + onlSchool);
+    $(".education-entry:last").append(onlYear);
+    $(".education-entry:last").append(onlUrl);
   }
 }
 displayEducation();
@@ -140,26 +155,16 @@ projects.display();
 
 $("#mapDiv").append(googleMap);
 
-var formattedName = bio.name;
-var formattedRole = bio.role;
-formattedMobile = bio.contacts.mobile;
-formattedEmail = bio.contacts.email;
-formattedTwitter = bio.contacts.twitter;
-formattedGithub = bio.contacts.github;
-formattedLocation = bio.contacts.location;
-formattedBioPic = bio.biopic;
-formattedWlcmMsg = bio.welcomeMessage;
 formattedSkills = bio.skills;
-formattedName = HTMLheaderName.replace("%data%", formattedName);
-formattedRole = HTMLheaderRole.replace("%data%", formattedRole);
-formattedMobile = HTMLmobile.replace("%data%", formattedMobile);
-formattedEmail = HTMLemail.replace("%data%", formattedEmail);
-formattedTwitter = HTMLtwitter.replace("%data%", formattedTwitter);
-formattedGithub = HTMLgithub.replace("%data%", formattedGithub);
-formattedLocation = HTMLlocation.replace("%data%", formattedLocation);
-formattedBioPic = HTMLbioPic.replace("%data%", formattedBioPic);
-formattedWlcmMsg = HTMLWelcomeMsg.replace("%data%", formattedWlcmMsg);
-formattedSkills = HTMLskills.replace("%data%", formattedSkills);
+formattedName = HTMLheaderName.replace("%data%", bio.name);
+formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+formattedWlcmMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#topContacts").append(formattedMobile);
@@ -167,9 +172,13 @@ $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedTwitter);
 $("#topContacts").append(formattedGithub);
 $("#topContacts").append(formattedLocation);
-$("#topContacts").append(formattedBioPic);
-$("#topContacts").append(formattedWlcmMsg);
-$("#topContacts").append(formattedSkills);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWlcmMsg);
+$("#header").append(HTMLskillsStart);
+for(var skl in bio.skills){
+  formattedSkills = HTMLskills.replace("%data%", bio.skills[skl]);
+  $("#header").append(formattedSkills);
+}
 
 var interName = bio.name;
 function inName(interName){
