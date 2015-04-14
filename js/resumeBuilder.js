b6928674e5bd680f39653a188614f683261a0275
Shy717Girl <Shy717Girl@users.noreply.github.com>
2015-04-14 15:46:28 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,9 @@
-var awesomeThoughts = "I am sanora and I am AWESOME";
-console.log(awesomeThoughts);
+//var awesomeThoughts = "I am sanora and I am AWESOME";
+//console.log(awesomeThoughts);
+var email = "sanora.griffin@udacity.com";
+var newEmail = 
+  email.replace{"udacity", "gmail");
+  
+console.log(email);
+console.log(newEmail);
+
