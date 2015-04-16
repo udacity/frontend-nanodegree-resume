6346be69bdfb96b2ab7a3522ff8747f446b02e75
sebastianf <sebo_024@yahoo.com>
2015-04-16 23:18:29 +0200

    Finished the heading

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,7 @@
 formattedName = "Sebastian Farago";
 formattedRole = "Front End Web Developer";
+
+HTMLheaderName = HTMLheaderName.replace("%data%",formattedName);
+$("#header").append(HTMLheaderName);
+HTMLheaderRole = HTMLheaderRole.replace("%data%",formattedRole);
+$("#header").append(HTMLheaderRole);
