cad7acc42849717d0a1058fe445b9d94b91a4f6a
sebastianf <sebo_024@yahoo.com>
2015-04-23 10:20:38 +0200

    new start

--- js/resumeBuilder.js
+++
@@ -1,7 +0,0 @@
-formattedName = "Sebastian Farago";
-formattedRole = "Front End Web Developer";
-
-HTMLheaderName = HTMLheaderName.replace("%data%",formattedName);
-$("#header").append(HTMLheaderName);
-HTMLheaderRole = HTMLheaderRole.replace("%data%",formattedRole);
-$("#header").append(HTMLheaderRole);
