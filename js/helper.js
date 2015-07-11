34f3f2bdb9a92023500f60e4770807f4dedb23cf
ToniRib <tonimarierib@gmail.com>
2015-07-10 20:29:08 -0600

    feat: Add project display method
    The 'display' method was added to the project object. This displays
    each one of the projects on the page, including images, when called.

--- js/helper.js
+++ js/helper.js
@@ -60,7 +60,9 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+The International Name challenge in Lesson 2 where you'll create a
+function that will need this helper code to run. Don't delete!
+It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
   $('button').click(function() {
@@ -86,6 +88,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -67,7 +67,7 @@ var work = {
 			dates: 'October 2011 - June 2013',
 			location: 'El Segundo, CA',
 			description: 'Ensure quality of the satellite by performing' +
-			'inspections and ensuring all processes are followed.'
+			' inspections and ensuring all processes are followed.'
 		}
 	]
 };
@@ -80,43 +80,90 @@ var project = {
 			dates: 'June 2015 - July 2015',
 			description: 'Online responsive portfolio created for' +
 			' Udacity\'s Front End Web Developer Nanodegree.',
-			images:['images/portfolio_small.jpg',
-			'images/portfolio_large.jpg']
+			images:['images/resize_portfolio_large_500.png']
 		},
 		{
 			title: 'Insomniacs Waking Dream Blog',
 			dates: 'July 2015 - present',
 			description: 'Personal online blog about life and' +
 			' changing careers.',
-			images: 'images/blog.jpg'
+			images: ['images/resize_blog.png']
 		}
 	]
 };
 
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
 /* Check whether there are skills in the bio object and display skills
  if there are */
 if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
+	$('#header').append(HTMLskillsStart);
 
-	for (i = 0; i < bio.skills.length; i++) {
-		var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-		$("#skills").append(formattedSkill);
+	for (var i = 0; i < bio.skills.length; i++) {
+		var formattedSkill = HTMLskills.replace('%data%', bio.skills[i]);
+		$('#skills').append(formattedSkill);
 	}
 }
 
 // Loop through each job in the work object, format, and append to page
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
-	var formattedJobEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	$(".work-entry:last").append(formattedJobEmployer + formattedJobTitle);
+function displayWork() {
+	for (var job in work.jobs) {
+		$('#workExperience').append(HTMLworkStart);
+		var formattedJobEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+		var formattedJobTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+		$('.work-entry:last').append(formattedJobEmployer + formattedJobTitle);
 
-	var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDatesWorked);
+		var formattedDatesWorked = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+		$('.work-entry:last').append(formattedDatesWorked);
 
-	var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-	$(".work-entry:last").append(formattedWorkLocation);
+		var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
+		$('.work-entry:last').append(formattedWorkLocation);
 
-	var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	$(".work-entry:last").append(formattedWorkDescription);
+		var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+		$('.work-entry:last').append(formattedWorkDescription);
+	}
 }
+
+// Call the displayWork function to add previous jobs to the page
+displayWork();
+
+// locationizer function
+function locationizer(work_obj) {
+	var locationArray = [];
+
+	for (var job in work_obj.jobs) {
+		locationArray.push(work_obj.jobs[job].location);
+	}
+
+	return locationArray;
+}
+
+// Add the project display method
+project.display = function() {
+	for (var entry in project.projects) {
+		$('#projects').append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace('%data%', project.projects[entry].title);
+		$('.project-entry:last').append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace('%data%', project.projects[entry].dates);
+		$('.project-entry:last').append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace('%data%', project.projects[entry].description);
+		$('.project-entry:last').append(formattedDescription);
+
+		if (project.projects[entry].images.length > 0) {
+			for (var image in project.projects[entry].images) {
+				var formattedImage = HTMLprojectImage.replace('%data%', project.projects[entry].images[image]);
+				$('.project-entry:last').append(formattedImage);
+			}
+
+		}
+	}
+}
+
+// call the project display method
+project.display();
