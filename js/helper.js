1a2bbf8cf8f20c77c15cbefeff7620efe9ed4b29
ToniRib <tonimarierib@gmail.com>
2015-07-13 12:21:10 -0600

    feat: Remove international name code from helper.js
    Removed the section of code to 'internationalize' the bio.name. This
    code was for the javascript class only, but isn't necessary for the
    final resume project.

--- js/helper.js
+++ js/helper.js
@@ -59,19 +59,6 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
-
-/*
-The International Name challenge in Lesson 2 where you'll create a
-function that will need this helper code to run. Don't delete!
-It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);
-  });
-});
-
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
 */
