6110ca492dd335c7bd9d1767e29e81204db2aa58
Jinwook Lee <jinwook.interchange@gmail.com>
2015-07-24 11:56:43 -0700

    ResumeJob
    Added Work experience (may need to modify more)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,19 +23,24 @@ var education =	{
 }
 
 var work = {
-	"Co-op": [
+	"Coop": [
 		{
-			"Name" : "Suncor",
+			"Name" : "Suncor Energy",
 			"Date" : "May 2014 - Sep 2014",
-			"title" : "Process Safety Information Specialist Co-op Student",
-			"City" : "Calgary, AB"
+			"Title" : "Process Safety Information Specialist Co-op Student",
+			"City" : "Calgary, AB",
+			"Description": "Validated hundreds of engineering tags from Major Projects for Asset Data Life-Cycle Project"
+
 		},
+
 		{
-			"Name" : "Suncor",
+			"Name" : "Suncor Energy",
 			"Date" : "Aug 2012 - Dec 2013",
-			"title" : "Engineering Co-op Student",
-			"City" : "Calgary, AB"
+			"Title" : "Engineering Co-op Student",
+			"City" : "Calgary, AB",
+			"Description": "Facilitated the successful closeout of multiple turned over projects"
 		}
+		
 	]
 }
 
@@ -72,6 +77,45 @@ var formattedRole = HTMLheaderRole.replace("%data%", bio.me[0].role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+if (bio.me[0].skills !== 0)	{
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkills = HTMLskills.replace("%data%", bio.me[0].skills[0]);
+	$("#skills").append(formattedSkills);
+
+	formattedSkills = HTMLskills.replace("%data%", bio.me[0].skills[1]);
+	$("#skills").append(formattedSkills);
+
+	formattedSkills = HTMLskills.replace("%data%", bio.me[0].skills[2]);
+	$("#skills").append(formattedSkills);
+
+}
+
+if (work.Coop[0] !== 0)	{
+	var formattedWork;
+	var formattedTitle;
+	var formattedcombined;
+	$("#workExperience").append(HTMLworkStart);
+	for (position in work.Coop) {
+
+		formattedWork = HTMLworkEmployer.replace("%data%", work.Coop[position].Name);
+		formattedTitle = HTMLworkTitle.replace("%data%", work.Coop[position].Title);
+		formattedcombined = formattedWork + formattedTitle;
+		$(".work-entry").append(formattedcombined);
+
+		formattedWork = HTMLworkDates.replace("%data%", work.Coop[position].Date);
+		$(".work-entry").append(formattedWork);
+
+		formattedWork = HTMLworkLocation.replace("%data%", work.Coop[position].City);
+		$(".work-entry").append(formattedWork);
+
+		formattedWork = HTMLworkDescription.replace("%data%", work.Coop[position].Description);
+		$(".work-entry").append(formattedWork);
+	}
+}
+
+
 
 
 
