c98029b7d5c4e7fe98230dc4c55f7eac6e9c71a9
wookiecookie <wookiecookie87@gmail.com>
2015-08-13 10:22:01 +0900

    bio, education, project, work objects added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,85 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio={
+ 	"name" : "Jon Park",
+ 	"role" : "Software Engineer",
+ 	"contents" : {
+ 		"mobile" : "010-7532-3961",
+ 		"email"  : "wookiecookie87@gmail.com",
+ 		"github" : "wookiecookie87",
+ 		"location" : "Seoul",
+ 		"major" : "Mathematics"
+ 	}
+ }
+
+ var work=[
+ 	{
+ 		"employer" : "Yullin Technology",
+	 	"location" : "Seoul",
+	 	"dates" : "7 months",
+	 	"title" : "Software Engineer"
+ 	},
+ 	{
+ 		"employer" : "Breeze Studio",
+ 		"location" : "Seoul",
+ 		"dates" : "4 months",
+ 		"title" : "Software Engineer"
+ 	}
+
+ ]
+
+ var project=[
+	 	{
+	 		"title" : "LIMS",
+	 		"location" : "Bundang",
+	 		"description" : "Digital Pen",
+	 		"dates" : "7months"
+	 	},
+	 	{
+	 		"title" : "Beatname",
+	 		"location" : "Seoul",
+	 		"description" : "Mobile Front End",
+	 		"dates" : "3months"
+	 	}
+ 	]
+
+ var education ={
+ 	"schools":[
+ 		{
+ 			"name" : "Formham University",
+ 			"location" : "New York City",
+ 			"degree" : "BS",
+ 			"major" : ["Economics", "Mathematics"],
+ 			"date" : "2008 - 2013",
+ 			"url" : "www.fordham.edu"
+ 		},
+ 		{
+ 			"name" : "Covenant Christian High School",
+ 			"city" : "Indianapolis, IN"
+ 		}
+ 	],
+ 	"onlineCourse":[
+ 		{
+ 			"title" : "Machine Learning",
+ 			"school" : "Coursera",
+ 			"dates" : "11 weeks",
+ 			"url" : "www.ccoursera.edu"
+ 		},
+ 		{
+ 			"title" : "Git and Github",
+ 			"school" : "Udacity",
+ 			"dates" : "2 weeks",
+ 			"url" : "www.Udacity.edu"
+ 		},
+ 		{
+ 			"title" : "Basic Javascript",
+ 			"school" : "Udacity",
+ 			"dates" : "2 weeks",
+ 			"url" : "www.Udacity.edu"
+ 		}
+
+ 	]
+ }	
+
