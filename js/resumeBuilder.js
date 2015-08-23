dcc83181c350a67dbb50b70614f5c5a9100f8203
IlyKramer <ilykramer@gmail.com>
2015-08-22 20:46:52 -0400

    Update resumeBuilder.js
    fix the map

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var name = "Ilyse B. Kramer";
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").append(formattedName);
+$("#header").prepend(formattedName);
 var role = "Web Developer";
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 var formattedRole = HTMLheaderRole.replace("%data%", role);
@@ -21,41 +21,48 @@ var bio = {
 	},
 		"welcomeMesssage" : "information services professional seeking career as front-end web developer",
 		"skills": [
-			"Git", "HTML", "CSS", "JavaScript" 
+			"Git", "HTML", "CSS", "JavaScript"
 		],
 		"bioPic": "images/bioPic.png"
 	}
 	
 	
 	var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-	var formattedmobile = HTMLmobile.replace("%data%", "267-918-4883");
-	$("#header").append(formattedmobile);
-
+	var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedmobile);
+	$("#footerContacts").append(formattedmobile);
 	
 	var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-	var formattedemail = HTMLemail.replace("%data%", "ilykramer@gmail.com");
-	$("#header").append(formattedemail);
+	var formattedemail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").append(formattedemail);
+	$("#footerContacts").append(formattedemail);
 
 	
 	var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-	var formattedgithub = HTMLgithub.replace("%data%", "IlyKramer");
-	$("#header").append(formattedgithub);
+	var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").append(formattedgithub);
+	$("#footerContacts").append(formattedgithub);
 	
 	var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-	var formattedtwitter = HTMLtwitter.replace("%data%", "@edgyamelia");
-	$("#header").append(formattedtwitter);
+	var formattedtwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#topContacts").append(formattedtwitter);
+	$("#footerContacts").append(formattedtwitter);
+	
 	
 	var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-	var formattedblog = HTMLblog.replace("%data%", "www.edgyamelia.wordpress.com");
-	$("#header").append(formattedblog);
+	var formattedblog = HTMLblog.replace("%data%", bio.contacts.blog);
+	$("#topContacts").append(formattedblog);
+	$("#footerContacts").append(formattedblog);
 	
 	var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-	var formattedlocation = HTMLlocation.replace("%data%", "Washington, DC");
-	$("#header").append(formattedlocation);
+	var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").append(formattedlocation);
+	$("#footerContacts").append(formattedlocation);
 
 	var HTMLbioPic = '<img src="%data%" class="biopic">';
 	var formattedbioPic = HTMLbioPic.replace("%data%", "images/bioPic.png");
-	$("#header").append(formattedbioPic);
+	$("#topContacts").append(formattedbioPic)
+	
 	
 	var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 	var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", "information services professional seeking career as front-end web developer");
@@ -218,6 +225,8 @@ var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 
 
 
+
+
 clickLocations = [];
 
 function logClicks(x,y) {
@@ -237,9 +246,7 @@ $(document).click(function(loc) {
 	logClicks(x,y);
 });
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
+
 clickLocations = [];
 
 function logClicks(x,y) {
@@ -253,22 +260,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
 });
 
+var map;   
 
 
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
-var map;    // declares a global map variable
-
-
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
 
   var locations;
@@ -277,33 +273,20 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
+  
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  var googleMap
+  $("#mapDiv").append(googleMap);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
   function locationFinder() {
 
-    // initializes an empty array
+ 
     var locations = [];
-
-    // adds the single location property from bio to the locations array
     locations.push(bio.contacts.location);
-
-    // iterates through school locations and appends each location to
-    // the locations array
     for (var school in education.schools) {
       locations.push(education.schools[school].location);
     }
-
-    // iterates through work locations and appends each location to
-    // the locations array
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
@@ -311,104 +294,53 @@ function initializeMap() {
     return locations;
   }
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
   function createMapMarker(placeData) {
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat(); 
+    var lon = placeData.geometry.location.lng();  
+    var name = placeData.formatted_address;   
+    var bounds = window.mapBounds;            
 
-    // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
       position: placeData.geometry.location,
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
     var infoWindow = new google.maps.InfoWindow({
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
     });
 
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
+
     bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
     map.fitBounds(bounds);
-    // center the map
     map.setCenter(bounds.getCenter());
   }
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
 
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
   function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
     var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
     for (var place in locations) {
-
-      // the search request object
       var request = {
         query: locations[place]
       };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
       service.textSearch(request, callback);
     }
   }
-
-  // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
-
-  // locations is an array of location strings returned from locationFinder()
   locations = locationFinder();
-
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
   pinPoster(locations);
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+	
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
-
+	
