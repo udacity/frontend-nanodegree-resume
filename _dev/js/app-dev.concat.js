17f6ff38f5cd615070d281cdcba134459d6049dd
Noel <noche@Noel-Noches-MacBook-Pro.local>
2015-08-27 11:14:02 -0400

    Initial commit

---
+++ _dev/js/app-dev.concat.js
@@ -0,0 +1,503 @@
+var $ = require("jquery");
+
+/*
+This file contains all of the code running in the background that makes resumeBuilder.js
+possible. We call these helper functions because they support your code.
+*/
+
+
+/*
+These are HTML strings. As part of the course, you'll be using JavaScript functions to
+replace the %data% placeholder text you see in them.
+*/
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span class="grey-text" id="role">%data%</span>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="grey-text">%contact%</span><span>%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="grey-text"><i class="fa fa-phone"></i></span><span>%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="grey-text"><i class="fa fa-envelope"></i></span><span>%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="grey-text"><i class="fa fa-twitter"></i></span><span>%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="grey-text"><i class="fa fa-github-alt"></i></span><span>%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="grey-text">blog</span><span>%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="grey-text"><i class="fa fa-home"></i></span><span>%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="Resume photo">';
+var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<div class="skills-box"><h3 id="skillsH3">Skills at a glance</h3><ul id="skills"></ul></div>';
+var HTMLskills = '<li><i class="fa fa-check"></i>%data%</li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="%url%">%data%';
+var HTMLschoolDegree = ' - %data%</a>';
+var HTMLschoolDates = '<div class="date-text grey-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="%url%">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+// var HTMLonlineURL = '<br><a href="%url%">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+
+/*
+The next few lines about clicks are for the Collecting Click Locations.
+*/
+clickLocations = [];
+
+function logClicks(x,y) {
+	clickLocations.push(
+		{
+			x: x,
+			y: y
+		}
+	);
+	console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+	logClicks(loc.pageX, loc.pageY);
+});
+
+
+/*
+Generates the custom Google Map for the website.
+*/
+var map;    // declares a global map variable
+
+
+/*
+Start here! initializeMap() is called when page is loaded.
+*/
+function initializeMap() {
+
+	var locations;
+
+	var mapOptions = {
+		disableDefaultUI: true
+	};
+
+	// This next line makes `map` a new Google Map JavaScript Object and attaches it to
+	// <div id="map">, which is appended as part of an exercise late in the course.
+	map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+	/*
+	locationFinder() returns an array of every location string from the JSONs
+	written for bio, education, and work.
+	*/
+	function locationFinder() {
+
+		// initializes an empty array
+		var locations = [];
+
+		// adds the single location property from bio to the locations array
+		locations.push(bio.contacts.location);
+
+		// iterates through school locations and appends each location to the locations array
+		for (var school in education.schools) {
+			locations.push(education.schools[school].location);
+		}
+
+		// iterates through work locations and appends each location to
+		// the locations array
+		for (var job in work.jobs) {
+			if (work.jobs[job].location !== "Classified") {
+				locations.push(work.jobs[job].location);
+			}
+		}
+
+		return locations;
+	}
+
+	/*
+	createMapMarker(placeData) reads Google Places search results to create map pins.
+	placeData is the object returned from search results containing information
+	about a single location.
+	*/
+	function createMapMarker(placeData) {
+
+		// The next lines save location data from the search result object to local variables
+		var lat = placeData.geometry.location.G;  // latitude from the place service
+		var lon = placeData.geometry.location.K;  // longitude from the place service
+		var name = placeData.formatted_address;   // name of the place from the place service
+		var bounds = window.mapBounds;            // current boundaries of the map window
+
+		// marker is an object with additional data about the pin for a single location
+		var marker = new google.maps.Marker({
+			map: map,
+			position: placeData.geometry.location,
+			title: name
+		});
+
+		// infoWindows are the little helper windows that open when you click
+		// or hover over a pin on a map. They usually contain more information
+		// about a location.
+		var infoWindow = new google.maps.InfoWindow({
+			content: name
+		});
+
+		// hmmmm, I wonder what this is about...
+		google.maps.event.addListener(marker, 'click', function() {
+
+			// your code goes here!
+			infoWindow.open(map, marker);
+		});
+
+		// this is where the pin actually gets added to the map.
+		// bounds.extend() takes in a map location object
+		bounds.extend(new google.maps.LatLng(lat, lon));
+		// fit the map to the new marker
+		map.fitBounds(bounds);
+		// center the map
+		map.setCenter(bounds.getCenter());
+	}
+
+	/*
+	callback(results, status) makes sure the search returned results for a location.
+	If so, it creates a new map marker for that location.
+	*/
+	function callback(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+				createMapMarker(results[0]);
+		}
+	}
+
+	/*
+	pinPoster(locations) takes in the array of locations created by locationFinder()
+	and fires off Google place searches for each location
+	*/
+	function pinPoster(locations) {
+
+		// creates a Google place search service object. PlacesService does the work of
+		// actually searching for location data.
+		var service = new google.maps.places.PlacesService(map);
+
+		// Iterates through the array of locations, creates a search object for each location
+		for (var place in locations) {
+
+			// the search request object
+			var request = {
+				query: locations[place]
+				};
+
+				// Actually searches the Google Maps API for location data and runs the callback
+				// function with the search results after each search.
+				service.textSearch(request, callback);
+		}
+	}
+
+	// Sets the boundaries of the map based on pin locations
+	window.mapBounds = new google.maps.LatLngBounds();
+
+	// locations is an array of location strings returned from locationFinder()
+	locations = locationFinder();
+
+	// pinPoster(locations) creates pins on the map for each location in
+	// the locations array
+	pinPoster(locations);
+
+}
+
+
+/*
+Uncomment the code below when you're ready to implement a Google Map!
+*/
+
+// Calls the initializeMap() function when the page loads
+// setTimeout is so that page loads smoothly before map.
+window.addEventListener('load', function() {
+	window.setTimeout(initializeMap, 2000);
+
+	var mapScript = document.createElement('script');
+	var mapSrc = "https://maps.googleapis.com/maps/api/js?libraries=places&callback=initializeMap";
+	var mainNode = document.getElementById('main');
+	mapScript.setAttribute('src', mapSrc);
+	mainNode.appendChild(mapScript);
+});
+
+
+// Vanilla JS way to listen for resizing of the window and adjust map bounds
+// Again, using setTimeout to improve page performance.
+window.addEventListener('resize', function(e) {
+	var eventArray = [];
+	var delayer;
+	eventArray.push(e);
+
+	if (eventArray.length > 0) {
+		window.setTimeout(function(eObj) {
+			var eObj = eventArray.pop(e);
+			eventArray = [];
+
+			// Make sure the map bounds get updated on page resize
+			map.fitBounds(mapBounds);
+		}, 2000);
+	}
+});
+
+
+/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
+// Object 1
+var bio = {
+	"name": "Hulk",
+	"role": "demolition expert",
+	"contacts": {
+		"mobile": "555-555-5555",
+		"email": "hulk_1962@email.com",
+		"github": "github.com/hulk1962",
+		"twitter": "twitter.com/r33l_hulk",
+		"location": "Malibu, California"
+	},
+	"welcomeMessage": '"What can Hulk do for you?"',
+	"skills": ["&checkmark;&nbsp; Smash evil doers", "&checkmark;&nbsp; Crush alien hordes", "&checkmark;&nbsp; Pummel projects"],
+	"biopic": "https://farm1.staticflickr.com/549/20166468218_2cf1342748.jpg",
+	"display": function() {
+		var fmtName = HTMLheaderName.replace("%data%", bio.name);
+		var fmtRole = HTMLheaderRole.replace("%data%", bio.role);
+		var fmtMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+		var fmtEmail = HTMLemail.replace("%data%", bio.contacts.email);
+		var fmtGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+		var fmtTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+		var fmtLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+		var fmtMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+		var fmtPic = HTMLbioPic.replace("%data%", bio.biopic);
+
+		$("#header").prepend(fmtRole);
+		$("#header").prepend(fmtName);
+		$("#topContacts").append(fmtMobile, fmtEmail, fmtGithub, fmtTwitter, fmtLocation);
+		$("#footerContacts").append(fmtMobile, fmtEmail, fmtGithub, fmtTwitter, fmtLocation);
+		$("#header").append(fmtPic);
+		$("#header").append(fmtMsg);
+		$("#header").append(HTMLskillsStart);
+
+		for (var skill in bio.skills) {
+			var fmtSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+
+			$("#skills").append(fmtSkills);
+		}
+	}
+};
+
+
+// Object 2
+var education = {
+	"schools": [
+		{
+			"name": "California Institute of Technology",
+			"location": "Pasadena, California",
+			"degree": "Ph.D.",
+			"majors": ["Nuclear Physics"],
+			"dates": "1976",
+			"url": "#"
+		},
+		{
+			"name": "Harvard University",
+			"location": "Cambridge, Massachusetts",
+			"degree": "M.S.",
+			"majors": ["Nuclear Physics", "Biochemistry"],
+			"dates": "1975",
+			"url": "#"
+		},
+		{
+			"name": "University of New Mexico",
+			"location": "Albuquerque, New Mexico",
+			"degree": "B.S.",
+			"majors": ["Physics", "Mathematics", "Microbiology"],
+			"dates": "1972-1974",
+			"url": "#"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-end Developer",
+			"school": "Udacity",
+			"date": "2015",
+			"url": "#"
+		},
+		{
+			"title": "Action Film Choreographer",
+			"school": "Marvel Edx",
+			"date": "2013",
+			"url": "#"
+		}
+	],
+	"display": function () {
+
+		if (education.schools) {
+			var schools = education.schools;
+
+			for (var school in schools) {
+				$("#education").append(HTMLschoolStart);
+				var schoolAndDegree = HTMLschoolName.replace("%data%", schools[school].name)
+													.replace("%url%", schools[school].url);
+				schoolAndDegree += HTMLschoolDegree.replace("%data%", schools[school].degree);
+				var fmtDates = HTMLschoolDates.replace("%data%",  schools[school].dates);
+				var fmtMajors = HTMLschoolMajor.replace("%data%", schools[school].majors.join(", "));
+				var fmtLocation = HTMLschoolLocation.replace("%data%",  schools[school].location);
+
+				$(".education-entry:last").append(schoolAndDegree, fmtDates, fmtLocation, fmtMajors);
+			}
+		}
+
+		if (education.onlineCourses) {
+			var eCourses = education.onlineCourses;
+
+			$("#education").append(HTMLonlineClasses);
+
+			for (var course in eCourses) {
+				$("#education").append(HTMLschoolStart);
+
+				var titleAndSchool = HTMLonlineTitle.replace("%data%", eCourses[course].title)
+													.replace("%url%", eCourses[course].url);
+				titleAndSchool += HTMLonlineSchool.replace("%data%", eCourses[course].school);
+
+				var fmtDate = HTMLonlineDates.replace("%data%", eCourses[course].date);
+
+				// TO INSTRUCTOR: I'm not sure why this is needed, so I put the url
+				// in the school name hotlink.
+// 					var fmtUrl = HTMLonlineURL.replace("%data%", eCourses[course].url)
+// 											  .replace("%url%", eCourses[course].url);
+
+				$(".education-entry:last").append(titleAndSchool, fmtDate);
+			}
+		}
+	}
+};
+
+
+// Object 3
+var work = {
+	"jobs":[
+		{
+			"employer": "The Avengers",
+			"title": "Adventurer",
+			"location": "Manhattan, New York City",
+			"dates": "1984-2013",
+			"description": "Subdued aggressive clientele from other dimensions and times."
+		},
+		{
+			"employer": "University of California, Los Angeles",
+			"title": "Custodian",
+			"location": "Los Angeles, California",
+			"dates": "1980-83",
+			"description": "Cleaned and maintained research labs around campus grounds."
+		},
+		{
+			"employer": "United States Defense Department",
+			"title": "Researcher",
+			"location": "Classified",
+			"dates": "1978-79",
+			"description": "Designed and oversaw construction of experimental Gamma Bomb."
+		}
+	],
+	"display": function() {
+
+		for (var i=0; i < work.jobs.length; i++) {
+			$("#workExperience").append(HTMLworkStart);
+
+			var fmtEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+			var fmtTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+			var empTitle = fmtEmployer + fmtTitle;
+			var fmtDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+			var fmtLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+			var fmtDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+			$(".work-entry:last").append(empTitle, fmtDates, fmtLocation, fmtDescription);
+		}
+	}
+};
+
+
+// Object 4
+var projects = {
+	"projects":[
+		{
+			"title": "Avengers NYC Project",
+			"dates": "2012",
+			"description": "Hulk with some help of sidekicks successfully repelled invasion of New York City by demi-god Loki and some extraterrestrial aliens.",
+			"images": ["https://farm1.staticflickr.com/497/20166422760_c41aafe657.jpg", "https://farm1.staticflickr.com/282/20166469918_270f33f507.jpg", "https://farm1.staticflickr.com/318/20328217206_2f1cc233df.jpg"]
+		},
+		{
+			"title": "Hulk Action Series",
+			"dates": "1978-82",
+			"description": "Hulk played eponymous hero in quest of finding cure for dreaded Bruce Banner disease.",
+			"images": ["https://farm1.staticflickr.com/549/20328215316_8c738aae3e.jpg", "https://farm1.staticflickr.com/278/20345981832_26fcbb76f4.jpg", "https://farm1.staticflickr.com/431/19731811484_64a1867915.jpg"]
+		}
+	],
+	"display": function() {
+
+		for (var i=0; i < projects.projects.length; i++) {
+			$("#projects").append(HTMLprojectStart);
+
+			var fmtTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+			var fmtDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+			var fmtDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+			$(".project-entry:last").append(fmtTitle, fmtDates, fmtDescription);
+
+			for (var img in projects.projects[i].images) {
+				var fmtImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+
+				$(".project-entry:last").append(fmtImage);
+			}
+		}
+	}
+};
+
+
+// Animates the navigation menu on page load
+$("nav").animate({
+	width: "50px"
+}, 1500);
+
+// Displays or hides the navigation menu
+$("nav, nav li").click(function() {
+	if ($("nav").css("width") === "50px") {
+		$("nav").animate({
+			width: "200px"
+		});
+		$("nav ul").css({
+			"display": "block",
+			"position": "absolute",
+			"z-index": "1",
+			"top": "52px",
+			"padding-bottom": "10px",
+			"width": "200px",
+			"background-color": "#000000"
+		});
+
+	} else {
+		$("nav").animate({
+			width: "50px"
+		});
+		$("nav ul").css("display", "none");
+	}
+});
+
+
+// Modular functions
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+
+// Displays Google Map
+$("#mapDiv").append(googleMap);
---
+++ _dev/js/main.js
@@ -0,0 +1,3 @@
+var App = (function(global) {
+	require("./app.min.js");
+})(this);
