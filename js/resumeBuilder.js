f6554735ec86296b59408c2a609f3aac1e435787
Daniel Burdick <burdick.daniel@gmail.com>
2015-08-27 20:48:26 -0700

    Initial Commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+var formattedName = HTMLheaderName.replace("%data%", "Daniel Burdick");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web-Dev");
+
+
+
+
+$("#header").prepend(formattedName, formattedRole);
