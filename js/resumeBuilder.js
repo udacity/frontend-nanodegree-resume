e3c44874dfb173f39bdc7fd7fc43e292ae9d4bf5
Chuiwen Ma <machuiwen@gmail.com>
2015-08-27 21:44:59 -0700

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Chuiwen Ma");
+
+var role = "Software Engineer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
