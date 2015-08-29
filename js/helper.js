daf9a40539cad2de9d3c39599e7d4597391a719b
Chuiwen Ma <machuiwen@gmail.com>
2015-08-28 17:59:27 -0700

    My first version resume, with work/project sections and a map.

--- js/helper.js
+++ js/helper.js
@@ -64,7 +64,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+    var iName = inName(bio.name) || function(){};
     $('#name').html(iName);  
   });
 });
@@ -235,11 +235,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,11 +16,18 @@ var bio = {
 	"bioPic": "images/me.jpg"
 };
 
-// var formattedName = HTMLheaderName.replace("%data%", bio.name);
-// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	for (skill in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
+	}
+}
 
 var education = {
 	"schools": [
@@ -76,9 +83,62 @@ var project = {
 			"title": "3D Reconstruction from Full-view Fisheye Camera",
 			"dates": "March 2015",
 			"description": "Proposed a full-view scene reconstruction algorithm based on Ricoh Theta fisheye camera. Developed the fisheye camera model, epipolar geometry and triangulation method. Reconstructed the 360 degree scene with only two captures. Generalized the method to multi-camera reconstruction, also proposed a dense reconstruction approach on rectified images. Developed a Matlab GUI.",
-			"images": [
+			"images": [ "images/me.jpg"
 			],
 			"report": "http://arxiv.org/pdf/1506.06273v1.pdf",
 		}
 	]
 };
+
+function displayWork() {
+
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}
+
+displayWork();
+
+function inName(name) {
+	name = name.trim().split(" ");
+	name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+	name[1] = name[1].toUpperCase();
+	return name[0] + " " + name[1];
+}
+
+$("#main").append(internationalizeButton);
+
+project.display = function() {
+	for (proj in project.projects) {
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", project.projects[proj].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", project.projects[proj].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", project.projects[proj].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (project.projects[proj].images.length > 0) {
+			for (image in project.projects[proj].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", project.projects[proj].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+project.display();
+
+$("#mapDiv").append(googleMap);
