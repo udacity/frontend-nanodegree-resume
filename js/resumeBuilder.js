601d0d952c3b75727591d1219f0cae95c8f18364
Niko <niko.caron90@gmail.com>
2015-08-30 18:11:51 -0300

    Resume builder skeleton
    feat: Resume builder skeleton
    
    Added a skeleton for the required code.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,43 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// Bio JSON
+var bio = {
+}
+
+// Function to display the bio JSON
+bio.display = function() {
+}
+
+// Displays the bio JSON
+bio.display();
+
+// Education JSON
+var education = {
+}
+
+// Function to display the education JSON
+education.display = function() {
+}
+
+// Displays the education JSON
+education.display();
+
+// Work JSON
+var work = {
+}
+
+// Function to display the work JSON
+work.display = function() {
+}
+
+// Displays the work JSON
+work.display();
+
+// Projects JSON
+var projects = {
+}
+
+// Function to display the projects JSON
+projects.display = function() {
+}
+
+// Displays the projects JSON
+projecs.display();
