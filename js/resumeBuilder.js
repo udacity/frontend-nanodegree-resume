1ac880d64f6e75b34eeee59995f64d0b9c71f1d3
Niko <niko.caron90@gmail.com>
2015-08-30 23:50:01 -0300

    Completed projects display function
    Feat: Completed projects display function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -177,6 +177,25 @@ var projects = {
 
 // Function to display the projects JSON
 projects.display = function() {
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+        
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+        
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+        
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImages);
+            }
+        }
+    }
 }
 
 // Displays the projects JSON
