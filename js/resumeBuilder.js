689f075ddcd5ef52b01738a3b3f812d2b2f2bbff
Daniel Burdick <burdick.daniel@gmail.com>
2015-09-12 00:41:48 -0700

    validate code and submit.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,20 @@
 var bio = {
-  'name': 'Daniel Burdick',
-  'age': 29,
-  'role': 'Web-Dev',
-  'biopic': 'http://placehold.it/300x300',
-  'contacts': {
-    'mobile': '206-817-8312',
-    'email': 'burdick.daniel@gmail.com',
-    'github': '<a class="white-text" href="http://github.com/burdiick">Burdiick</a>',
-    'location': 'Bothell, WA'
+  name: 'Daniel Burdick',
+  age: 29,
+  role: 'Web-Dev',
+  biopic: 'http://placehold.it/300x300',
+  contacts: {
+    mobile: '206-817-8312',
+    email: 'burdick.daniel@gmail.com',
+    github: '<a class="white-text" href="http://github.com/burdiick">Burdiick</a>',
+    location: 'Bothell, WA'
   },
-  'skills': ['HTML', 'CSS', 'Javascript', 'Objective-C', 'Swift', 'Java', 'Clojure!!', 'C#', 'C++', 'C', 'BASIC', 'Assembly'],
-  'welcomeMessage': 'Welcome to my online resume! It is built with love, in javascript.'
+  skills: ['HTML', 'CSS', 'Javascript', 'Objective-C', 'Swift', 'Java', 'Clojure!!', 'C#', 'C++', 'C', 'BASIC', 'Assembly'],
+  welcomeMessage: 'Welcome to my online resume! It is built with love, in javascript.'
 };
 
-bio.display = function() {
+bio.display = function () {
+  "use strict";
   $('#header').prepend(replaceData(HTMLheaderRole, bio.role));
   $('#header').prepend(replaceData(HTMLheaderName, bio.name));
   bio.displayContacts('#topContacts');
@@ -29,41 +30,41 @@ bio.display = function() {
   }
 };
 
-bio.displayContactsInFooter = function() {
+bio.displayContactsInFooter = function () {
   bio.displayContacts('#footerContacts');
 };
 
 // Helper function for displaying contact info in multiple locations.
-bio.displayContacts = function(tag) {
+bio.displayContacts = function (tag) {
   $(tag).append(replaceData(HTMLmobile, bio.contacts.mobile));
   $(tag).append(replaceData(HTMLemail, bio.contacts.email));
   $(tag).append(replaceData(HTMLgithub, bio.contacts.github));
 };
 
 var work = {
-  'jobs': [{
-    'employer': 'Vanguard Constuction Services',
-    'title': 'Managing Member',
-    'location': 'Seattle, WA',
-    'dates': {
-      'start': '2010',
-      'end': '2015'
+  jobs: [{
+    employer: 'Vanguard Constuction Services',
+    title: 'Managing Member',
+    location: 'Seattle, WA',
+    dates: {
+      start: '2010',
+      end: '2015'
     },
-    'description': 'Purchase and remodel homes. Determin Scope of work. Hire and manage subcontractors. Perform general carpentry and finish work to complete job.'
+    description: 'Purchase and remodel homes. Determin Scope of work. Hire and manage subcontractors. Perform general carpentry and finish work to complete job.'
   }, {
-    'employer': 'Graham Contracting',
-    'title': 'Project Site Coordinator',
-    'location': 'Seattle, WA',
-    'dates': {
-      'start': '2007',
-      'end': '2010'
+    employer: 'Graham Contracting',
+    title: 'Project Site Coordinator',
+    location: 'Seattle, WA',
+    dates: {
+      start: '2007',
+      end: '2010'
     },
-    'description': 'Manage setup and maintanence of on site trailer. Make all purchases for the job site and file POs. Assist with Change Orders. Perform record keeping for all documents on site.'
+    description: 'Manage setup and maintanence of on site trailer. Make all purchases for the job site and file POs. Assist with Change Orders. Perform record keeping for all documents on site.'
   }]
 };
 
 // Standard for loop used to iterate through jobs.
-work.display = function() {
+work.display = function () {
   if (work.jobs.length > 0) {
     for (var i = 0; i < work.jobs.length; i++) {
       $('#workExperience').append(HTMLworkStart);
@@ -78,20 +79,20 @@ work.display = function() {
 };
 
 var education = {
-  'schools': [{
-    'name': 'Lake Washington Technical College',
-    'location': '11605 132nd Ave NE, Kirkland, WA 98034',
-    'degree': 'Associates Degree',
-    'majors': 'Electronics Technology',
-    'dates': '2012-2013',
-    'url': 'http://www.lwtech.edu/'
+  schools: [{
+    name: 'Lake Washington Technical College',
+    location: '11605 132nd Ave NE, Kirkland, WA 98034',
+    degree: 'Associates Degree',
+    majors: 'Electronics Technology',
+    dates: '2012-2013',
+    url: 'http://www.lwtech.edu/'
   }],
-  'onlineCourses': [{
-    'school': 'Udacity',
-    'title': 'Front-end Web Development',
-    'dates': '2015-Present',
-    'description': 'Learn the skills and best practices needed to build responsive modern websites.',
-    'url': 'http://www.udacity.com'
+  onlineCourses: [{
+    school: 'Udacity',
+    title: 'Front-end Web Development',
+    dates: '2015-Present',
+    description: 'Learn the skills and best practices needed to build responsive modern websites.',
+    url: 'http://www.udacity.com'
   }]
 };
 
@@ -120,16 +121,16 @@ education.display = function () {
 };
 
 var projects = {
-  'project': [{
-    'title': 'Portfolio',
-    'dates': 'July, 2015',
-    'description': 'A simple portfolio project with a focus on responsive programming principles.',
-    'images': ['images/project-one-page.png', 'images/project-one-code.png']
+  project: [{
+    title: 'Portfolio',
+    dates: 'July, 2015',
+    description: 'A simple portfolio project with a focus on responsive programming principles.',
+    images: ['images/project-one-page.png', 'images/project-one-code.png']
   }, {
-    'title': 'Resume',
-    'dates': 'July, 2015',
-    'description': 'This webpage! A javascript/jquery primer on manipulating the DOM.',
-    'images': ['images/project-two-page.png', 'images/project-two-code.png']
+    title: 'Resume',
+     dates: 'July, 2015',
+    description: 'This webpage! A javascript/jquery primer on manipulating the DOM.',
+    images: ['images/project-two-page.png', 'images/project-two-code.png']
   }]
 };
 
