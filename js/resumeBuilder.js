abc3ab4de56201416b62cd20a9f9c95b05aa4db0
Scott Michael <scottmichael2@users.noreply.github.com>
2015-09-12 19:07:43 -0500

    JS Lessons
    This is about 1/2 way through the Udacity JS lessons.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,114 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work={
+	"jobs":[
+		{
+			"employer":"Emdeon",
+			"title":"Software Engineer",
+			"location":"Nashville, TN",
+			"dates":"2007-2015",
+			"description":"software development"
+		},
+		{
+			"employer":"American Standard",
+			"title":"Programmer/Analyst",
+			"location":"Nashville, TN",
+			"dates":"2001-2006",
+			"description":"software development"
+		}
+	]
+};
+
+var projects={
+	"projects":[
+		{
+			"title":"American Standard Website",
+			"dates":"2004-2006",
+			"description":"Developed the American Standard Leasing website.",
+			"images":["http://www.americanstandardair.com/etc/designs/irco-asa-us-en/_jcr_content/contentpage/masthead/headerLogoImage.img.png/x1386877603560.png.pagespeed.ic.DZsNaccOC7.png","http://www.americanstandardair.com/content/irco-asa/us/en/products/heating-and-cooling/air-conditioners/platinum-zv-air-conditioning-.img.png/1434659686121.jpg"]
+		},
+		{
+			"title":"Emdeon Assistant",
+			"dates":"2007-2015",
+			"description":"Developed the Emdeon Assistant website.",
+			"images":["https://upload.wikimedia.org/wikipedia/en/b/bb/Emdeon.png"]
+
+		}
+	]
+};
+
+var bio={
+	"name":"Scott Michael",
+	"role":"Web Developer",
+	"contacts":{
+		"mobile":"615-555-5555",
+		"email":"scottmichael2@gmail.com",
+		"github":"scottmichael2",
+		"twitter":"@scottmichael2",
+		"location":"Hermitage, TN"
+	},
+	"picture":"images/fry.jpg",
+	"message":"Welcome to my online resume!",
+	"skills":["HTML"," JavaScript"," jQuery"]
+};
+
+var education={
+	"schools":[
+		{
+			"name":"Florida Southern College",
+			"location":"Lakeland, FL",
+			"degree":"BS",
+			"majors":["Computer Science"],
+			"dates":2000,
+			"url":"http://www.flsouthern.edu/home.aspx"
+		},
+		{
+			"name":"University of Phoenix",
+			"location":"Phoenix, AZ",
+			"degree":"MS",
+			"majors":["Computer Information Systems"],
+			"dates":2007,
+			"url":"http://www.phoenix.edu/"
+		}
+	]
+},
+	"onlineCourses":[
+		{
+			"JavaScript Syntax",
+			"school":"Udacity",
+			"date":2015,
+			"url": "http://www.udacity.com/course/ud804"
+		}
+	]
+};
+
+
+
+//var formattedName=HTMLheaderName.replace("%data%",bio.name);
+//var formattedRole=HTMLheaderRole.replace("%data%",bio.role);
+//var formattedMobile=HTMLmobile.replace("%data%",bio.contacts.mobile);
+//var formattedEmail=HTMLemail.replace("%data%",bio.contacts.email);
+//var formattedTwitter=HTMLtwitter.replace("%data%",bio.contacts.twitter);
+//var formattedGithub=HTMLgithub.replace("%data%",bio.contacts.github);
+//var formattedLocation=HTMLlocation.replace("%data%",bio.contacts.location);
+//var formattedPic=HTMLbioPic.replace("%data%",bio.picture);
+//var formattedWelcome=HTMLwelcomeMsg.replace("%data%",bio.message);
+//var formattedSkills=HTMLskills.replace("%data%",bio.skills);
+
+//$("#topContacts").append(formattedLocation);
+//$("#topContacts").append(formattedMobile);
+//$("#topContacts").append(formattedEmail);
+//$("#topContacts").append(formattedTwitter);
+//$("#topContacts").append(formattedGithub);
+//$("#header").prepend(formattedSkills);
+//$("#header").prepend(HTMLskillsStart);
+//$("#header").prepend(formattedWelcome);
+//$("#header").prepend(formattedPic);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+//var work={
+//	"position":"Software Engineer",
+//	"employer":"Emdeon",
+//	"years":8
+//};
+//work.city="Nashville";
+
