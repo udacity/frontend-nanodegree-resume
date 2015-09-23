a54212e042fc091d14f57e9655a8dc2b27f46461
xavierjacks <xavierjacks@yahoo.com>
2015-09-23 19:00:45 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace ("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
  */
