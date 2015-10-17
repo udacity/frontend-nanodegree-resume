485aa9b132dc8f558610701796189347ced5d052
Juan Meza <jrmeza514@gmail.com>
2015-10-17 06:46:31 -0700

    More Formating
    Added Dot Notation for son References, and a 80 character ruler

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,4 @@
+var data = "%data%";
 var bio = {
     "name": "Juan Meza",
     "role": "Front End Developer",
@@ -20,33 +21,51 @@ var bio = {
     "bioPic": "images/fry.jpg"
 };
 bio.display = function() {
-    var formattedName = HTMLheaderName.replace("%data%", bio["name"]);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio["role"]);
-    var formattedMobile = HTMLmobile.replace("%data%", bio["contacts"]["mobile"]);
-    var formattedEmail = HTMLemail.replace("%data%", bio["contacts"]["email"]);
-    var formattedTwitter = HTMLtwitter.replace("%data%", bio["contacts"]["twitter"]);
-    var formattedGithub = HTMLgithub.replace("%data%", bio["contacts"]["github"]);
-    var formattedLocation = HTMLlocation.replace("%data%", bio["contacts"]["location"]);
-    var formattedBioPic = HTMLbioPic.replace("%data%", bio["bioPic"]);
-    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio["welcomeMessage"]);
+    var formattedName =
+        HTMLheaderName.replace(data, bio.name);
+    var formattedRole =
+        HTMLheaderRole.replace(data, bio.role);
+    var formattedMobile =
+        HTMLmobile.replace(data, bio.contacts.mobile);
+    var formattedEmail =
+        HTMLemail.replace(data, bio.contacts.email);
+    var formattedTwitter =
+        HTMLtwitter.replace(data, bio.contacts.twitter);
+    var formattedGithub =
+        HTMLgithub.replace(data, bio.contacts.github);
+    var formattedLocation =
+        HTMLlocation.replace(data, bio.contacts.location);
+    var formattedBioPic =
+        HTMLbioPic.replace(data, bio.bioPic);
+    var formattedWelcomeMsg =
+        HTMLwelcomeMsg.replace(data, bio.welcomeMessage);
 
-    $("#header").prepend(formattedRole);
-    $("#header").prepend(formattedName);
-    $("#topContacts").append(formattedMobile);
-    $("#topContacts").append(formattedEmail);
-    $("#topContacts").append(formattedTwitter);
-    $("#topContacts").append(formattedGithub);
-    $("#topContacts").append(formattedLocation);
-    $("#header").append(formattedBioPic);
-    $("#header").append(formattedWelcomeMsg);
-    $("#header").append(HTMLskillsStart);
-    $("#footerContacts").append(formattedMobile);
-    $("#footerContacts").append(formattedEmail);
-    $("#footerContacts").append(formattedTwitter);
-    $("#footerContacts").append(formattedGithub);
-    $("#footerContacts").append(formattedLocation);
-    $.each(bio["skills"], function(index, currentSkill) {
-        var formattedSkill = HTMLskills.replace("%data%", currentSkill);
+    $("#header").prepend(
+        formattedRole,
+        formattedName
+    );
+    $("#topContacts").append(
+        formattedMobile,
+        formattedEmail,
+        formattedTwitter,
+        formattedGithub,
+        formattedLocation
+    );
+    $("#header").append(
+        formattedBioPic,
+        formattedWelcomeMsg,
+        HTMLskillsStart
+    );
+    $("#footerContacts").append(
+        formattedMobile,
+        formattedEmail,
+        formattedTwitter,
+        formattedGithub,
+        formattedLocation
+    );
+    $.each(bio.skills, function(index, currentSkill) {
+        var formattedSkill =
+            HTMLskills.replace(data, currentSkill);
         $("#skills").append(formattedSkill);
     });
 };
@@ -55,32 +74,39 @@ var work = {
         "employer": " Target Corporation",
         "title": "Stockroom Team Member",
         "location": "Cottle Rd, San Jose, CA",
-        "dates": "2014 to Present",
+        "dates": "2015",
         "description": "Maintaining Accurate Inventory of Store Merchandise"
     }, {
         "employer": " Target Corporation",
         "title": "Truck Unload Team Member",
         "location": "San Jose, CA",
-        "dates": "2014 to 2015",
+        "dates": "2015",
         "description": "Unload Incoming Trucks with new Merchandise"
     }]
 };
-work.display= function() {
-    for (var currentJob = 0; currentJob < work["jobs"].length; currentJob++) {
-        $("#workExperience").append(HTMLworkStart);
+work.display = function() {
+    $.each(work.jobs, function(index, currentJob) {
+        $("#workExperience").append( HTMLworkStart );
         currentEntry = $(".work-entry")[currentJob];
-        var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work["jobs"][currentJob]["employer"]);
-        var formattedWorkTitlte = HTMLworkTitle.replace("%data%", work["jobs"][currentJob]["title"]);
-        var formattedWrkDates = HTMLworkDates.replace("%data%", work["jobs"][currentJob]["dates"]);
-        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work["jobs"][currentJob]["location"]);
-        var formattedWorkDescription = HTMLworkDescription.replace("%data%", work["jobs"][currentJob]["description"]);
+        var formattedWorkEmployer =
+            HTMLworkEmployer.replace( data, currentJob.employer );
+        var formattedWorkTitlte =
+            HTMLworkTitle.replace( data, currentJob.title );
+        var formattedWrkDates =
+            HTMLworkDates.replace( data, currentJob.dates );
+        var formattedWorkLocation =
+            HTMLworkLocation.replace( data, currentJob.location );
+        var formattedWorkDescription =
+            HTMLworkDescription.replace( data, currentJob.description );
 
-        $(currentEntry).append(formattedWorkEmployer);
-        $(currentEntry).append(formattedWorkTitlte);
-        $(currentEntry).append(formattedWrkDates);
-        $(currentEntry).append(formattedWorkLocation);
-        $(currentEntry).append(formattedWorkDescription);
-    }
+        $(currentEntry).append(
+            formattedWorkEmployer,
+            formattedWorkTitlte,
+            formattedWrkDates,
+            formattedWorkLocation,
+            formattedWorkDescription
+        );
+    });
 };
 var education = {
     "schools": [{
@@ -88,14 +114,14 @@ var education = {
         "location": "Santa Teresa Highschool ,San Jose, CA",
         "degree": "GED",
         "major": ["High School Diploma"],
-        "dates": "2010-2014",
+        "dates": "2014",
         "url": "http://santateresa.esuhsd.org"
     }, {
         "name": "West Valley College",
         "location": "West Valley College, Saratoga, CA",
         "degree": "AA",
         "major": ["Web and Mobile Design"],
-        "dates": "2014-Present",
+        "dates": "2015",
         "url": "http://www.westvalley.edu"
     }],
     "onlineCourses": [{
@@ -106,14 +132,19 @@ var education = {
     }]
 };
 education.display = function() {
-    $.each(education["schools"], function(index, currentSchool) {
+    $.each(education.schools , function(index, currentSchool) {
         $("#education").append(HTMLschoolStart);
         var currentEntry = $(".education-entry")[index];
-        var formattedSchoolName = HTMLschoolName.replace("%data%", currentSchool["name"]);
-        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", currentSchool["degree"]);
-        var formattedSchoolDates = HTMLschoolDates.replace("%data%", currentSchool["dates"]);
-        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", currentSchool["major"]);
-        var fomattedSchoolLocation = HTMLschoolLocation.replace("%data%", currentSchool["location"]);
+        var formattedSchoolName =
+            HTMLschoolName.replace(data, currentSchool.name);
+        var formattedSchoolDegree =
+            HTMLschoolDegree.replace(data, currentSchool.degree);
+        var formattedSchoolDates =
+            HTMLschoolDates.replace(data, currentSchool.dates);
+        var formattedSchoolMajor =
+            HTMLschoolMajor.replace(data, currentSchool.major);
+        var fomattedSchoolLocation =
+            HTMLschoolLocation.replace(data, currentSchool.location);
 
         $(currentEntry).append(formattedSchoolName + formattedSchoolDegree);
         $(currentEntry).append(fomattedSchoolLocation);
@@ -123,17 +154,24 @@ education.display = function() {
 
     });
     $("#education").append(HTMLonlineClasses);
-    $.each(education["onlineCourses"], function(index, currentCourse) {
+    $.each(education.onlineCourses, function(index, currentCourse) {
         $("#education").append(HTMLschoolStart);
-        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", currentCourse["title"]);
-        var currentEntry = $(".education-entry")[education["schools"].length + index];
-        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", currentCourse["school"]);
-        var formattedOnlineDates = HTMLonlineDates.replace("%data%", currentCourse["dates"]);
-        var formattedOnlineUrl = HTMLonlineURL.replace("%data%", currentCourse["url"]);
+        var formattedOnlineTitle =
+            HTMLonlineTitle.replace(data, currentCourse.title);
+        var currentEntry =
+            $(".education-entry")[education.schools.length + index];
+        var formattedOnlineSchool =
+            HTMLonlineSchool.replace(data, currentCourse.school);
+        var formattedOnlineDates =
+            HTMLonlineDates.replace(data, currentCourse.dates);
+        var formattedOnlineUrl =
+            HTMLonlineURL.replace(data, currentCourse.url);
 
-        $(currentEntry).append(formattedOnlineTitle + formattedOnlineSchool);
-        $(currentEntry).append(formattedOnlineDates);
-        $(currentEntry).append(formattedOnlineUrl);
+        $(currentEntry).append(
+            formattedOnlineTitle + formattedOnlineSchool,
+            formattedOnlineDates,
+            formattedOnlineUrl
+        );
     });
 
 };
@@ -143,29 +181,39 @@ var projects = {
         "title": "Intereactive Resume",
         "dates": "2015",
         "description": "Online Resume as Part of my Web Portfolio",
-        "images": ["images/197x148.gif", "images/197x148.gif", "images/197x148.gif", "images/197x148.gif"],
+        "images": [
+            "images/197x148.gif",
+            "images/197x148.gif",
+            "images/197x148.gif",
+            "images/197x148.gif"
+        ],
         "link": "index.html"
     }]
 };
 projects.display = function() {
-        $.each(projects["projects"], function(index, currentProject) {
-            var formattedProject = HTMLprojectStart;
-            $("#projects").append(formattedProject);
-            currentEntry = $(".project-entry")[index];
-            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", currentProject["title"]);
-            var formattedProjectDates = HTMLprojectDates.replace("%data%", currentProject["dates"]);
-            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", currentProject["description"]);
+    $.each(projects.projects, function(index, project) {
+        $("#projects").append(HTMLprojectStart);
+        currentEntry = $(".project-entry")[index];
+        var formattedProjectTitle =
+            HTMLprojectTitle.replace(data, project.title);
+        var formattedProjectDates =
+            HTMLprojectDates.replace(data, project.dates);
+        var formattedProjectDescription =
+            HTMLprojectDescription.replace(data, project.description);
 
-            $(currentEntry).append(formattedProjectTitle);
-            $(currentEntry).append(formattedProjectDates);
-            $(currentEntry).append(formattedProjectDescription);
+        $(currentEntry).append(
+            formattedProjectTitle,
+            formattedProjectDates,
+            formattedProjectDescription
+        );
 
-            $.each(currentProject["images"], function(index, value) {
-                var formattedProjectImage = HTMLprojectImage.replace("%data%", value);
-                $(currentEntry).append(formattedProjectImage);
-            })
+        $.each(project.images, function(index, value) {
+            var formattedProjectImage =
+                HTMLprojectImage.replace(data, value);
+            $(currentEntry).append(formattedProjectImage);
         });
-    }
+    });
+}
 bio.display();
 work.display();
 projects.display();
