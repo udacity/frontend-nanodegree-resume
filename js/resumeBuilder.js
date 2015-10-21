52d1bb53631181262aa7b7a1645b264476e1b31d
Mark <markweig@gmail.com>
2015-10-21 16:43:54 -0400

    Add work, projects, bio, and education objects
    In preparation for Lesson 3, we had to remove previous code, it is commented out at the bottom, and have all of our objects for the resume in place.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,3 @@
-// From earlier exercises
-
-// $("#main").append("Mark Weigel");
-
-// var awesomeThoughts = 'I am Mark and I am AWESOME!';
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-// $("#main").append(funThoughts);
-
 var bio = {
 	"name" : "Mark Weigel",
 	"role" : "Web Developer",
@@ -25,37 +16,147 @@ var bio = {
 	]
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
-var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var projects = {
+	"projects": [
+		{
+			"title": "Project #",
+			"dates": "Fill-YEAR",
+			"description": "A cute little description",
+			"images": ["url1", "url2"]
+		},{
+			"title": "Project #",
+			"dates": "Fill-YEAR",
+			"description": "A cute little description",
+			"images": ["url1", "url2"]
+		},{
+			"title": "Project #",
+			"dates": "Fill-YEAR",
+			"description": "A cute little description",
+			"images": ["url1", "url2"]
+		}]
+};
 
-// These should be replaced with a couple of for loops?
-var formattedSkillsOne = HTMLskills.replace("%data%", bio.skills[1]);
-var formattedSkillsTwo = HTMLskills.replace("%data%", bio.skills[2]);
-var formattedSkillsThree = HTMLskills.replace("%data%", bio.skills[3]);
+var work = {
+	"jobs": [
+		{
+			"employer": "Amis Trattoria",
+			"title": "Server",
+			"location": "Philadelphia",
+			"dates": "July 2012 - Present",
+			"description": "You're going to have to be clever here."
+		},{
+			"employer": "South Philadelphia Tap Room",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},{
+			"employer": "Caribou Cafe",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},{
+			"employer": "Comedy Cellar",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},{
+			"employer": "Timberlodge Steakhouse",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},{
+			"employer": "Cub Foods",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		}],
+	"extracurricular": [
+		{
+			"name": "Vestals",
+			"description": ""
+		},{
+			"name": "Rattle Clack Entertainment",
+			"description": ""
+		}]
+};
+
+var education = {
+	"schools": [
+		{
+			"name" : "Normandale Community College",
+			"city" : "Bloomington",
+			"majors" : ["Engineering Prereqs"],
+			"graduation" : "Nope",
+			"dates" : "Fillin",
+			"courseInfo" : "Put your course info here"
+		},{
+			"name" : "University of Minnesota",
+			"city" : "Minneapolis",
+			"majors" : ["Liberal Arts"],
+			"dates": "Fillin",
+			"graduation" : "nope",
+			"courseInfo" : "Put your course info here"
+		},{
+			"name" : "Cooper High School",
+			"city" : "Plymouth",
+			"majors" : ["College Prep"],
+			"dates": "Fillin",
+			"graduation" : 1998,
+			"courseInfo" : "Put your course info here"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title" : "Front-End Nanodegree",
+			"school" : "Udacity",
+			"dates" : 2015,
+			"url" : "http://www.udacity.com/"
+		}
+	]
+};
 
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
-var formattedGithub = HTMLtwitter.replace("%data%", bio.contactInfo.github);
-var formattedBlog = HTMLblog.replace("%data%", bio.contactInfo.blog);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// I had a perfectly formatted header and they're making me ditch this code
 
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedGithub);
-$("#topContacts").append(formattedTwitter);
-$("#topContacts").append(formattedLocation);
 
-$("#header").append(formattedPic);
-$("#header").append(formattedMessage);
-$("#header").append(HTMLskillsStart);
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// var formattedPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
+// var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-$("#skills").append(formattedSkillsOne);
-$("#skills").append(formattedSkillsTwo);
-$("#skills").append(formattedSkillsThree);
+// // These should be replaced with a couple of for loops?
+// var formattedSkillsOne = HTMLskills.replace("%data%", bio.skills[0]);
+// var formattedSkillsTwo = HTMLskills.replace("%data%", bio.skills[1]);
+// var formattedSkillsThree = HTMLskills.replace("%data%", bio.skills[2]);
+
+
+// var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+// var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+// var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+// var formattedGithub = HTMLtwitter.replace("%data%", bio.contactInfo.github);
+// var formattedBlog = HTMLblog.replace("%data%", bio.contactInfo.blog);
+// var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+// $("#topContacts").append(formattedMobile);
+// $("#topContacts").append(formattedEmail);
+// $("#topContacts").append(formattedGithub);
+// $("#topContacts").append(formattedTwitter);
+// $("#topContacts").append(formattedLocation);
+
+// $("#header").append(formattedPic);
+// $("#header").append(formattedMessage);
+// $("#header").append(HTMLskillsStart);
+
+// $("#skills").append(formattedSkillsOne);
+// $("#skills").append(formattedSkillsTwo);
+// $("#skills").append(formattedSkillsThree);
