1fc557f21f47d448e678d5a68dd18174e2939b8e
egehrmann <eric.gehrmann@ge.com>
2015-10-28 17:54:41 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 /*
 $("#main").append("Eric Gehrmann");
 var awesomethoughts
-awesomethoughts = "I am Eric and I am AWESOME"
+awesomethoughts = "I am Eric and I am AWESOME!"
 console.log(awesomethoughts)
  */
