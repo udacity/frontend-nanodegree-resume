62e6550271ead30e42803748627f89d93af5c898
Gregg Strohkorb <greggstrohkorb@Greggs-MacBook-Pro.local>
2015-11-03 08:43:07 -0600

    Added JSONs to resumeBuilder.js, removed other code to resumeBuilderTEMP.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,117 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+// JSONs
+
+var bio = {
+	"name" : "Gregg Strohkorb",
+	"role" : "Data Scientist and Developer",
+	"contacts" :
+	  {
+		"mobile" : "785-221-9437",
+		"email" : "gregg@strohkorb.com",
+		"github" : "gstroh",
+		"twitter" : "@gstroh",
+		"location" : "Leawood, KS, USA"
+	  },
+	"welcomeMessage" : "Welcome",
+	"skills" : ["Data Analysis", "Applications Development", "Portfolio Management"],
+	"biopic" : "images/gregg.jpg"
+}
+
+var education = {
+	"schools": [
+	  {
+		"name": "The College of William and Mary",
+		"location": "Williamsburg, VA",
+		"degree": "B.S.",
+		"majors": "Biology",
+		"dates": 1977,
+		"url": "https://www.wm.edu/"
+	  },
+	  {
+		"name": "The College of William and Mary",
+		"location": "Williamsburg, VA",
+		"degree": "M.S.",
+		"majors": ["Applied Math", "Computer Science"],
+		"dates": 1981,
+		"url": "https://www.wm.edu/"
+	  },
+	  {
+		"name": "Thunderbird School of Global Management",
+		"location": "Glendale, AZ",
+		"degree": "MBA",
+		"majors": "International Finance",
+		"dates": 1987,
+		"url": "http://www.thunderbird.edu/"
+	  }
+	  ],
+	"onlineCourses": [
+	  {
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+	  },
+	  {
+		"title": "Version Control with GIT and GitHub",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+	  },
+	  {
+		"title": "JavaScript Basics Course",
+		"school": "Udacity",
+		"date": 2015,
+		"url": "https://www.udacity.com/course/javascript-basics--ud804"
+	  }
+	  ]
+}
+
+var work = {
+	"jobs": [
+	  {
+		"employer": "Guggenheim Investments",
+		"title": "Portfolio Manager",
+		"location": "Overland Park, KS",
+		"dates": "2006-Present",
+		"description": "Portfolio management, quantitative research, quantitative systems development"
+	  },
+	  {
+		"employer": "Denver Investment Advisors",
+		"title": "Senior Quantitative Analyst",
+		"location": "Denver, CO",
+		"dates": "1996-2006",
+		"description": "Quantitative research, applications development, stock selection modeling"
+	  },
+	  {
+		"employer": "Saudi Arabian Oil Company",
+		"title": "Supervisor of Hydrocarbon Planning Computer Systems",
+		"location": "Dhahran, Saudi Arabia",
+		"dates": "1988-1996",
+		"description": "Supervise software & hardware units, systems generated hydrocarbon income for Saudi Arabia ($ billions)"
+	  }
+	  ]
+}
+
+var projects = {
+	"projects": [
+	  {
+		"title": "Compass Applications",
+		"dates": "2015-Present",
+		"description": "Applications developed to support the Compass model of portfolio management.",
+		"images": ["url1", "url2", "url3"]
+	  },
+	  {
+		"title": "Curve Applications",
+		"dates": "2006-2009",
+		"description": "Applications developed to support the Curve model of portfolio management.",
+		"images": ["url1", "url2", "url3"]
+	  },
+	  {
+		"title": "Conversion of OSPAS planning and offtake systems from character-based flat files to windows-based SQL DB.",
+		"dates": "1992-1994",
+		"description": "Team leader for design of mission critical applications and DB design.",
+		"images": ["url1", "url2", "url3"]
+	  }
+	  ]
+}
+
