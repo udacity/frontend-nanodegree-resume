a76fb8f5070e4d7157e72992534c18152ebcc9ac
Benjamin Clarke <ben@theshipyard.com>
2015-11-06 07:24:43 -0500

    changed role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 $('#main').append('Benjamin Clarke');
 
 var formattedName = HTMLheaderName.replace('%data%', Benjamin Clarke);
-var formattedRole = HTMLheaderRole.replace('%data%', Developer);
+var formattedRole = HTMLheaderRole.replace('%data%', Person);
 
 $('header').append(formattedName);
 $('header').append(formattedRole);
