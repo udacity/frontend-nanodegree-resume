9613b0e8096169b94c35e9af21860c7f98b9e8e5
Gregg Strohkorb <greggstrohkorb@Greggs-MacBook-Pro.local>
2015-11-06 09:00:45 -0600

    End of Javascript Basic class, before finishing touches on resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -73,21 +73,21 @@ var work = {
 		"title": "Portfolio Manager",
 		"location": "Overland Park, KS",
 		"dates": "2006-Present",
-		"description": "Portfolio management, quantitative research, quantitative systems development"
+		"description": "Portfolio management, quantitative research, quantitative systems development."
 	  },
 	  {
 		"employer": "Denver Investment Advisors",
 		"title": "Senior Quantitative Analyst",
 		"location": "Denver, CO",
 		"dates": "1996-2006",
-		"description": "Quantitative research, applications development, stock selection modeling"
+		"description": "Quantitative research, applications development, stock selection modeling."
 	  },
 	  {
 		"employer": "Saudi Arabian Oil Company",
 		"title": "Supervisor of Hydrocarbon Planning Computer Systems",
 		"location": "Dhahran, Saudi Arabia",
 		"dates": "1988-1996",
-		"description": "Supervise software & hardware units, systems generated hydrocarbon income for Saudi Arabia ($ billions)"
+		"description": "Supervise software & hardware units, systems generated hydrocarbon income for Saudi Arabia ($ billions)."
 	  }
 	  ]
 }
@@ -98,20 +98,98 @@ var projects = {
 		"title": "Compass Applications",
 		"dates": "2015-Present",
 		"description": "Applications developed to support the Compass model of portfolio management.",
-		"images": ["url1", "url2", "url3"]
+		"images": ["images/compass-map-small.jpg"]
 	  },
 	  {
 		"title": "Curve Applications",
 		"dates": "2006-2009",
 		"description": "Applications developed to support the Curve model of portfolio management.",
-		"images": ["url1", "url2", "url3"]
+		"images": ["images/R2000.jpg"]
 	  },
 	  {
 		"title": "Conversion of OSPAS planning and offtake systems from character-based flat files to windows-based SQL DB.",
 		"dates": "1992-1994",
 		"description": "Team leader for design of mission critical applications and DB design.",
-		"images": ["url1", "url2", "url3"]
+		"images": []
 	  }
 	  ]
 }
+
+if (bio.skills.length > 0) {
+  // header
+  $("#header").append(HTMLskillsStart);
+  // skills
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+}
+
+function DisplayWork() {
+  for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	// Concatenate the two strings.
+	var formattedEmployerAndTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerAndTitle);
+
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	$(".work-entry:last").append(formattedLocation);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+//	console.log(work.jobs[job].employer);
+  }
+}
+
+DisplayWork();
+
+$(document).click(function(loc) {
+	//var e = jQuery.Event("click");\
+	//console.log(loc);
+	logClicks(loc.pageX, loc.pageY);
+});
+
+$("#main").append(internationalizeButton);
+
+function inName(oldName) {
+	var finalName = oldName;
+    var names = oldName.trim().split(" ");
+    names[1] = names[1].toUpperCase();
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    finalName = names.join(" ");
+    return finalName;
+}
+
+projects.display = function () {
+	  	for (project in projects.projects) {
+	  		// header
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			$(".project-entry:last").append(formattedTitle);
+
+			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			$(".project-entry:last").append(formattedDates);
+
+			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			$(".project-entry:last").append(formattedDescription);
+
+			if (projects.projects[project].images.length > 0) {
+    		  for (image in projects.projects[project].images) {
+        		var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+    		  }
+    		}
+    	}
+};
+
+projects.display();
+
+// map
+$("#mapDiv").append(googleMap);
 
