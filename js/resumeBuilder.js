57fd661cf10813fb68e6ace052f184b54140fd26
George Fischer <geosynchronous@mac.com>
2015-12-01 21:05:18 -0700

    End of Javascript Lesson 1 Code Assignment
    Setting up resume data structure for bio, work, projects and education
    categories.  I still have to fill data in for the place holders.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,156 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// End of Javascript Lesson 1 Code Assignment
+
+
+var bio = {
+	"name": "George Fischer",
+	"role": "Web Developer",
+	"contacts":
+		{
+			"email": "geosynchronous@mac.com",
+			"mobile": "801-555-5555",
+			"github": "https://Geosynchronous.github.io",
+			"google+": "geo-google+",
+			"linkedin": "geo-linkedin",
+			"twitter": "geo-twitter"
+			"location": "Salt Lake City"
+		},
+	"welcome": "Welcome to My Resume",
+	"skills":[
+			"Front-End Web Development",
+			"Courseware Development",
+			"Physics",
+			"Electronics",
+			"Instrumentation"
+	],
+	"bioPic": "images/me.jpg"
+}
+
+// Examples of how to print in browser window.
+// Remove later.
+
+// $("#main").append(bio["name"]);
+// $("#main").append(bio.role);
+
+
+var education = {
+	"schools": [
+		{
+			"name": "University of Utah",
+			"city": "Salt Lake City, UT, US",
+			"degree": "BS",
+			"major": "Biology",
+			"minor": "Chemistry",
+			"gradYear": "1977",
+			"url": "http://www.utah.edu/"
+		},
+		{
+			"name": "University of Utah",
+			"city": "Salt Lake City, UT, US",
+			"degree": "MS",
+			"major": "Physics",
+			"minor": "Instrumentation",
+			"gradYear": "1980",
+			"url": "http://www.utah.edu/"
+		},
+		{
+			"name": "University of Utah",
+			"city": "Salt Lake City, UT, US",
+			"degree": "Education Certification",
+			"major": "Secondary School Instructor",
+			"minor": "",
+			"gradYear": "1984",
+			"url": "http://www.utah.edu/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-End Web Development",
+			"school": "Udacity",
+			"dates": "2015",
+			"url": "http://www.udacity.com/courses/all"
+		}
+	]
+}
+
+// Remove later.
+// $("#main").append(education.schools[0].major);
+
+
+var work = {
+	"jobs":[
+		{
+			"employer": "",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},
+		{
+			"employer": "",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		},
+		{
+			"employer": "",
+			"title": "",
+			"location": "",
+			"dates": "",
+			"description": ""
+		}
+	]
+}
+
+var projects = {
+	"projects":[
+		{
+			"title": "",
+			"dates": "",
+			"description": "",
+			"images":[
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				}
+			]
+		},
+		{
+			"title": "",
+			"dates": "",
+			"description": "",
+			"images":[
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				}
+			]
+		},
+		{
+			"title": "",
+			"dates": "",
+			"description": "",
+			"images":[
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				},
+				{
+					"imageURL":""
+				}
+			]
+		}
+	]
+}
