f70ed141f7787437869f8699702eb28c5339718b
George Fischer <geosynchronous@mac.com>
2015-12-13 19:01:58 -0700

    Starting to display resume
    Been a while since I could work on this, need to nail down a working
    version now.  Worked first quarter of Lesson 2.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,26 +4,25 @@
 var bio = {
 	"name": "George Fischer",
 	"role": "Web Developer",
+	"welcomeMessage": "Welcome to My Resume",
 	"contacts":
 		{
-			"email": "geosynchronous@mac.com",
 			"mobile": "801-555-5555",
+			"email": "geosynchronous@mac.com",
 			"github": "https://Geosynchronous.github.io",
-			"google+": "geo-google+",
-			"linkedin": "geo-linkedin",
-			"twitter": "geo-twitter"
+			"twitter": "geo-twitter",
 			"location": "Salt Lake City"
 		},
-	"welcome": "Welcome to My Resume",
 	"skills":[
-			"Front-End Web Development",
-			"Courseware Development",
+			"Front-End Web",
+			"Courseware",
 			"Physics",
 			"Electronics",
 			"Instrumentation"
 	],
 	"bioPic": "images/me.jpg"
 }
+
 
 // Examples of how to print in browser window.
 // Remove later.
@@ -36,29 +35,26 @@ var education = {
 	"schools": [
 		{
 			"name": "University of Utah",
-			"city": "Salt Lake City, UT, US",
+			"location": "Salt Lake City, UT, US",
 			"degree": "BS",
-			"major": "Biology",
-			"minor": "Chemistry",
-			"gradYear": "1977",
+			"majors": ["Biology"],
+			"dates": 1977,
 			"url": "http://www.utah.edu/"
 		},
 		{
 			"name": "University of Utah",
 			"city": "Salt Lake City, UT, US",
 			"degree": "MS",
-			"major": "Physics",
-			"minor": "Instrumentation",
-			"gradYear": "1980",
+			"majors": ["Physics"],
+			"dates": 1980,
 			"url": "http://www.utah.edu/"
 		},
 		{
 			"name": "University of Utah",
 			"city": "Salt Lake City, UT, US",
 			"degree": "Education Certification",
-			"major": "Secondary School Instructor",
-			"minor": "",
-			"gradYear": "1984",
+			"majors": ["Secondary School Instructor"],
+			"dates": 1984,
 			"url": "http://www.utah.edu/"
 		}
 	],
@@ -66,7 +62,7 @@ var education = {
 		{
 			"title": "Front-End Web Development",
 			"school": "Udacity",
-			"dates": "2015",
+			"dates": 2015,
 			"url": "http://www.udacity.com/courses/all"
 		}
 	]
@@ -79,25 +75,88 @@ var education = {
 var work = {
 	"jobs":[
 		{
-			"employer": "",
-			"title": "",
-			"location": "",
-			"dates": "",
-			"description": ""
+			"employer": "City Academy Charter School",
+			"title": "Engineering Teacher",
+			"location": "Salt Lake City, Utah",
+			"dates": "2013 - 2014",
+			"description": "Taught Digital Electronics & Engineering Design with 3D CAD"
 		},
 		{
-			"employer": "",
-			"title": "",
-			"location": "",
-			"dates": "",
-			"description": ""
+			"employer": "Home",
+			"title": "Domestic Engineer",
+			"location": "Salem, Utah",
+			"dates": "1995 - 2013",
+			"description": "Did the Mr. Mom thing in a semi-rural location."
 		},
 		{
-			"employer": "",
-			"title": "",
-			"location": "",
-			"dates": "",
-			"description": ""
+			"employer": "Math Department, University of Utah",
+			"title": "LAN Coordinator",
+			"location": "Salt Lake City, Utah",
+			"dates": "1993 - 1995",
+			"description": "Assisted Network Administrator serving users & infrastructure."
+		},
+		{
+			"employer": "Wasatch Educations Systems",
+			"title": "Courseware Developer",
+			"location": "Salt Lake City, Utah",
+			"dates": "1986 - 1993",
+			"description": "Developed physics and adult education courseware."
+		},
+		{
+			"employer": "Park City High School",
+			"title": "Physics & Biology Teacher",
+			"location": "Park City, Utah",
+			"dates": "1985 - 1986",
+			"description": "Taught Phyics and Biology."
+		},
+		{
+			"employer": "Wasatch Education Systems",
+			"title": "Programmer",
+			"location": "Salt Lake City, Utah",
+			"dates": "1984 - 1985",
+			"description": "Programmed biology courseware and simulations."
+		},
+		{
+			"employer": "National Solar Observatory",
+			"title": "Physics Intern",
+			"location": "Sunspot, New Mexico",
+			"dates": "Summer 1984",
+			"description": "Designed solar tracking data loggers for worldwide survey."
+		},
+		{
+			"employer": "Quartex",
+			"title": "Research Engineer",
+			"location": "Salt Lake City, Utah",
+			"dates": "1983",
+			"description": "R&D for crystal oscillator technologies."
+		},
+		{
+			"employer": "Eaton Kenway",
+			"title": "Electrical Engineer",
+			"location": "Salt Lake City, Utah",
+			"dates": "1982",
+			"description": "Storage-retrieval robot development and configuration."
+		},
+		{
+			"employer": "Physics, University of Utah",
+			"title": "Electrical Engineer",
+			"location": "Salt Lake City, Utah",
+			"dates": "1981",
+			"description": "Finished Cosmic Ray Research work on Fly's Eye Project."
+		},
+		{
+			"employer": "Physics, University of Utah",
+			"title": "Teaching Assistant",
+			"location": "Salt Lake City, Utah",
+			"dates": "1978 - 1981",
+			"description": "Taught graduate level analog & digital electronics. and computer interfacing labs."
+		},
+		{
+			"employer": "University of Utah",
+			"title": "Research Assistant",
+			"location": "Salt Lake City, Utah",
+			"dates": "1974 - 1980",
+			"description": "Worked in chemistry, molecular biology, biochemistry and physics labs."
 		}
 	]
 }
@@ -130,3 +189,40 @@ var projects = {
 		}
 	]
 }
+
+// This code added from beginning of Lesson 2
+
+$("#header").append(HTMLheaderName);
+
+if(bio.skills.length > 0) {
+
+  $("#header").append(HTMLskillsStart);
+
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+  $("#skills").append(formattedSkill);
+}
+
+
+// Begin work on displaying category info to resume page
+
+$("#workExperience").append(HTMLworkStart);
+
+for (job in work.jobs) {
+
+ 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+}
+
+    // for(var i = 0; i < 9; i++) { console.log(i); }
+    //   console.log(HTMLskillsStart);
