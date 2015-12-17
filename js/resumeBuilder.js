45b999d1aee9531ec0db64db201afe468b7876ba
George Fischer <geosynchronous@mac.com>
2015-12-16 17:41:54 -0700

    Display Projects
    followed course instructions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -77,86 +77,86 @@ var work = {
 		{
 			"employer": "City Academy Charter School",
 			"title": "Engineering Teacher",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "2013 - 2014",
-			"description": "Taught Digital Electronics & Engineering Design with 3D CAD"
+			"description": "Taught Digital Electronics Course & Engineering Design Courses with AutodDesk Inventor 3D-CAD."
 		},
 		{
 			"employer": "Home",
 			"title": "Domestic Engineer",
-			"location": "Salem, Utah",
+			"location": "Salem, UT",
 			"dates": "1995 - 2013",
-			"description": "Did the Mr. Mom thing in a semi-rural location."
+			"description": "Did the Mr. Mom thing in a semi-rural location.  Supplemented public education with additional home schooling."
 		},
 		{
 			"employer": "Math Department, University of Utah",
 			"title": "LAN Coordinator",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1993 - 1995",
-			"description": "Assisted Network Administrator serving users & infrastructure."
+			"description": "Assisted Network Administrator serving departmental users & maintaining netowrk infrastructure."
 		},
 		{
 			"employer": "Wasatch Educations Systems",
 			"title": "Courseware Developer",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1986 - 1993",
-			"description": "Developed physics and adult education courseware."
+			"description": "Developed interactive mastery physics and adult education courseware. Developed simulations and teacher guides."
 		},
 		{
 			"employer": "Park City High School",
 			"title": "Physics & Biology Teacher",
-			"location": "Park City, Utah",
+			"location": "Park City, UT",
 			"dates": "1985 - 1986",
-			"description": "Taught Phyics and Biology."
+			"description": "Taught Physics and Biology, with an emphasis on labwork."
 		},
 		{
 			"employer": "Wasatch Education Systems",
 			"title": "Programmer",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1984 - 1985",
 			"description": "Programmed biology courseware and simulations."
 		},
 		{
 			"employer": "National Solar Observatory",
 			"title": "Physics Intern",
-			"location": "Sunspot, New Mexico",
+			"location": "Sunspot, NM",
 			"dates": "Summer 1984",
-			"description": "Designed solar tracking data loggers for worldwide survey."
+			"description": "Designed and tested rugged stand-alone solar tracking data loggers for worldwide remote mountain-top surveys."
 		},
 		{
 			"employer": "Quartex",
 			"title": "Research Engineer",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1983",
-			"description": "R&D for crystal oscillator technologies."
+			"description": "R&D for crystal oscillator technologies, used in lab weighing scales. Eliminated reproducibility production problems."
 		},
 		{
 			"employer": "Eaton Kenway",
 			"title": "Electrical Engineer",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1982",
 			"description": "Storage-retrieval robot development and configuration."
 		},
 		{
 			"employer": "Physics, University of Utah",
 			"title": "Electrical Engineer",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1981",
-			"description": "Finished Cosmic Ray Research work on Fly's Eye Project."
+			"description": "Finished Cosmic Ray Research work on Fly's Eye Project. A data acquisition array of 80 large UV telescopes."
 		},
 		{
 			"employer": "Physics, University of Utah",
 			"title": "Teaching Assistant",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1978 - 1981",
-			"description": "Taught graduate level analog & digital electronics. and computer interfacing labs."
+			"description": "Taught graduate level analog & digital electronics, and computer interfacing labs."
 		},
 		{
 			"employer": "University of Utah",
 			"title": "Research Assistant",
-			"location": "Salt Lake City, Utah",
+			"location": "Salt Lake City, UT",
 			"dates": "1974 - 1980",
-			"description": "Worked in chemistry, molecular biology, biochemistry and physics labs."
+			"description": "As a student, worked in chemistry, molecular biology, biochemistry and physics labs."
 		}
 	]
 }
@@ -164,33 +164,33 @@ var work = {
 var projects = {
 	"projects":[
 		{
-			"title": "",
-			"dates": "",
-			"description": "",
+			"title": "Portfolio",
+			"dates": "Oct-Nov 2015",
+			"description": "Udacity Nanodegree Front-End Web Design project to create a website portfolio.",
 			"images":[
-				"https://geosynchronous/github.io"
+				"images/me.jpg"
 			]
 		},
 		{
-			"title": "",
-			"dates": "",
-			"description": "",
+			"title": "Healtcare Website",
+			"dates": "Jan-March 2009",
+			"description": "Created a set of interactive webpages for Full Circle Care, and Naturopathic Clinic.",
 			"images":[
-				"https://geosynchronous/github.io"
+				"images/me.jpg"
 			]
 		},
 		{
-			"title": "",
-			"dates": "",
-			"description": "",
+			"title": "Physics Courseware",
+			"dates": "1989-2000",
+			"description": "Designed and developed an interactive physics course on sound, with real-world simulations and a simulated lab with measurirng instruments.",
 			"images":[
-				"https://geosynchronous/github.io"
+				"images/me.jpg"
 			]
 		}
 	]
 }
 
-// This code added from beginning of Lesson 2
+// This code added from beginning of Lesson 2 and still needs work
 
 $("#header").append(HTMLheaderName);
 
@@ -211,18 +211,66 @@ if(bio.skills.length > 0) {
 }
 
 
-// Begin work on displaying category info to resume page
+// Display Work Experience Category and Attributes
 
-$("#workExperience").append(HTMLworkStart);
+function displayWork() {
 
-for (job in work.jobs) {
 
- 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-	$(".work-entry:last").append(formattedEmployerTitle);
+	for (job in work.jobs) {
+
+		$("#workExperience").append(HTMLworkStart);
+
+	 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
 }
 
-    // for(var i = 0; i < 9; i++) { console.log(i); }
-    //   console.log(HTMLskillsStart);
+displayWork();
+
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.projects[project].images.length>0) {
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+
+	}
+}
+
+projects.display();
+
+
+// Logs user mouse click positions.
+
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+  	logClicks(x,y);
+
+});
