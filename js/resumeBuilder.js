6fa42e33cf635f85d19f6c725be1b002df5e5a84
llwang8 <wangxbl56@gmail.com>
2016-01-10 22:30:54 -0500

    completed JSON data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,177 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append(["Li Wang"]);
+
+ //var awesomeThoughts, funThoughts;
+ //awesomeThoughts = "I am Li Wang and I am awesome!";
+ //funThoughts = awesomeThoughts.replace("awesome", "FUN");
+ //console.log(awesomeThoughts);
+ //$("#main").append(funThoughts);
+ var bio = {
+ 	"name": "Li Wang",
+ 	"role": "Web Developer",
+ 	"welcomeMessage":"Build An Application And Make a Difference!",
+ 	"contacts": {
+ 		"email": "wangxbl56@gmail.com",
+ 		"github": "https://github.com/llwang8",
+ 		"twitter": "https://twitter.com/Lwang2015"
+ 	},
+ 	"skills": ["Ruby on Rails", "JavaScript", "SQL", "jQuery", "PostgreSQL", "HTML", "CSS"]
+
+ };
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Inlingua Princeton",
+			"title": "Voice Talent and Translator",
+			"location": "Princeton, NJ",
+			"dates": "2006-2015",
+			"description": "Record Chinese and verify scripts for clinical trial projects conducted in China. Translator for public schools to help immigrant families."
+		},
+		{
+			"employer": "CherryValley Studio",
+			"title": "Founder",
+			"location": "Princeton, NJ",
+			"dates": "2002-2015",
+			"description": "Operated studio to provide comprehensive quality DVD services to businesses and families. Managed all business aspects including accounting, taxation, marketing and website."
+		},
+		{
+			"employer": "C&A and Associates",
+			"title": "Associate Programmer",
+			"location": "Somerville, NJ",
+			"dates": "2000-2001",
+			"description": "Participate in the development PPM (people profile management), Built Essistme (v1) application, customized CNCv1 (news engine) for various clients using ASP, Javascript, SQL, HTML/DHTML, CSS, Visual  Interdev."
+		},
+		{
+			"employer": "PILLSBURY, MADISON & SUTRO LLP",
+			"title": "Financial Analyst",
+			"location": "San Francisco, CA",
+			"dates": "1998",
+			"description": "Management reporting, forecasting, budgeting and financial analysis."
+		},
+		{
+			"employer": "Beckman Coulter",
+			"title": "Financial Analyst",
+			"location": "Palo Alto, CA",
+			"dates": "1996-1998",
+			"description": "Management reporting, forecasting, budgeting and analysis."
+		},
+		{
+			"employer": "Linus Pauling Institute",
+			"title": "Controller",
+			"location": "Palo Alto, CA",
+			"dates": "1995-1996",
+			"description": "Management reporting, budgeting, analysis, accounting, taxation and purchasing."
+		}
+
+	]
+
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "ramp_projects",
+			"dates": "2015",
+			"description": "Ruby projects - Blackjack, game-of-life, league-point-calculator, restaurant-health-check, ...",
+			"images":[]
+		},
+		{
+			"title": "my_app",
+			"dates": "2015",
+			"description": "Rails app - The Real ResourcesXchange",
+			"images":[]	
+		},
+		{
+			"title": "send_app",
+			"dates": "2015",
+			"description": "Rails app - Send twilio message to pre-certify number",
+			"images":[]
+		},
+		{
+			"title": "moviestore",
+			"dates": "2015",
+			"description": "Rails app - rails-learning-projects-sitepoint-moviestore",
+			"images":[]
+		},
+		{
+			"title": "blog2",
+			"dates": "2015",
+			"description": "Rails app - Rails-learning-projects-guide.rubyonrails.org-blog",
+			"images":[]
+		}
+
+	]
+}
+
+var education = {
+	"schools": [
+	{
+		"name": "Startup Institute",
+		"city": "New York, NY",
+		"degree": "Certificate of Achievement",
+		"major": "Web Development",
+		"dates": "2015",
+		"url": "https://www.startupinstitute.com"
+	},
+	{
+		"name": "Chubb Institute",
+		"city": "Islin, NJ",
+		"degree": "Certificate of Achievement",
+		"major": "Programming and Web Development",
+		"dates": "1999",
+		"url": "http://www.chubb-computer-institute.org/index.html"
+	},
+	{
+		"name": "Bryant University",
+		"city": "Smithfield, RI",
+		"degree": "MBA",
+		"major": "Accounting",
+		"dates": "1992-1993",
+		"url": "http://www.bryant.edu"
+	},
+	{
+		"name": "Nankai University",
+		"city": "Tianjing, P.R. China",
+		"degree": "B.S.",
+		"major": "Biology",
+		"dates": "1984-1988",
+		"url": "http://www.nankai.edu.cn"
+	}
+	],
+
+	"Online Course": [
+		{
+			"title": "JavaScript",
+			"school": "W3School",
+			"dates": "December 2015",
+			"url": "http://www.w3schools.com/js/default.asp"
+		},
+		{
+			"title": "HTML",
+			"school": "W3School",
+			"dates": "January 2016",
+			"url": "http://www.w3schools.com/html/default.asp"
+		},
+		{
+			"title": "JQuery",
+			"school": "W3School",
+			"dates": "January 2016",
+			"url": "http://www.w3schools.com/jquery/default.asp"
+		},
+		{
+			"title": "JavaScript Basics - Resume Project",
+			"school": "Udacity.com",
+			"dates": "January 2016",
+			"url": "https://www.udacity.com/course/viewer#!/c-ud804/l-1930528550/e-1935058561/m-2948908664"
+		}
+
+	]
+
+};
+
+
+
+
