ce55082c547961763a3c13d17260559a766aede7
devel <systemdevcloud@live.co.uk>
2016-01-19 22:48:20 +0000

    added the work and employment objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,33 +2,109 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var skills = ['javascript', "css", "HTML", "PHP", "drupal"];
+var skills = ["javascript", "css", "HTML", "PHP", "drupal"];
 
 // javascript object
 var bio = {
 "name" :  "Alexander Brown",
 "role" : "Web Developer",
+
+"contacts" : {
+"mobile" : "07793749873",
 "email" : "test@test.com",
 "github" :   "datadev23",
-"picture" : "images.jpg",
+"location": "Brighton",
+
 "welcome" : "Hello how are you",
-"skills" : skills
+"location" : "Hove"
+
+} ,
+"skills" : skills,
+"picture" : "images/fry.jpg",
+}
+
+var work ={
+"employer" : {
+"position" : "Web Developer",
+"employer" : "me",
+"Years of attendance" : "2014",
+"company" : "TUI"
+}
+
+};
+
+
+var education = {
+    "schools": [
+        {
+            "name": "city University",
+            "city": "London",
+            "major": "Computer Science",
+            "graduation": "2012"
+
+        },
+
+        {
+            "name": "Essex University",
+            "city": "Colchester",
+            "major": "Computer Science",
+            "graduation": "2013"
+        }
+
+
+    ]
+
+
 };
 
 
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLbioPic = '<img src="%data%" class="biopic">';
 
 var formattedName =HTMLheaderName.replace("%data%", bio.name) ;
 var formattedRole =HTMLheaderRole.replace("%data%", bio.role) ;
-var formatedEmail= HTMLemail.replace("%data%",bio.email);
-var formatedGithub = HTMLgithub.replace('%data%', bio.github);
-var formattedskills = HTMLskillsStart.replace('%data%', bio.skills);
-$("#header").prepend(formattedName);
+
+
+
+//var formattedHTMLcontactGeneric= HTMLcontactGeneric.replace("%data%",bio.email);
+var formatedMobile = HTMLmobile.replace("%data%",bio.contacts['mobile']);
+var formatedEmail= HTMLemail.replace("%data%",bio.contacts['email']);
+var formatedGithub = HTMLgithub.replace('%data%', bio.contacts['github']);
+var formattedLocation = HTMLlocation.replace('%data%', bio.contacts['location']);
+var formattedbioPic = HTMLlocation.replace('%data%', bio.picture);
+
+var formattedskills = HTMLskills.replace('%data%', bio.skills);
+
+// Employmet details
+var HTMLschoolName = '<a href="#">%data%';
+var workEmploymentName = HTMLschoolName.replace('%data%', education.schools['name']);
+
+
+var HTMLworkEmployer = '<a href="#">%data%';
+var formattedworkEmployer = HTMLworkEmployer.replace('%data%', work.employer['company']);
+
+//alert(work.employer['position']);
+
+
+$("#header").append(formattedName);
 $("#header").append(formattedRole);
-$("#header").append(formatedEmail);
-$('#header').append(formatedGithub);
-$("#header").append(formattedskills);
+
+
+// Bio info
+$("#topContacts").append(formatedMobile);
+$("#topContacts").append(formatedEmail);
+$("#topContacts").append(formatedGithub);
+$("#topContacts").append(formattedLocation);
+$('#topContacts').prepend(formatedGithub);
+$("#topContacts").append(HTMLskillsStart);
+$("#topContacts").append(formattedskills);
+
+
