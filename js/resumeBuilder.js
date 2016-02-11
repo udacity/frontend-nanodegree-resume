337c53e2430eaf1ce1cf9ba15005d520e1c001e8
Tiffany Stallings <je.deviens.moi@gmail.com>
2016-02-11 10:56:24 -0500

    Add populated JSON objects to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,100 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs": [{
+		"employer": "Trader Joe's",
+		"title": "Crew Member",
+		"location": "Marietta, GA",
+		"dates": "September 2012 - Present",
+		"description": "Responsible for creating effective signage and artwork \
+			for the store, and for ordering product in different sections. Maintained \
+			the company's high standards for customer service and interaction while \
+			performing all tasks quickly and efficiently."
+		}, {
+		"employer": "The Clubhouse at Hackers",
+		"title": "Line Cook, Cake Decorator",
+		"location": "Statesboro, GA",
+		"dates": "August 2011 - May 2012",
+		"description": "Baked and decorated birthday cakes for on-site parties. \
+			Cooperated with team-mates to run the line and prepare ingredients for \
+			upcoming shifts."
+		}, {
+		"employer": "The Foolish Frog",
+		"title": "Line Cook",
+		"location": "Beaufort, South Carolina",
+		"dates": "November 2009 - June 2011",
+		"description": "Became an expert in preparing salads, desserts and \
+			griddle-cooked items. Defined new standards for the salad station and \
+			administered training to fellow employees on said standards."
+		}]
+}
+
+var projects = {
+	"projects": [{
+		"title": "Place Holder",
+		"dates": "February 2016",
+		"description": "",
+		"images": [
+			"http://placehold.it/300x200",
+			"http://placehold.it/300x200"]
+		}, {
+		"title": "Place Holder",
+		"dates": "February 2016",
+		"description": "Lorem ipsum dolor sit amet.",
+		"images": [
+			"http://placehold.it/300x200",
+			"http://placehold.it/300x200"]
+		}, {
+		"title": "Place Holder",
+		"dates": "February 2016",
+		"description": "Lorem ipsum dolor sit amet.",
+		"images": [
+			"http://placehold.it/300x200",
+			"http://placehold.it/300x200"]
+	}]
+}
+
+var bio = {
+	"name": "Tiffany Stallings",
+	"role": "Front End Web Developer",
+	"welcomeMessage": "Lorem ipsum dolor sit amet.",
+	"contacts": {
+		"mobile": "843-592-6095",
+		"email": "tiffany.c.stallings@gmail.com",
+		"github": "tiffanystallings",
+		"twitter": "@ZameenaRaqs",
+		"location": "Marietta, GA"
+	}
+}
+
+var education = {
+	"schools": [{
+		"name": "Pennsylvania Culinary Institute (Closed 2012)",
+		"location": "Pittsburgh, Pennsylvania",
+		"degree": "Associate of Specialized Technology",
+		"major": "Culinary Arts",
+		"dates": "July 2007 - October 2008",
+		"url": "https://en.wikipedia.org/wiki/Le_Cordon_Bleu_Institute_of_Culinary_Arts_in_Pittsburgh"
+	}],
+
+	"onlineCourses": [{
+		"title": "Front End Web Developer Nanodegree",
+		"school": "Udacity",
+		"dates": "February 2016 - In Progress",
+		"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}, {
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"dates": "February 2016",
+		"url": "https://www.udacity.com/courses/ud304"
+		}, {
+		"title": "JavaScript Basics",
+		"school": "Udacity",
+		"dates": "February 2016",
+		"url": "https://www.udacity.com/courses/ud804"
+		}, {
+		"title": "HTML, CSS, JavaScript, and Python",
+		"school": "CodeCademy",
+		"dates": "January 2015 - April 2015",
+		"url": "https://www.codecademy.com/nysari"
+	}]
+
+}
