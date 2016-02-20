87d99438cea9528b9220210e892c398dec712f95
gameboyA <rubygba@foxmail.com>
2016-02-20 16:41:35 +0800

    l1 string JSON DOM

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var education = {
+  "schools": [
+    {
+      "name": "Southeastern University",
+      "location": "Nanjing",
+      "degree": "BA",
+      "dates": 2010,
+      "url": "http://example.com"
+    },
+    {
+      "name": "Xiang Fan No.3 High School",
+    }
+  ]
+},
+  "skills": [
+    {
+      "title": "Program Languages",
+      "name": ["C++", "JavaScript", "HTML", "CSS"],
+      "level": "Familiar"
+    }
+  ]
+}
