da9a16c6bab204a66f91fafb49d9dedc7ebb5f91
gameboyA <rubygba@foxmail.com>
2016-02-21 11:02:50 +0800

    l1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
+var role = "Web Developer";
+
+var formattedRole = HTMLheaderRole.replace("%data%", name); //自定义Role规格化文本
+var formattedName = HTMLheaderName.replace("%data%", role);
+$("#header").prepend(formattedRole); //jQ插入规格化文本
+$("#header").prepend(formattedName);
+
+var bio = {
+  "name": "Game Boy A",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "0710-XXXX",
+    "email": "XXX@XXX",
+    "github": "rubygba",
+    "location": "China"
+  },
+  "welcomeMessage": "abc bb bb bb bb bb bla bla bla.",
+  "skills": [
+    "s1", "s2", "s3", "s4", "s5"
+  ],
+  "bioPic": "images/fry.jpg"
+}
+
 var education = {
   "schools": [
     {
@@ -10,8 +33,7 @@ var education = {
     {
       "name": "Xiang Fan No.3 High School",
     }
-  ]
-},
+  ],
   "skills": [
     {
       "title": "Program Languages",
