db3a5d7d777568f6647d6e62ba0edc458c5687f1
dmitryls <dmitryls@gmail.com>
2016-02-26 17:45:33 +0200

    Added files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,70 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs" : [
+		{
+			"employer" : "UBS",
+			"title" : "PM",
+			"location" : "Kiev",
+			"dates" : "2011-2016"
+		},
+		{
+			"employer" : "ADIA",
+			"title" : "PgM",
+			"location" : "Abu Dhabi",
+			"dates" : "2016-2019"
+		}
+
+	]
+};
+
+var projects = {
+	"projects" : [
+	{
+		"title" : "DMG",
+		"dates" : "2011-2013",
+		"description" : "DMG gg",
+		"images" : ["url1","url2","url3"]
+	}
+	]
+}
+
+var bio = {
+	"name" : "Dmytro",
+	"role" : "welcome",
+	"contacts" : {
+		"phone" : "093 2535326",
+		"email" : "dmitryls@gmail.com"
+	},
+	"skills" : ["Project Management", "Agile", "Scrum", "Budget Management"]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "DTL",
+			"location" : "Kiev",
+			"majors" : ["Programming", "Algebra" , "Physics"],
+			"dates" : "96-98",
+			"url" : "http://dtl.com"
+		},
+		{
+			"name" : "KPI",
+			"location" : "Kiev",
+			"majors" : ["High math", "Networks" , "War"],
+			"dates" : "98-04",
+			"url" : "http://kpi.com"
+		}
+	],
+	"onlineCources" : [
+		{
+			"title":"Howto learn",
+			"school" : "Coursera",
+			"dates" : "2016"
+		},
+		{
+			"title" : "Javascript",
+			"school" : "Udacity",
+			"dates": "2016"
+		}
+	]
+
+}
