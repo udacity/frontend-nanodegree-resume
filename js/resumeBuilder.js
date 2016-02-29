1677ac1cf1be636d26f9700cce569bbc30964e6e
Dan Gallegos <gallegos20@gmail.com>
2016-02-28 20:16:02 -0500

    Changed append input at line 18.
    changed .append(skills.length) to .append(skills)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,18 @@
 //$("#main").append("Daniel");
 // [string].replace([old],[new])
-//Declaring variables at the top
-var role = "Quality Engineer, Scrum Master";
-var name = "Daniel Gallegos";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 //Adding to text to 'main'
 //$("#main").append(funThoughts);
 
 //Adding Name & Job title to header
+var role = "Quality Engineer, Scrum Master";
+var name = "Daniel Gallegos";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+//Adding Skills to the Skills selection
+var skills = ["Agile Development", "Scrum Methodology", "Visual Studio 2015", "JavaScript", "GitHub"];
+$("#main").append(skills);
