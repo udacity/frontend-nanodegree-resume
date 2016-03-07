473e04244797141ccd0ad40bcf643bf7e99653ae
gallegos20 <gallegos20@gmail.com>
2016-03-06 22:38:58 -0500

    Problem Set 1
    JS Basics - Problem Set 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,65 +1,81 @@
-
-//generic variables here
-var contact = "Here is how to reach me :p";
-var name = "Daniel Gallegos";
-var picture = "images/fry.jpg";
-var role = "Quality Engineer & Scrum Master";
-var skills = ["Agile Development", "Scrum Methodology", "Visual Studio 2015", "JavaScript", "GitHub"];
-var welcome = "Hey!  Thanks for checking out my Resume!  This is actually the first project I've taken on, it is written in JavaScript, using jQuery and of course HTML and CSS.  Thanks for checking it out!";
-
-//formatted variables go here
-var formattedContact = HTMLcontactGeneric.replace("%data%", contact);
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedPicture = HTMLbioPic.replace("%data%", picture);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-var formattedSkills = HTMLskills.replace("%data%", skills.join(", "));
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", welcome);
-
-
 // Object Literal Notion below for 'bio'
-
 var bio = {
-  "name": name,
-  "role": role,
-  "contactinfo": {
+  "name": "Daniel Gallegos",
+  "role": "Quality Engineer",
+  "contacts": {
     "mobile": "716-514-3622",
     "email": "gallegos20@gmail.com",
     "github": "gallegos20",
     "twitter": "djg35",
     "locatation": "Buffalo, NY"
   },
-  "picture": picture,
-  "welcome": welcome,
-  "skills": skills
-};
-//
-// $("#main").append(bio.name);
+  "welcomeMessage": "Welcome to my biography!",
+  "skills": ["Agile Development", "Scrum Methodology", "Visual Studio 2015", "JavaScript", "GitHub"]
+}
 
-bio.city = "Buffalo";
-// bio["email"] = "gallegos20@gmail.com";
-var work = {};
-work["title"] = "Quality Engineer";
-work["dates"] = "February 2015 - Current";
-work["city"] = "Buffalo";
+var education = {
+  "schools": [
+    {
+      "name": "University at Buffalo",
+      "location": "Buffalo",
+      "degree": "Business Administration",
+      "majors": ["Finance"],
+      "dates": "May 2010",
+      "url": "http://www.lew-port.com"
+    },
+    {
+      "name": "Lewiston Porter High School",
+      "location": "Porter",
+      "degree": "High School Diplolma",
+      "majors": ["N/A"],
+      "dates": "June 2004",
+      "url": "http://www.lew-port.com"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JavaScript Basics",
+      "school": "Udacity",
+      "dates": "February 2016",
+      "url": "http://www.udacity.com"
+    }
+  ]
+  }
 
-var education = {};
-education.name = "Univeristy at Buffalo";
-education.dates = "September 2006 - May 2010";
-education.city = "Buffalo";
+var work = {
+  "jobs": [
+    {
+      "employer": "Liazon",
+      "title": "Quality Engineer",
+      "location": "Buffalo",
+      "dates": "February 2015 - Current",
+      "description": "I work hard and this is the description of what I do"
+    },
+    {
+      "employer": "Liazon",
+      "title": "Quality Engineer",
+      "location": "Buffalo",
+      "dates": "February 2015 - Current",
+      "description": "I work hard and this is the description of what I do"
+    },
+    {
+      "employer": "Liazon",
+      "title": "Quality Engineer",
+      "location": "Buffalo",
+      "dates": "February 2015 - Current",
+      "description": "I work hard and this is the description of what I do"
+  }
+  ]
+}
 
+var projects = {
+  "projects": [
+    {
+      "title": "JavaScript Basics",
+      "dates": "February 2016",
+      "description": "JavaScript basics utilizing jQuery to build a digital resume.",
+      "images": ["images/197x148.gif"]
+    }
 
-
-$("#main").append(bio.city);
-$("#main").append(work["title"]);
-$("#main").append(education.name);
-//Adding Name & Job title to header
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-
-
-
-//Adding to main
-//$("#main").prepend(bio);
-
-// $("#main").append(skills[0]);
-// $("#main").append(skills.length);
+  ]
+}
