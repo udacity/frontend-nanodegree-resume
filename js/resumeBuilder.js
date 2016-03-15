1e58221d8ea198c64bba3071f7b5d50886acd1c2
Jakub Czyz <jakub.jan.czyz@gmail.com>
2016-03-15 18:20:59 +0100

    Lesson 1 finished

---
+++ js/resumeBuilder.js
@@ -0,0 +1,89 @@
+
+
+
+
+var bio = 
+{
+	"name": "Jakub Czyz",
+	"role": "prezes",
+	"contacts" {
+		"mobile": "512072257",
+		"email": "jakub.jan.czyz@gmail.com",
+		"github": "jjczyz",
+		"twitter": "@hatsam",
+		"location": "Warsaw"
+	},
+	"picture_URL": "http://oi65.tinypic.com/11izck2.jpg",
+	"welcome_message": "No siema :D",
+	"skills": ["awesomness","cos"]
+}
+
+var work =
+{
+	jobs =
+	[
+		{	
+		"Employer" 	: "niwiemocobiega"
+		"Title"		: "Luxmed"
+		"Location" 	: "Tester"
+		"Dates" 	: "01-49-1230 do 32-14-1201"
+		},
+		{	
+		"Employer" 	: "niwiemocobiega"
+		"Title"		: "Luxmed"
+		"Location"	: "Tester"
+		"Dates" 	: "01-49-1230 do 32-14-1201"
+		};
+	]
+}
+var Education =
+{
+	Schools =
+	[
+		{
+			"Name" 	 	: "niwiemocobiega"
+			"City"	 	: "Politechnika Gowna"
+			"Degree" 	: "Engineeeeer"
+			"Dates" 	: "26262"
+			"Location"  : "Warszawa"
+			"Major" 	: "Electronics"
+		},
+		{
+			"Name" 		: "niwiemocobiega"
+			"City" 		: "Politechnika Gowna"
+			"Degree" 	: "Engineeeeer"
+			"Dates"		: "26262"
+			"Location" 	: "Warszawa"
+			"Major"		: "Electronics"
+		}
+	]
+	Online_courses =
+	[
+		{
+			"Title" 	:
+			"School" 	:
+			"Dates" 	:
+			"Url" 		:
+		}
+	]
+};
+
+var projects =
+{
+	projects =
+	[
+		{
+		"Title"
+		"Dates" 
+		"Description"
+		"Images" = 
+			[
+			"jakis_obrazek" = "tu_jakis_url"
+			]
+		}
+	]
+}
+
+
+
+
