7d23a5c1ee6144d907f26a250a12e125bd1cf269
Lee4Gitr <Lee4Gitr@gmail.com>
2016-03-24 11:53:22 -0400

    Formatted name and role added to the Resume.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+var formattedName = HTMLheaderName.replace("%data%", "Aubrey Mills");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
