c975c64db0150078601f8aa523ef7a76225ab0cf
Tiernan <tiernanc@gmail.com>
2016-04-05 15:42:31 -0700

    Make internationalization button use name from DOM

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,9 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var $name = $('#name');
+    var iName = inName($name.text()) || function(){};
+    $name.html(iName);
   });
 });
 
