0d14ca6fbbbb184c28881fb8449a3e5d32a01839
Jeehae Park <jpark2973@gmail.com>
2016-04-30 14:12:10 -0700

    Add bio JSON obj

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio = {
+  "name" : 'Jessica Park',
+  "role" : 'Software Engineer',
+  "contacts" : {
+		"mobile" : '415-602-9974',
+ 	 	"email" : 'jessjhpark@gmail.com' ,
+ 	 	"github" : 'github.com/jeehaepark',
+  	"twitter" : '@tinklejess'
+  },
+  "location" : 'San Francisco',
+  "welcomeMessage" : 'I am a mathematics graduate who is transitioning to Computer Science via Hack Reactor Core\'s software engineering accelerated program at MakerSquare, a JavaScript-focused programming school in San Francisco, CA. I focus on front-end development in JavaScript/HTML/CSS, using technologies such as Node.js and Express, libraries such as jQuery, D3, and Bootstrap, and most front-end MVC frameworks with a specialization in Angular.js. Other parts of the stack that I\'m strong include git, Postgres, MongoDB, MySQL, and tools such as Gulp, Grunt, Mocha, Chai and Karma. Other things I dabble in: statistics, social impact, full-stack development, classical music (violin)',
+  "skills" : ['JavaScript','node.js','express.js','AngularJS','PostgreSQL','MongoDB'],
+  "biopic" : 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAASbAAAAJGYzY2UxZGE3LWZiNTEtNDQyZC1iNmIyLTRhZDA0Y2Y5OTRlMg.jpg',
+  "display" : function(){}
+ };
