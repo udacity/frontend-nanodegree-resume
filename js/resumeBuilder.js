adb295c4a67eee32d2e6bc1342d6037d4f5c7c71
Marcin55100 <marcin55100@gmail.com>
2016-05-12 00:48:56 +0200

    Progres 20%
    ...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#header").append();
+ $("#main").append(["Marcin <p>"]);
+ 
+ 	var awesomeThoughts="I am Marcin";
+	console.log(awesomeThoughts)
+	var newThoughts=awesomeThoughts.replace("Marcin", "Smartin");
+ 
+ $("#main").append(newThoughts);
