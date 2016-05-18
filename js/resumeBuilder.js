d2b978d2d07ffe8a701bda734b079dd3f2738621
fishwang2016 <zhenyu2004@hotmail.com>
2016-05-18 17:32:52 +0800

    added JSON format information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,57 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 //$("#header").append("Welcome back!");
+
+var work ={};
+var projects = {};
+var bio = {};
+var education = {};
+
+work ={
+
+	"employer":"Jabil",
+	"title":"Engineer",
+	"location":"GZ",
+	"dates":"2007-2009"
+}
+
+projects ={
+
+	"title": "ML",
+	"dates":"2016",
+	"description":"This is a nano degree project i've pay a lot of work."
+}
+
+bio ={
+
+	"name": "Fish Wang",
+	"role": "male",
+
+	"welcomeMessage":"hi",
+	"contacts": {"tel":"123112313" , "email":"asdfdsa@ads.com" ,"location":"GZ"
+
+}
+
+education ={
+
+	"schools": [{
+          "name":"gdut",
+          "location":"gz",
+          "degree": "BS",
+          "majors": "Machine"
+
+          "schoolURL":"www.gdut.edu.cn",
+          ""
+
+	}]
+
+	"onlineCourses":[{
+
+		"title":"ML",
+		"school":"GT",
+		"dates":"2015",
+		"courseURL":"www.udacity.com"
+
+	}]
+
+}
