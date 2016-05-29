ba0067f2b3c17a37e9fbdfde4e23610dd9e6ad93
Junfeng142857 <tom.zhangms@outlook.com>
2016-05-29 11:17:44 +0800

    Changes on the go
    Initial load-up, half-way

--- js/ramdon-drafts.js
+++ js/ramdon-drafts.js
@@ -1,8 +1,4 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var works = {
-	"jobs":
+var work = 
 	[
 		{
 			"employer": "BLCU",
@@ -16,24 +12,23 @@ var works = {
 			"location": "Singapore",
 			"dates": "2011 - 2016"
 		}
-	]};
+	]
 
-var projects = {
-	"projects":
+var projects = 
 	[
 		{
 			"title": "Support Case",
 			"dates": "12-2013 - 05-2014",
 			"description": "Dealing with customer issues.",
-			"images":["images/fry.jpg","images/fry.jpg"]
+			["images/fry.jpg","images/fry.jpg"]
 		},
 		{
 			"title": "SMU Intranet Portal",
 			"dates": "05-2014 - 09-2014",
 			"description": "Building a SharePoint farm for SMU as the Intranet portal for staff.",
-			"images":["images/fry.jpg","images/fry.jpg"]
+			["images/fry.jpg","images/fry.jpg"]
 		}
-	]};
+	]
 
 var bio = 
 	{
@@ -44,9 +39,9 @@ var bio =
 				"mobile": "+6581555600",
 				"email": "leadermd@163.com",
 				"blog": "www.junfengzhang.com"
-		},
+		}
 		"skills": ["public speaking", "Networking", "SharePoint", "SQL Server", "PowerShell"]
-	};
+	}
 
 var education = { 
 		schools:
@@ -54,7 +49,7 @@ var education = {
 			"name": "BLCU",
 			"location":"BJ",
 			"degree": "Bachelor",
-			"major": ["linguistics"],
+			"major": ["linguistics"]
 			"dates": "09-2008 - 06-2012",
 			"url": "www.blcu.edu.cn"
 		},
@@ -62,7 +57,7 @@ var education = {
 			"name": "Ole Miss",
 			"location":"Oxform",
 			"degree": "IEP",
-			"major": ["linguistics"],
+			"major": ["linguistics"]
 			"dates": "09-2009 - 01-2010",
 			"url": "www.olemiss.edu"		
 		}
@@ -82,31 +77,4 @@ var education = {
 		"url": "www.blcu.edu.cn"
 		}
 		]
-	};
-
-
-
-var skills = bio.skills;
-var formtedSkill1 = HTMLskills.replace("%data%", skills[0]);
-var formtedSkill2 = HTMLskills.replace("%data%", skills[1]);
-var formtedSkill3 = HTMLskills.replace("%data%", skills[2]);
-var formtedSkill4 = HTMLskills.replace("%data%", skills[3]);
-var formatedHeader = HTMLheaderName.replace("%data%", "HeaderHeader");
-
-if (skills.length !== 0) {
-	$("#header").append(formatedHeader);
-	$("#header").append(HTMLskillsStart);
-	$("#skills").append(formtedSkill1);
-	$("#skills").append(formtedSkill2);
-	$("#skills").append(formtedSkill3);
-	$("#skills").append(formtedSkill4)
-};
-
-
-for (work in works.jobs) {
-	$("#workExperience").append(HTMLworkStart);
-	var formatedEmployer = HTMLworkEmployer.replace("%data%", works.jobs[work]["employer"]);
-	var formatedWorkTitle = HTMLworkTitle.replace("%data%", works.jobs[work]["title"]);
-	$(".work-entry:last").append(formatedEmployer + formatedWorkTitle);
-};
-
+	}
