adcf01f6ae80f8517c58fa8d6795c2f3b438bba6
ronen-e <alter.ronen@gmail.com>
2016-06-10 02:18:12 +0300

    Get data from json file disabled caching on page refactor

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,28 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var model, octopus, view;
+function main() {
+     $.getJSON('./cv.json')
+     .done(initialize)
+     .fail(function() { console.log('fail', arguments); })
+     .always(function() { console.log('always', arguments); });
+ }
+
+ function initialize(data) {
+     console.log('data', data);
+     model = data;
+     var bio = model.bio;
+     var $div = $('<div>');
+     $div.append(
+         [
+         'name: ' + bio.name,
+         'role: ' + bio.role,
+         'email: ' + bio.contacts.email,
+         'skills: ' + bio.skills,
+         'Welcome' + bio.welcomeMsg,
+         'Github: ' + bio.contacts.github].join('<br>')
+     );
+     $div.append('<img src="' + bio.biopic + '"  />');
+     $('body').append($div);
+    //  octopus.init();
+ }
+
+main();
