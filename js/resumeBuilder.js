6e0c50f9f83aa404b66407976690ac219e6d2c88
Wolfever <jgg.555@163.com>
2016-06-12 16:15:06 +0800

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,4 +3,5 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  var firstName = "Wolf" ;
  var age = 32 ;
- console.log(firstName);
+ var awesomeThoughts = "I am Wolf and I have awesome thoughts! "
+ console.log(awesomeThoughts);
