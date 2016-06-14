7c365d636b920259c32169f64de0ee495e1c9852
Matthew Perkins <matthew@yieldpop.com>
2016-06-14 10:34:18 +0100

    it's all good - second commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,17 +13,113 @@ This is empty on purpose! Your code to build the resume will go here.
 
  // $("#main").append(funThoughts);
 
-var name = "Matthew Perkins";
-var role = "Entrepreneur Bad Ass Dingy"
+var skills = ["skiing", "climbing", "coding"]
 
-var formattedName = HTMLheaderName.replace("%data%", name);
+var bio = {
+	name: "Matthew Perkins",
+	role: "Entrepreneur bad boy",
+	mobile: "07391 449 184",
+	email: "mfperkins@gmail.com",
+	pictureURL: "http://i.imgur.com/TkwsKIw.jpg",
+	welcomeMessage: "Welcome to my incrediballs resume website", 
+	skills: skills
+}
 
-$("#header").append(formattedName);
+var work = new Object();
+work.start = 2012;
+work.position = "Group Product Manager";
+work.employer = "The Climate Corp";
+work.years = 4;
+work.city = "San Francisco";
+work.description = "Amazing worthless job"
 
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var education = {
+	"schools": [
+		{
+		"name": "Harvard Business School",
+		"years": 2,
+		"city": "Boston"
+		},
+	{
+		"name": "University of Durham",
+		"years": 4,
+		"city": "Durham"
+		}
+	],
+	"onlineCourses": [
+	{
+		"name": "Udacity",
+		"course": "Intro to JavaScript"
+	},
+	{
+		"name": "CodeAcademy",
+		"course": "JavaScript for Beginners"
+	}
+	]
+}
 
-$("#header").append(formattedRole);
+var projects = {
+	"Climate": [
+		{
+			"name": "weather forecasting",
+			"year": 2014
+		},
+		{	"name": "overview",
+			"year": 2015
+		}],
+	"Syngenta": [
+		{
+			"name": "R&D strategy",
+			"year": 2010
+		},
+		{	"name": "ICS Veggies",
+			"year": 2011
+		}]
+	}
+	
+	
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// $("#header").append(formattedName);
 
-var skills = ["awesomeness", "programming", "teaching", "JS"]
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// $("#header").append(formattedRole);
 
-console.log(skills.length);
+// var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+// $("#footerContacts").prepend(formattedMobile);
+
+// var formattedEmail = HTMLemail.replace("%data%", bio.email);
+// $("#footerContacts").prepend(formattedEmail);
+
+// // var formattedPicture = HTMLbioPic.replace("%data%", bio.pictureURL)
+// // $("#header").append(formattedPicture);
+
+// var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage)
+// $("#header").append(formattedWelcome);
+
+// var formattedSkills = HTMLskillsStart.replace("%data%", bio.skills)
+// $("#education").append(formattedSkills);
+
+// var formattedWorkStart = HTMLworkStart.replace("%data%", work.start);
+// var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+// var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.position);
+// var formattedWorkDates = HTMLworkDates.replace("%data%", work.years);
+// var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.city);
+// var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.description);
+
+// $("#workExperience").append(formattedWorkEmployer);
+// $("#workExperience").append(formattedWorkStart);
+// $("#workExperience").append(formattedWorkTitle);
+// $("#workExperience").append(formattedWorkDates);
+// $("#workExperience").append(formattedWorkLocation);
+// $("#workExperience").append(formattedWorkDescription);
+	
+// var formattedSchoolName = HTMLschoolName.replace("%data%", education.name);
+// var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.city)
+
+// $("#education").append(formattedSchoolName);
+// $("#education").append(formattedSchoolLocation);
+
+
+
+
+
