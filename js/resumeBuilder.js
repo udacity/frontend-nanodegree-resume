e4e537afc8d2fef21b66a53820193750666129f1
aaa <aaa@bbb.com>
2016-07-19 11:40:05 +0800

    Create work, projects, bio, education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,88 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var work = {
+   "jobs": [
+     {
+       "employer": "",
+     "title": "",
+     "location": "",
+     "dates": "",
+     "description": ""
+     },
+     {
+       "employer": "",
+       "title": "",
+       "location": "",
+       "dates": "",
+       "description": ""
+     }
+   ]
+ }
+
+ var projects = {
+   "projects": [
+     {
+       "title": "",
+       "dates": "",
+       "description": "",
+       "images": []
+     },
+     {
+       "title": "",
+       "dates": "",
+       "description": "",
+       "images": []
+     }
+   ]
+ }
+
+ var bio = {
+   "name": "",
+   "role": "",
+   "welcomeMessage": "",
+   "biopic": "",
+   "contacts": {
+     "mobile": "",
+     "email": "",
+     "github": "",
+     "twitter": "",
+     "location": ""
+   },
+   "skills": []
+ }
+
+ var education = {
+   "schools": [
+     {
+       "name": "",
+       "location": "",
+       "degree": "",
+       "dates": "",
+       "url": "",
+       "majors": []
+     },
+     {
+       "name": "",
+       "location": "",
+       "degree": "",
+       "dates": "",
+       "url": "",
+       "majors": []
+     }
+   ],
+   "onlineCourses": [
+     {
+       "title": "",
+       "school": "",
+       "dates": "",
+       "url": ""
+     },
+     {
+       "title": "",
+       "school": "",
+       "dates": "",
+       "url": ""
+     }
+   ]
+ }
