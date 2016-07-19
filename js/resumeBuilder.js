ae173c590b0d42be9f5964383ad014329c8795f3
aaa <aaa@bbb.com>
2016-07-19 11:53:45 +0800

    Fill information in the objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,18 +4,18 @@ This is empty on purpose! Your code to build the resume will go here.
  var work = {
    "jobs": [
      {
-       "employer": "",
-     "title": "",
-     "location": "",
-     "dates": "",
-     "description": ""
+       "employer": "Giant",
+       "title": "Web Developer",
+       "location": "New York",
+       "dates": "2000",
+       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero nec nulla pulvinar elementum vel ut dolor. Donec semper volutpat malesuada. Aliquam consequat dolor sit amet iaculis sollicitudin. "
      },
      {
-       "employer": "",
-       "title": "",
-       "location": "",
-       "dates": "",
-       "description": ""
+       "employer": "Wee",
+       "title": "Frontend Developer",
+       "location": "London",
+       "dates": "2007",
+       "description": "Ut porttitor eget ex sed pellentesque. Mauris fringilla arcu at facilisis ullamcorper. Donec aliquam augue sed mattis vehicula. Aenean aliquam sapien id quam varius, ac sollicitudin turpis hendrerit."
      }
    ]
  }
@@ -23,65 +23,66 @@ This is empty on purpose! Your code to build the resume will go here.
  var projects = {
    "projects": [
      {
-       "title": "",
-       "dates": "",
-       "description": "",
+       "title": "Earth to Sky",
+       "dates": "2004",
+       "description": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
        "images": []
      },
      {
-       "title": "",
-       "dates": "",
-       "description": "",
+       "title": "Go to Mars",
+       "dates": "2030",
+       "description": "Donec in purus a risus aliquet posuere vel quis tortor. Vivamus a dolor id eros molestie vehicula. Vivamus imperdiet, leo a tempor sagittis, libero mauris imperdiet ante, quis luctus ex sem quis urna. ",
        "images": []
      }
    ]
  }
 
  var bio = {
-   "name": "",
-   "role": "",
-   "welcomeMessage": "",
-   "biopic": "",
-   "contacts": {
-     "mobile": "",
-     "email": "",
-     "github": "",
-     "twitter": "",
-     "location": ""
-   },
-   "skills": []
+   "name": "aaa",
+   "role": "Developer",
+   "welcomeMessage": "Hello Hey Hi",
+   "biopic": "Nulla eleifend odio sollicitudin iaculis dictum. Vestibulum sed magna magna. ",
+   "contacts":
+     {
+       "mobile": "402-8372-8374",
+       "email": "aaa@bbb.com",
+       "github": "aaabbb",
+       "twitter": "@girlisnoone",
+       "location": "Seven Kingdom"
+     },
+   "skills": ["Eat", "Code", "Sleep", "Draw", "Coffee"]
  }
 
  var education = {
    "schools": [
      {
-       "name": "",
-       "location": "",
-       "degree": "",
-       "dates": "",
+       "name": "lacus feugiat",
+       "location": "Nunc",
+       "degree": "quis",
+       "dates": "1999",
        "url": "",
-       "majors": []
+       "majors": ["Great", "Good", "Fine"]
      },
      {
-       "name": "",
-       "location": "",
-       "degree": "",
-       "dates": "",
+       "name": "suscipit in",
+       "location": "id sem",
+       "degree": "Vestibulum",
+       "dates": "2004",
        "url": "",
-       "majors": []
+       "majors": ["Cool", "Ok", "Ahh"]
      }
    ],
    "onlineCourses": [
      {
-       "title": "",
-       "school": "",
-       "dates": "",
+       "title": "Neque porro",
+       "school": "DangDangDang",
+       "dates": "2006",
        "url": ""
      },
      {
-       "title": "",
-       "school": "",
-       "dates": "",
+       "title": "est qui dolorem",
+       "school": "DingDingDing",
+       "dates": "2007",
        "url": ""
      }
    ]
