975d9414bc13af6724947941ad45966a04f80609
Josef Mirante <josef.mirante@gmail.com>
2016-08-16 21:01:13 -0400

    feat: Add name and role to header via helper.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+Add name and role to header
+*/
+name = "Josef Mirante"
+role = "Web Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
