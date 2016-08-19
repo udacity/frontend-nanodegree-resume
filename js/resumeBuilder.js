92dd650ec56aa05bbcf5eac59445d9da55eb936a
Josef Mirante <josef.mirante@gmail.com>
2016-08-18 20:20:46 -0400

    feat: Add skill array and add an element of it to main div

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,9 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+/*
+Create skills list and append an element to the #main div
+*/
+var skills = ["red hair", "rapier-like wit", "humbleness"];
+$("#main").append(skills[1]);
