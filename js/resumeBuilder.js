7bbbee39de6a4353268994a441f4e540d697dcbd
Josef Mirante <josef.mirante@gmail.com>
2016-08-19 22:04:33 -0400

    feat: Comment out prior code for displaying data in page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -40,58 +40,61 @@ var education = {
 	]
 };
 
-// Append name and roll information to header.
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// below is prior work for displaying data in webpage, commenting out
+// for now.
 
-// Append contact information to top contacts
-var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+// // Append name and roll information to header.
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
 
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedGithub);
-$("#topContacts").append(formattedLocation);
+// // Append contact information to top contacts
+// var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+// var formattedEmail = HTMLemail.replace("%data%", bio.email);
+// var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+// var formattedLocation = HTMLlocation.replace("%data%", bio.location);
 
-// Append bio image.
-var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
+// $("#topContacts").append(formattedMobile);
+// $("#topContacts").append(formattedEmail);
+// $("#topContacts").append(formattedGithub);
+// $("#topContacts").append(formattedLocation);
 
-$("#header").append(formattedPictureURL);
+// // Append bio image.
+// var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
 
-// Append welcome message.
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+// $("#header").append(formattedPictureURL);
 
-$("#header").append(formattedWelcomeMsg);
+// // Append welcome message.
+// var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 
-// Append skills title.
-$("#header").append(HTMLskillsStart);
+// $("#header").append(formattedWelcomeMsg);
 
-// Append skills.
-var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-$("#skills").append(formattedSkill);
-var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-$("#skills").append(formattedSkill);
-var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-$("#skills").append(formattedSkill);
+// // Append skills title.
+// $("#header").append(HTMLskillsStart);
 
-// Append work history
-var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
-var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.title);
-var formattedWorkDates = HTMLworkDates.replace("%data%", work.dates);
-var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.location);
-$("#workExperience").append(HTMLworkStart);
-$(".work-entry").append(formattedEmployer);
-$(".work-entry").append(formattedWorkTitle);
-$(".work-entry").append(formattedWorkDates);
-$(".work-entry").append(formattedWorkLocation);
+// // Append skills.
+// var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+// $("#skills").append(formattedSkill);
+// var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+// $("#skills").append(formattedSkill);
+// var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+// $("#skills").append(formattedSkill);
 
-// Append education history
-var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[0].name);
-var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[0].city);
-$("#education").append(HTMLschoolStart);
-$(".education-entry").append(formattedSchoolName);
-$(".education-entry").append(formattedSchoolLocation);
+// // Append work history
+// var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+// var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.title);
+// var formattedWorkDates = HTMLworkDates.replace("%data%", work.dates);
+// var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.location);
+// $("#workExperience").append(HTMLworkStart);
+// $(".work-entry").append(formattedEmployer);
+// $(".work-entry").append(formattedWorkTitle);
+// $(".work-entry").append(formattedWorkDates);
+// $(".work-entry").append(formattedWorkLocation);
+
+// // Append education history
+// var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[0].name);
+// var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[0].city);
+// $("#education").append(HTMLschoolStart);
+// $(".education-entry").append(formattedSchoolName);
+// $(".education-entry").append(formattedSchoolLocation);
