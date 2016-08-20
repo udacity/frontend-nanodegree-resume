0d36daea86414ca34ce1e8276de0b7f09e9dd646
Josef Mirante <josef.mirante@gmail.com>
2016-08-19 22:21:30 -0400

    feat: Update object data scheme

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,43 +1,72 @@
-// Create bio object to contain biographical info.
-var bio = {
-	"name" : "Josef Mirante",
-	"role" : "Web Developer",
-	"mobile" : "(360) 540-0952",
-	"email" : "josef.mirante@gmail.com",
-	"github" : "josef.mirante",
-	"twitter" : "",
-	"location" : "Philadelphia",
-	"pictureURL" : "images/fry.jpg",
-	"welcomeMsg" : "Great news everybody!",
-	"skills" : ["red hair", "rapier-like wit", "humbleness"]
+// work data object
+var work = {
+	"jobs": [{
+		"employer": "Mercer Consulting",
+		"title": "Implementation Services Team Leader",
+		"location": "Philadelphia, PA",
+		"dates": "2014 - 2016",
+		"description": "team lead for pension software implementation"
+	}, {
+		"employer": "Towers Watson",
+		"title": "Calculation Developer",
+		"location": "Philadelphia, PA",
+		"dates": "2011 - 2014",
+		"description": "Implemented pension software code"
+	}]
 };
 
-// Create work object to hold work history.
-var work = {
-	"title" : "Implementation Services Team Leader",
-	"employer" : "Mercer Consulting",
-	"dates" : "2014 - 2016",
-	"workLocation" : "Philadelphia, PA"
+// projects data object
+var projects = {
+	"projects": [{
+		"title": "title 1",
+		"dates": "dates 1",
+		"description": "description 1",
+		"images": [
+			"image url 1", "image url 2"
+		]
+	}, {
+		"title": "title 2",
+		"dates": "dates 2",
+		"description": "description 2",
+		"images": [
+			"image url 3", "image url 4"
+		]
+	}]
 };
+
+// Create bio object to contain biographical info.
+var bio = {
+	"name": "Josef Mirante",
+	"role": "Web Developer",
+	"welcomeMessage": "Great news everybody!",
+	"bioPic": "images/fry.jpg",
+	"contacts": {
+		"mobile": "(360) 540 0952",
+		"email": "josef.mirante@gmail.com",
+		"github": "josef.mirante",
+		"twitter": "nobody",
+		"location": "Philadelphia"
+	},
+	"skills": ["red hair", "rapier-like wit", "humbleness"]
+};
+
+
 
 // Create education object.
 var education = {
-	"schools" : [
-	{
-		"name" : "University of Washington",
-		"city" : "Seattle, WA",
-		"degree" : "BS",
-		"major" : ["Mathematics"],
-		"graduationYear" : "2008"
-	},
-	{
-		"name" : "Everett Community College",
-		"city" : "Everett, WA",
-		"degree" : "AA",
-		"major" : ["General Studies"],
-		"graduationYear" : "2006"
-	}
-	]
+	"schools": [{
+		"name": "University of Washington",
+		"location": "Seattle, WA",
+		"degree": "BS",
+		"dates": "2006 - 2008",
+		"majors": ["Mathematics"]
+	}, {
+		"name": "Everett Community College",
+		"location": "Everett, WA",
+		"degree": "AA",
+		"dates": "2004 - 2006",
+		"majors": ["Math and Science Emphasis"]
+	}]
 };
 
 // below is prior work for displaying data in webpage, commenting out
