8adbeac7fd44c5f8214340995ac0f01a5d0fe3f9
Androsh <apanasiuk00@gmail.com>
2016-09-20 23:25:10 +0100

    Data types-Array manipulation, .replace, arrays, variables

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Andy Panasiuk";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["HTML", "CSS", "JavaScript", "jQuery"]
+
+$(main).append(skills.length);
+
+
+
+
+
