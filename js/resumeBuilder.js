a3504363eb17e822b5a8b4086f51cb613abdec50
Androsh <apanasiuk00@gmail.com>
2016-09-23 14:15:04 +0100

    All changes up to date in resumebuilder + pic

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,68 @@
-var name = "Andy Panasiuk";
-var formattedName = HTMLheaderName.replace("%data%", name);
 
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+// var awesomeThoughts = "I am Andy and I am AWESOME!"
+// var funThoughts =
+//     awesomeThoughts.replace("AWESOME", "FUN");
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// console.log(awesomeThoughts);
 
-var skills = ["HTML", "CSS", "JavaScript", "jQuery"]
+// $("#main").append(funThoughts);
 
-$(main).append(skills.length);
+// var email = "zacknafain@gmail.com";
+// var newEmail =
+//     email.replace("zacknafain", "apanasiuk00");
+
+// console.log(email);
+// console.log(newEmail);
+
+// var name = "Andy Panasiuk";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var role = "Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+// var skills =
+//     ["HTML", "CSS", "JS", "jQuery"]
+
+// $("#main").prepend(skills.length);
 
 
+// var bio = {
+//   "name": "Andy Panasiuk",
+//   "role": "Web Developer",
+//   "contacts": {
+//     "mobile": "07507111029",
+//     "email": "apanasiuk@gmail.com",
+//     "github": "Androsh",
+//     "twitter": "@apanasiuk",
+//     "location": "London"
+//   },
+//   "welcomeMessage": "Hello World!",
+//   "skills": [
+//     "HTML", "CSS", "JavaScript", "jQuery"
+//   ],
+//   "bioPic": "images/logo.jpg"
 
+// };
 
+// $(main).append(bio.bioPic);
 
+// bio.city = "London";
+// bio.email = "zacknafain@gmail.com";
+
+// $(main).append(bio.city);
+
+  var work = {}
+  work.position = "ATP";
+  work.employer = "NHS";
+  work.years = 4;
+
+  var education = {}
+  education["name"] = "WSHGiT";
+  education["years"] = "2009-2012";
+  education["city"] = "Warsaw, Poland";
+
+  $("#main").append(work["position"]);
+  $("#main").append(education.name);
