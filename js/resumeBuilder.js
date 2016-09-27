db5e8f112d90a1f86399f63fd79fd60d43da9ece
Androsh <apanasiuk00@gmail.com>
2016-09-27 17:57:30 +0100

    Project pic + 4 objects in resumeBuilder.js (work, projects, bio, education)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -54,15 +54,77 @@
 
 // $(main).append(bio.city);
 
-  var work = {}
-  work.position = "ATP";
-  work.employer = "NHS";
-  work.years = 4;
+var work = {
+  "jobs": [
+    {
+      "employer": "Flaming & Co",
+      "title": "Head Bartender",
+      "location": "Warsaw, Poland",
+      "dates": "2009 - 2011",
+      "description": "Creating Cocktail menu, seasonal and occasional. Supervising bartenders and waiters, maintaining and ordering stock. Preparing orders for waiters, hot, cold drinks, wines and cocktails. "
+    },
+    {
+      "employer": "St. Georges Hospital",
+      "title": "Assistant Theatre Practitioner",
+      "location": "London, UK",
+      "dates": "2011 - 2016",
+      "description": "Assisting versatile medical team during surgical procedures in fastpaced emergency theatres. Preparing and maintaining medical equipment and stock in theatres"
+    }
+  ]
+};
 
-  var education = {}
-  education["name"] = "WSHGiT";
-  education["years"] = "2009-2012";
-  education["city"] = "Warsaw, Poland";
+var projects = {
+  "projects": [
+    "title": "Portfolio",
+    "dates": 2016,
+    "description": "Responsive, mobile first portfolio website using Bootstrap, CSS and jQuery",
+    "images": ["images/portfolio.jpg"]
+    ]
+};
 
-  $("#main").append(work["position"]);
-  $("#main").append(education.name);
+var bio = {
+  "name": "Andy Panasiuk",
+  "role": "Web Developer",
+  "welcomeMessage": "Welcome!",
+  "bioPic": "images/logo.jpg",
+  "contacts": {
+    "mobile": "07507111029",
+    "email": "apanasiuk@gmail.com",
+    "github": "Androsh",
+    "twitter": "@apanasiuk",
+    "location": "London"
+  },
+  "skills": ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery"]
+};
+
+
+
+var education = {
+  "schools": [
+    {
+      "name": "Secondary School nr 1",
+      "location": "Mlawa",
+      "degree": "A-Lvl",
+      "dates": "2002-2005",
+      "url": "http://lo1mlawa.pl/",
+      "majors": ["Math", "English", "History of Art", "Geography"]
+    },
+    {
+      "name": "Massage and Rehabilitation",
+      "location": "Ciechanow",
+      "degree": "Associate's Degree",
+      "dates": "2005-2007",
+      "url": "http://www.mszciechanow.pl/",
+      "major": ["Massage", "Rehabilitation"]
+    }
+
+  ]
+  "online courses": [
+    {
+      "title": "FEND",
+      "school": "Udacity",
+      "dates": "2016",
+      "url": "http://udacity.com"
+    }
+  ]
+};
