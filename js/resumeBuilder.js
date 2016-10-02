1177ce5166fd5882fe1872e89420c0ef0cd5cda6
Androsh <apanasiuk00@gmail.com>
2016-10-02 18:03:41 +0100

    all changes so far on lenovo

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,11 +19,13 @@ var work = {
 
 var projects = {
   "projects": [
-    "title": "Portfolio",
-    "dates": "2016",
-    "description": "Responsive, mobile first portfolio website using Bootstrap, CSS and jQuery",
-    "images": ["images/portfolio.jpg"]
-    ]
+    {
+      "title": "Portfolio",
+      "dates": "2016",
+      "description": "Responsive, mobile first portfolio website using Bootstrap, CSS and jQuery",
+      "images": ["images/portfolio.jpg"]
+    }
+  ]
 }
 
 var bio = {
@@ -40,8 +42,6 @@ var bio = {
   },
   "skills": ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery"]
 }
-
-
 
 var education = {
   "schools": [
@@ -61,9 +61,8 @@ var education = {
       "url": "http://www.mszciechanow.pl/",
       "major": ["Massage", "Rehabilitation"]
     }
-
-  ]
-  "online courses": [
+  ],
+  "onlineCourses": [
     {
       "title": "FEND",
       "school": "Udacity",
@@ -72,3 +71,84 @@ var education = {
     }
   ]
 }
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedName);
+
+
+if(bio.skills.length > 0) {
+
+  $("#header").append(HTMLskillsStart);
+
+  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+  $("#skills").append(formattedSkill);
+};
+
+// for(var i=0; i<9; i++){console.log(i);}
+
+function displayWork() {
+  for(job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
+};
+
+displayWork();
+
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x,y);
+  });
+
+
+function inName(name) {
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+  return name[0] +" "+name[1];
+}
+$("#main").append(internationalizeButton);
+
+projects.display = function() {
+  for (project in projects.projects) {
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedTitle);
+
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedDescription);
+
+    if (projects.projects[project].images.length > 0) {
+      for (image in projects.projects[project].images) {
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+      $(".project-entry:last").append(formattedImage);
+      }
+    }
+  }
+}
+
