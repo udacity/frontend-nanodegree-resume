da83f14dfa752ef5c859ff6019dd548b31c7cd5e
mafavigo <fabricio.vivas@gmail.com>
2016-10-25 21:12:46 -0400

    first changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Mario Vivas";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Senior Software Engineer";
+var formatterRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formatterRole);
+
+var firstArray = ["UNO","DOS","TRES"];
+
+console.log(firstArray.length);
+
+var numArray = [1,2,3];
+
+console.log(numArray[numArray.length-1]+1);
