b8a0c20389e7e8f43aaddf9bb72af92e2a9e7832
VivasM <fabricio.vivas@gmail.com>
2016-11-04 16:51:28 -0400

    learning objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var skills = ["oracle","pl/sql","c#","java"];
+//Objects - example
+var bio = {};
+bio = {name : "Mario"
+, lastname : "Vivas"
+, age : 44
+, role : "Software Engineer"
+, phone : "555-555-555"
+, skill : skills
+};
+
 var name = "Mario Vivas";
-var formattedName = HTMLheaderName.replace("%data%", name);
+//var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedName = HTMLheaderName.replace("%data%", bio.name + " " + bio.lastname);
 
 var role = "Senior Software Engineer";
 var formatterRole = HTMLheaderRole.replace("%data%", role);
