e540f157dfca37199badfeabdaeadfc81ae197fd
larsfeste <lars.feste@mavenwave.com>
2016-11-28 15:02:56 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("lars feste");
 
-var = awesomeThoughts 
-awesomeThoughts = "I am Lars and I am AWESOME!" 
+var awesomeThoughts = "I am Lars and I am AWESOME!";
 console.log(awesomeThoughts);
+
