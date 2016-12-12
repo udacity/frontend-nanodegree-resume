25b0f04c8dee668b9d5b77d0eeca4fadd1058827
Michael <michael-burkhart@gmx.com>
2016-12-12 19:31:18 +0100

    Revert "Revert "remove line""
    This reverts commit f704fdc8eb571b5190311c96cc667f29ad1b7c56.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append("Michael");
+
+// var awesomeThoughts = "I am Michael and I am awesome!";
+
+// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+// console.log(awesomeThoughts);
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+var name = "Michael";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Student";
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
