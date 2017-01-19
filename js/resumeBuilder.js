51bc37992effec56e8d07cfda6beb0c7ef21a9ee
Blackpotch <oodlypub@bigpond.com>
2017-01-19 21:35:36 +1030

    Complete upto data types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-$("#main").append("Black Maggot");
+var name = "Black Maggot";
+var role = "Maggot Identification Provider";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+console.log (name);
