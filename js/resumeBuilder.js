14d4d0db9cd69e02ba9817987f91ddfa0f3e7c25
sca <c.a.sameera@gmail.com>
2017-01-25 01:06:22 +0530

    mixing replace & append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Sameera C.";
+var role = "Frontend Developer"
+
+var formattedName = HTMLheaderName.replace ("%data%", name);
+var formattedRole = HTMLheaderRole.replace ("%data%", role);
+
+$('#header').prepend (formattedName);
+$('#header').append (formattedRole);
