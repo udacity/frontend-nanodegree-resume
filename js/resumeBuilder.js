f365251dcb07102e37401bf4f3a4dcc8c81a16d4
sca <c.a.sameera@gmail.com>
2017-02-11 15:57:48 +0530

    skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,35 @@ var role = "Frontend Developer"
 
 var formattedName = HTMLheaderName.replace ("%data%", name);
 var formattedRole = HTMLheaderRole.replace ("%data%", role);
+var formattedSkills = null;
+
 
 $('#header').prepend (formattedRole);
 $('#header').prepend (formattedName);
+
+
+var skills = ["Programing", "Designing", "JS", "ReactJS"];
+
+
+
+var bio = {
+	"bioPicture": "images/fry.jpg",
+	"name": formattedName,
+	"role": formattedRole,
+	"skills": skills,
+	"contacts": {
+		"mobile": '123-456-7890',
+		"email": 'c.a.sameera@gmail.com'
+	},
+	"welcomeMessage": "Hey, this is my welcome message"
+}
+
+if (bio.skills.length > 0) {
+	$("#header").append (HTMLskillsStart);
+	skills.forEach (function (item) {
+		formattedSkills = formattedSkills + ' ' + HTMLskillsStart.replace ("%data%", item);
+	});
+
+	$('#skills').append (formattedSkills);
+
+}
