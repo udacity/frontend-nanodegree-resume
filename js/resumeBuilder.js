c4a7abbe614b214341b401d4d1bc4bd33809d655
Stephani McGrath <stephanimcgrath@Stephanis-MacBook-Pro-2.local>
2017-02-22 09:50:02 -0800

    changes made from review

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,6 @@
 var firstName = "Stephani";
 var fullName = "Stephani McGrath";
-var age = 39
-var jobTitle = "Software Developer";
-console.log (firstName);
+var jobTitle = " Web Developer";
 var fullName = HTMLheaderName.replace("%data%", fullName);
 $("#header").append(fullName);
 var role = HTMLheaderRole.replace("%data%", jobTitle);
