f455ed610b6e84560e9d35b11a05f96fecf462c8
rohan <jada99x@gmail.com>
2017-03-28 09:28:24 -0400

    bio object created with name and role displaying on page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,30 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+    "name": "John Doe",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "917-555-5555",
+        "email": "johndoe@example.com",
+        "github": "johndoe",
+        "twitter": "@johndoe",
+        "location": "New York"
+    },
+    "welcomeMessage": "Welcome to my online resume.",
+    "skills": [
+        "HTML", "CSS", "Javascript", "jQuery"
+    ],
+    "biopic": "images/me",
+    display: function () {
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").append(formattedName);
+
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $(".header:last").append(formattedRole);
+
+    }
+};
+
+bio.display();
