2d71032cbf977532a645b235cbf6f0f0b5bd0144
rohan <jada99x@gmail.com>
2017-03-29 10:25:25 -0400

    placeholder data filled in

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,20 +3,20 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 var bio = {
-    "name": "John Doe",
-    "role": "Web Developer",
+    "name": "Rohan Rose",
+    "role": "Front-End Web Developer",
     "contacts": {
-        "mobile": "917-555-5555",
-        "email": "johndoe@example.com",
-        "github": "johndoe",
-        "twitter": "@johndoe",
+        "mobile": "555-555-5555",
+        "email": "rrose3@live.com",
+        "github": "rrose3",
+        "twitter": "@rrose319",
         "location": "New York"
     },
     "welcomeMessage": "Welcome to my online resume.",
     "skills": [
         "HTML", "CSS", "Javascript", "jQuery"
     ],
-    "biopic": "images/fry.jpg",
+    "biopic": "images/fry.jpg", //ToDO: replace image
     display: function () {
         var formattedName = HTMLheaderName.replace("%data%", bio.name);
         var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -50,19 +50,19 @@ var bio = {
 var education = {
     "schools": [
         {
-            "name": "test",
-            "location": "test1",
-            "degree": "2",
-            "majors": "3",
-            "dates": "xxxx-xxxx"
+            "name": "TCI College of Technology",
+            "location": "New York, New York",
+            "degree": "Associate",
+            "majors": "Networking",
+            "dates": "2008-2010"
         }
     ],
     "onlineCourses": [
         {
-            "title": "ph",
-            "school": "ph",
-            "dates": "xxxx-xxxx",
-            "url": "http://www.example.com"
+            "title": "Front-End Web Developer",
+            "school": "Udacity",
+            "dates": "2017",
+            "url": "http://www.udacity.com"
         }
     ],
     display: function () {
@@ -107,11 +107,11 @@ var education = {
 var work = {
     "jobs": [
         {
-            "employer": "placeholder",
-            "title": "placeholder",
-            "location": "placeholder",
-            "dates": "xxxx-xxxx",
-            "description": "placeholder"
+            "employer": "Chefs' Warehouse",
+            "title": "Inventory Control",
+            "location": "Bronx, New York",
+            "dates": "2012-Present",
+            "description": "Performs daily cycle count of warehouse inventory. Completes daily reports which include but not limited to, Short Product Report, Expiration Date Report, Large Adjustment Report, Negative Inventory Report, Negative Weight Report and any inventory related issues that arise."
         }
     ],
     display: function () {
@@ -138,8 +138,8 @@ var work = {
 var projects = {
     "projects": [
         {
-            "title": "Sample Project 1",
-            "dates": "xxxx-xxxx",
+            "title": "Build a Portfolio Site",
+            "dates": "2017",
             "description": "in progress",
             "images": [
                 "images/197x148.gif",
