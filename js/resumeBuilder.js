606198d902eab4c466c45b988fc1efc4d34b762c
Damian Galeano <dj.damian.b@hotmail.com>
2017-04-25 17:21:31 -0500

    Create header and add skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,54 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Damian Galeano",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "650-555-5555",
+        "email": "dj.damian.b@hotmail.com",
+        "github": "DamianGaleano9",
+        "twitter": "@Damian9",
+        "location": "Medellin"
+    },
+    "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
+    "skills": [
+        "HTML", "CSS", "DJ"
+    ],
+    "bioPic": "images/fry.jpg"
+};
+
+var name = bio.name;
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = bio.role;
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var mobile = bio.contacts.mobile;
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+$("#topContacts").append(formattedMobile);
+
+var email = bio.contacts.email;
+var formattedEmail = HTMLemail.replace("%data%", email);
+$("#topContacts").append(formattedEmail);
+
+var github = bio.contacts.github;
+var formattedGithub = HTMLgithub.replace("%data%", github);
+$("#topContacts").append(formattedGithub);
+
+var _location = bio.contacts.location;
+var formattedLocation = HTMLlocation.replace("%data%", _location);
+$("#topContacts").append(formattedLocation);
+
+var bioPic = bio.bioPic;
+var formattedBioPic = HTMLbioPic.replace("%data%", bioPic);
+$("#header").append(formattedBioPic);
+
+/* var welcomeMessage = bio.welcomeMessage;
+var formattedWelcomeMessage = HTMLWelcomeMsg.replace("%data%", welcomeMessage);
+$("#header").append(formattedWelcomeMessage); */
+
+var skills = bio.skills;
+var formattedSkills = HTMLskills.replace("%data%", skills);
+$("#header").append(HTMLskillsStart);
+$("#skills").append(formattedSkills);
