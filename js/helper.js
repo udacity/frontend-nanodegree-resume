298e7149e954e425b527bdeb1ca6f5a361c7788e
N8Sayer <forcelord50@gmail.com>
2017-04-27 16:21:29 -0500

    Updated base files

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="header-role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -37,22 +37,28 @@ var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<picture>' +
+                          '<source media="(min-width: 900px)"' +
+                                  'srcset="%large%">' +
+                          '<source media="(min-width: 600px)"' +
+                                  'srcset="%med%">' +
+                         '<img class="project-img" src="%small%">' +
+                       '</picture>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineClasses = '<h3 class="online-classes">Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#" target="_blank">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
@@ -62,6 +68,14 @@ var googleMap = '<div id="map"></div>';
 /*
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
+
+function inName() {
+    finalName = bio.name.split(" ");
+    finalName[0] = finalName[0].slice(0,1).toUpperCase() + finalName[0].slice(1).toLowerCase();
+    finalName[1] = finalName[1].toUpperCase();
+    return finalName[0] + " " + finalName[1];
+};
+
 $(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
@@ -87,6 +101,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  logClicks(loc.pageX,loc.pageY);
 });
 
 
@@ -239,11 +254,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,220 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    name: "Nate Sargent",
+    role: "Front-End Web Developer",
+    biopic: "images/me.jpg",
+    welcomeMessage: "FeelsGoodMan",
+    skills: ["JavaScript","Overwatch","Viola"],
+    contacts: {
+      mobile: "(913) 626-0567",
+      email: "forcelord50@gmail.com",
+      github: "N8Sayer",
+      location: "Lenexa, KS"
+    }
+};
+
+var work = {
+  jobs: [
+    {
+      employer: "Sungevity Solar",
+      title: "Senior Remote Solar Designer",
+      location: "Kansas City, MO",
+      dates: "05/2015 - 03/2017",
+      description: "Has extensive experience working in Salesforce: generating custom reports, modifying accounts, and creating custom viewpoints. " +
+                    "Wrote and helped design RSD’s workload management Google Sheet, which has over 1000 lines of code. " +
+                    "Programmed several Google sheets for multiple departments within the company to help augment gaps within existing processes to improve efficiency. " +
+                    "Worked with AutoCAD and Google SketchUp to build 2D and 3D models to assist customers with understanding their more difficult quotes."
+    },{
+      employer: "SAFC Biosciences",
+      title: "DPM Manufacturing Tech I",
+      location: "Lenexa, KS",
+      dates: "01/2014 - 05/2015",
+      description: "Was responsible for: manufacturing high quality Dry Powder Media used by major pharmaceutical companies,  " +
+                   "assembling and disassembling equipment in multiple configurations(Production/Cleaning/Drying), adhering to " +
+                   "pharmaceutical-grade manufacturing protocols(cGMP), as well as using the Agile methods of Lean and Kanban in our processes."
+    }, {
+      employer: "UPS",
+      title: "Front-Line Supervisor",
+      location: "Lenexa, KS",
+      dates: "08/2008 - 01/2014",
+      description: "Was responsible for training newly hired employees in the skills and safety knowledge needed to excel. " +
+                    "Successfully trained 7 employees who demonstrated the skills necessary to be promoted to a Front Line Supervisor position" +
+                    " in 16 months as a Training Supervisor. " +
+                    "Supervised 17 employees, managed their pay, start and end times, and was responsible for the quality of their work. " +
+                    "Was employed at UPS for 5.5 years. Spent 2008-2011 working as an hourly loader, and 2011-2014 as a supervisor."
+    }
+  ]
+};
+
+var education = {
+  schools: [
+    {
+      name: "JCCC",
+      schoolURL: "www.jccc.edu",
+      location: "Overland Park, KS",
+      degree: "EMT Certificate (State and National)",
+      dates: "2012-2013",
+      url: "http://www.jccc.edu/academics/health/emergency-medical-science/",
+      majors: "Emergency Medical Technician"
+    },{
+      name: "University of Kansas",
+      schoolURL: "www.ku.edu",
+      location: "Lawrence, KS",
+      degree: "N/A",
+      dates: "2007-2008",
+      url: "https://engr.ku.edu/",
+      majors: "Engineering-Undeclared"
+    }
+  ],
+  onlineCourses: [
+    {
+      title: "Udacity Front-End Web Developer Nanodegree",
+      school: "Udacity",
+      dates: "04/2017-Present",
+      url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },{
+      title: "PVOL203: PV System Fundamentals (Battery-Based)",
+      school: "Solar Energy International",
+      dates: "01/2017",
+      url: "https://www.solarenergy.org/courses/solar-training-pv-system-fundamentals-battery-based-online/"
+    },{
+      title: "PVOL101: Solar Electric Design and Installation (Grid-Direct)",
+      school: "Solar Energy International",
+      dates: "11/2015",
+      url: "https://www.solarenergy.org/courses/solar-training-solar-electric-design-and-installation-grid-direct-online/"
+    }
+  ]
+};
+
+var projects = {
+  projects: [
+    {
+      title: "Github",
+      images: ["images/github-400small.jpg",
+               "images/github-700med.jpg 700w",
+               "images/github-1000large.jpg 1000w"],
+      url: "https://www.github.com/N8Sayer",
+      dates: 2017,
+      description: "This is my personal Github Repo"
+    },{
+      title: "RSD Dashboard",
+      images: ["images/rsd-dashboard-400small.jpg",
+               "images/rsd-dashboard-700med.jpg 700w",
+               "images/rsd-dashboard-1000large.jpg 1000w"],
+      url: "https://docs.google.com/spreadsheets/d/1UN1_8w5Z4S-UoX7C4GitsPihCCSLXmxqDEeexdSKfGo/edit?usp=sharing",
+      dates: 2016,
+      description: ["This document managed workflow, tracked statistics, and managed daily job assignments"]
+    }
+  ]
+};
+
+bio.display = function () {
+  var formattedName = HTMLheaderName.replace("%data%",bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+  var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+  var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+  var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+  var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+  var formattedBiopic = HTMLbioPic.replace("%data%",bio.biopic);
+  var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+  $("#header").append(formattedBiopic);
+  $("#header").append(formattedWelcomeMessage);
+  $("#topContacts").append(formattedMobile);
+  $("#topContacts").append(formattedEmail);
+  $("#topContacts").append(formattedGithub);
+  $("#topContacts").append(formattedLocation);
+
+  if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+    for (var x=0;x<bio.skills.length;x++) {
+      var formattedSkill = HTMLskills.replace("%data%",bio.skills[x]);
+      $("#skills").append(formattedSkill);
+    }
+  }
+
+  $("#footerContacts").append(formattedMobile);
+  $("#footerContacts").append(formattedEmail);
+  $("#footerContacts").append(formattedGithub);
+  $("#footerContacts").append(formattedLocation);
+};
+
+work.display = function () {
+  for (var x=0;x<work.jobs.length;x++) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[x].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[x].title);
+    var formattedDates = HTMLworkDates.replace("%data%",work.jobs[x].dates);
+    var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[x].location);
+    var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[x].description);
+    $(".work-entry:last").append(formattedEmployer.concat(formattedTitle));
+    $(".work-entry:last").append(formattedDates);
+    $(".work-entry:last").append(formattedLocation);
+    $(".work-entry:last").append(formattedDescription);
+  }
+};
+
+projects.display = function() {
+  for (var x=0;x<projects.projects.length;x++) {
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[x].title).replace('#',projects.projects[x].url);
+    var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[x].dates);
+    var formattedImage = HTMLprojectImage.replace("%small%",projects.projects[x].images[0]).replace("%med%",projects.projects[x].images[1]).replace("%large%",projects.projects[x].images[2]);
+    var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[x].description);
+    $(".project-entry:last").append(formattedTitle);
+    $(".project-entry:last").append(formattedDates);
+    $(".project-entry:last").append(formattedDescription);
+    $(".project-entry:last").append(formattedImage);
+  }
+};
+
+education.display = function () {
+  for (var x=0;x<education.schools.length;x++) {
+    $("#education").append(HTMLschoolStart);
+
+    var formattedName = HTMLschoolName.replace("%data%",education.schools[x].name).replace("#",education.schools[x].url);
+    var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[x].degree);
+    var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[x].location);
+    var formattedDates = HTMLschoolDates.replace("%data%",education.schools[x].dates);
+    var tempMajors = "";
+    for (var y=0;y<education.schools[x].majors[y].length;y++) {
+      if (y < education.schools[x].majors[y].length -1) {
+        tempMajors += education.schools[x].majors + ", ";
+      }
+      else {
+        tempMajors += education.schools[x].majors;
+      }
+    }
+    var formattedMajors = HTMLschoolMajor.replace("%data%",tempMajors);
+
+    $(".education-entry:last").append(formattedName.concat(formattedDegree));
+    $(".education-entry:last").append(formattedLocation);
+    $(".education-entry:last").append(formattedDates);
+    $(".education-entry:last").append(formattedMajors);
+  }
+
+  $(".education-entry:last").append(HTMLonlineClasses);
+  for (var x=0;x<education.onlineCourses.length;x++) {
+    $("#education").append(HTMLschoolStart);
+
+    var formattedTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[x].title).replace("#",education.onlineCourses[x].url);
+    var formattedSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[x].school);
+    var formattedDates = HTMLonlineDates.replace("%data%",education.onlineCourses[x].dates);
+
+    $(".education-entry:last").append(formattedTitle.concat(formattedSchool));
+    $(".education-entry:last").append(formattedDates);
+  }
+};
+
+bio.display();
+work.display();
+education.display();
+projects.display();
+$("#mapDiv").append(googleMap);
+//$("#main").append(internationalizeButton);
