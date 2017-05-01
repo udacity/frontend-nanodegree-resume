e1f283266c34f29f7192256de617f937b8beda81
Dyer <KDyer@mlmic.net>
2017-04-30 20:26:35 -0400

    data types edits

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts = "I am Karen and I am AWESOME!";
+ var funThoughts = awesomeThoughts.replace("AWESOME", "fun");
+ // two ways to use .replace. with the value in the function and...
+ var formattedName = HTMLheaderName.replace("%data%", "Karen Dyer");
+ // with the varialbe declared outside the function, and the variable name used instead of the value itself.
+ var role = "Web Developer"
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#main").append([funThoughts]);
+ $("#header").prepend([formattedRole]);
+ $("#header").prepend([formattedName]);
