d1f3994c07579acd48159f0ed5cf436c6278da6b
Dyer <KDyer@mlmic.com>
2017-05-07 17:29:12 -0400

    end of object literal notation section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,37 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var awesomeThoughts = "I am Karen and I am AWESOME!";
- var funThoughts = awesomeThoughts.replace("AWESOME", "fun");
- // two ways to use .replace. with the value in the function and...
  var formattedName = HTMLheaderName.replace("%data%", "Karen Dyer");
  // with the varialbe declared outside the function, and the variable name used instead of the value itself.
  var role = "Web Developer"
  var formattedRole = HTMLheaderRole.replace("%data%", role);
+ var work = {
+ 	"position":"Web Developer &nbsp;",
+ 	"employer":"MLMIC &nbsp;",
+ 	"years":"14 &nbsp;",
+ 	"city" :"Latham &nbsp;"
+ }
+ // var formattedEmail = HTMLemailEmail.replace("%data%", work.position);
+ var projects = {
+ 	"mlmic":"work project &nbsp;",
+ 	"uDacityProg":"programming 101 project &nbsp;",
+ 	"uDacityFront":"front end project &nbsp;"
+ }
+ var education = {
+ 	"school":"Marlboro &nbsp;",
+	"yearsAttended":"2 &nbsp;",
+ 	"city":"Marlboro &nbsp;"
+ }
 
- $("#main").append([funThoughts]);
  $("#header").prepend([formattedRole]);
  $("#header").prepend([formattedName]);
+ $("#main").append([work.position]);
+ $("#main").append([work.employer]);
+ $("#main").append([work.years]);
+ $("#main").append([work.city]);
+ $("#main").append([projects.mlmic]);
+ $("#main").append([projects.uDacityProg]);
+ $("#main").append([projects.uDacityFront]);
+ $("#main").append([education.school]);
+ $("#main").append([education.yearsAttended]);
+ $("#main").append([education.city]);
