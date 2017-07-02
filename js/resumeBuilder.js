3493d35152d8d9992caf01f9d60e86756deb6db8
SJ Rim <rim.seungjoon@gmail.com>
2017-07-02 16:01:28 -0700

    fixing stuff
    figuring out what i did wrong with commits/merges

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,32 +23,6 @@ var bio = {
 		"JavaScript",
 		"Bootstrap",
 		"JQuery"
-<<<<<<< HEAD
-		]
-};
-
-bio.display = function() {
-	$("#header").prepend(HTMLheaderName.replace("%data%", bio.name), HTMLheaderRole.replace("%data%", bio.role));
-	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
-	$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
-
-	formattedContactInfo = [];
-	formattedContactInfo.push(HTMLmobile.replace("%data%", bio.contacts.mobile));
-	formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
-	formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
-	formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
-
-	if (bio.skills.length > 0) {
-		$("#header").append(HTMLskillsStart);
-		for (var i in bio.skills) {
-			$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
-		}
-	}
-
-	for (i in formattedContactInfo) {
-		$("#topContacts").append(formattedContactInfo[i]);
-		$("#footerContacts").append(formattedContactInfo[i]);
-=======
 		],
 	"display": function() {
 		$("#header").prepend(HTMLheaderName.replace("%data%", bio.name), HTMLheaderRole.replace("%data%", bio.role));
@@ -72,7 +46,6 @@ bio.display = function() {
 			$("#topContacts").append(formattedContactInfo[i]);
 			$("#footerContacts").append(formattedContactInfo[i]);
 		}
->>>>>>> temp
 	}
 };
 
@@ -96,23 +69,6 @@ var work = {
 			"dates": "Summer of 2015",
 			"description": "AutoCAD Civil 3D, Land Development, Site Layout"
 		}
-<<<<<<< HEAD
-	]
-};
-
-work.display = function() {
-	$("#workExperience").append(HTMLworkStart);
-	for (var job in work.jobs) {
-		$(".work-entry").append(HTMLworkEmployer.replace("%data%", work.jobs[job].employer), HTMLworkTitle.replace("%data%", work.jobs[job].title));
-		$(".work-entry").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
-		$(".work-entry").append(HTMLworkLocation.replace("%data%", work.jobs[job].location));
-		$(".work-entry").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));
-	}
-}
-
-work.display();
-
-=======
 	],
 	"display": function() {
 		$("#workExperience").append(HTMLworkStart);
@@ -129,45 +85,10 @@ work.display();
 
 // EDUCATION -------------------------------------------------------------
 
->>>>>>> temp
 var education = {
 	"schools": [
 		{
 			"name": "Virgnia Tech",
-<<<<<<< HEAD
-			"city": "Blacksburg, VA",
-			"degree": "BA",
-			"major": ["Civil/Environmental Engineering"]
-		},
-		{
-			"name": "Seoul National University",
-			"city": "Seoul, South Korea",
-			"degree": "Study Abroad",
-			"major": ["Civil/Environmental Engineering"]
-		}
-	]
-};
-
-var projects = {
-	"projects": [
-		{
-			"title": "Zombies",
-			"dates": "Fall 2016",
-			"description": "Adventure-based game built with Python."
-		},
-		{
-			"title": "Resume 1.0",
-			"dates": "Spring 2017",
-			"description": "First web-page, my online resume."
-		},
-		{
-			"title": "Resume 2.0",
-			"dates": "Summer 2017",
-			"description": "Second web-page, second version of my online resume."
-		}
-	]
-};
-=======
 			"location": "Blacksburg, VA",
 			"degree": "Bachelor's Degree",
 			"major": ["Civil/Environmental Engineering"],
@@ -220,4 +141,3 @@ education.display();
 
 // GOOGLE MAP API ---------------------------------------------------------------------
 $("#mapDiv").append(googleMap);
->>>>>>> temp
