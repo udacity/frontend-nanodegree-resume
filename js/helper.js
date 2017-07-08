2f67db7b15d0b002531d4208f326350bd182987a
Jesus Guerrero <jesusant.guerrero@gmail.com>
2017-07-08 19:59:12 -0400

    resume page with the MVO model aplied

--- js/helper.js
+++ js/helper.js
@@ -127,13 +127,13 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(octopus.getBioInfo().contacts.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    octopus.getEducationInfo().schools.forEach(function(school){
       locations.push(school.location);
     });
 
@@ -141,7 +141,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    octopus.getWorkInfo().jobs.forEach(function(job){
       locations.push(job.location);
     });
 
