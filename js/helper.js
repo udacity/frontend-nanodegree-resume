514844044d3e7d85874bf568e1fc0b6d22eeb194
N8Sayer <forcelord50@gmail.com>
2017-07-16 10:07:19 -0500

    Modified a bit
    Needs some work

--- js/helper.js
+++ js/helper.js
@@ -16,10 +16,10 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span class="header-role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">cell</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a href="%href%"><span class="white-text">%data%</span></a></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><a href="%href%"><span class="white-text">%data%</span></a></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
@@ -40,13 +40,15 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<picture>' +
-                          '<source media="(min-width: 900px)"' +
-                                  'srcset="%large%">' +
-                          '<source media="(min-width: 600px)"' +
-                                  'srcset="%med%">' +
-                         '<img class="project-img" src="%small%">' +
-                       '</picture>';
+var HTMLprojectImage ='<a href="%href%"' +
+                        '<picture>' +
+                            '<source media="(min-width: 900px)"' +
+                                    'srcset="%large%">' +
+                            '<source media="(min-width: 600px)"' +
+                                    'srcset="%med%">' +
+                           '<img class="project-img" src="%small%">' +
+                         '</picture>' +
+                       '</a>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#" target="_blank">%data%';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,12 +5,19 @@ var bio = {
     name: "Nate Sargent",
     role: "Front-End Web Developer",
     biopic: "images/me.jpg",
-    welcomeMessage: "FeelsGoodMan",
-    skills: ["JavaScript","Overwatch","Viola"],
+    welcomeMessage: "",
+    skills: ["HTML, CSS, JavaScript (jQuery, Knockout, studying React)",
+             "",
+             "Custom builds Windows PCs, Servers, and Media Centers.",
+             "",
+             "Proficient at editing/creating audio using Audacity and Ableton Live",
+             "",
+             "CPR certified through the American Heart Association since 2004"],
     contacts: {
       mobile: "(913) 626-0567",
       email: "forcelord50@gmail.com",
       github: "N8Sayer",
+      githubLink: "https://www.github.com/N8Sayer",
       location: "Lenexa, KS"
     }
 };
@@ -22,28 +29,25 @@ var work = {
       title: "Senior Remote Solar Designer",
       location: "Kansas City, MO",
       dates: "05/2015 - 03/2017",
-      description: "Has extensive experience working in Salesforce: generating custom reports, modifying accounts, and creating custom viewpoints. " +
-                    "Wrote and helped design RSD’s workload management Google Sheet, which has over 1000 lines of code. " +
-                    "Programmed several Google sheets for multiple departments within the company to help augment gaps within existing processes to improve efficiency. " +
-                    "Worked with AutoCAD and Google SketchUp to build 2D and 3D models to assist customers with understanding their more difficult quotes."
+      description: "Wrote and designed the Remote Solar Design department’s workload management Google Sheet. Implementation of this sheet decreased time to quote by 140%. " +
+                    "Worked extensively in Salesforce generating custom reports, modifying accounts, and creating custom viewpoints. " +
+                    "Programmed Google sheets for multiple departments within the company to assist with existing processes and to increase efficiency. " +
+                    "Worked with AutoCAD and Google SketchUp to build 2D and 3D models to improve the customer's experience and help improve their understanding of the quote."
     },{
       employer: "SAFC Biosciences",
       title: "DPM Manufacturing Tech I",
       location: "Lenexa, KS",
       dates: "01/2014 - 05/2015",
-      description: "Was responsible for: manufacturing high quality Dry Powder Media used by major pharmaceutical companies,  " +
-                   "assembling and disassembling equipment in multiple configurations(Production/Cleaning/Drying), adhering to " +
-                   "pharmaceutical-grade manufacturing protocols(cGMP), as well as using the Agile methods of Lean and Kanban in our processes."
+      description: "Used the Agile methods of Lean and Kanban to manufacture Dry Powder Media, assembled and disassembled rooms of equipment, " +
+                   "and adhered to pharmaceutical-grade manufacturing protocols."
     }, {
-      employer: "UPS",
+      employer: "United Parcel Services",
       title: "Front-Line Supervisor",
       location: "Lenexa, KS",
       dates: "08/2008 - 01/2014",
-      description: "Was responsible for training newly hired employees in the skills and safety knowledge needed to excel. " +
-                    "Successfully trained 7 employees who demonstrated the skills necessary to be promoted to a Front Line Supervisor position" +
-                    " in 16 months as a Training Supervisor. " +
-                    "Supervised 17 employees, managed their pay, start and end times, and was responsible for the quality of their work. " +
-                    "Was employed at UPS for 5.5 years. Spent 2008-2011 working as an hourly loader, and 2011-2014 as a supervisor."
+      description: "Supervised 17 employees, managed their pay, start and end times, and was responsible for the quality of their work. " +
+                    "Trained 7 employees to a Supervisor level and who were promoted as a result of my training and their hard work. " +
+                    "Worked at UPS from 08/2008-06/2011 working as a laborer, and 06/2011-1/2014 as a supervisor."
     }
   ]
 };
@@ -56,7 +60,7 @@ var education = {
       degree: "EMT Certificate (State and National)",
       dates: "2012-2013",
       url: "http://www.jccc.edu/academics/health/emergency-medical-science/",
-      majors: ["Emergency Medical Technician"]
+      majors: ["Emergency Medicine"]
     },{
       name: "University of Kansas",
       location: "Lawrence, KS",
@@ -70,7 +74,7 @@ var education = {
     {
       title: "Udacity Front-End Web Developer Nanodegree",
       school: "Udacity",
-      dates: "04/2017-Present",
+      dates: "04/2017-07/2017",
       url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
     },{
       title: "PVOL203: PV System Fundamentals (Battery-Based)",
@@ -89,21 +93,13 @@ var education = {
 var projects = {
   projects: [
     {
-      title: "Github",
-      images: ["images/github-400small.jpg",
-               "images/github-700med.jpg 700w",
-               "images/github-1000large.jpg 1000w"],
-      url: "https://www.github.com/N8Sayer",
-      dates: "2017",
-      description: "This is my personal Github Repo"
-    },{
-      title: "RSD Dashboard",
+      title: "Google Apps Projects",
       images: ["images/rsd-dashboard-400small.jpg",
                "images/rsd-dashboard-700med.jpg 700w",
                "images/rsd-dashboard-1000large.jpg 1000w"],
-      url: "https://docs.google.com/spreadsheets/d/1UN1_8w5Z4S-UoX7C4GitsPihCCSLXmxqDEeexdSKfGo/edit?usp=sharing",
+      url: "https://drive.google.com/drive/folders/0BxJeGYCNyrP5MTBpdng3cS1CUVk?usp=sharing",
       dates: "2016",
-      description: ["This document managed workflow, tracked statistics, and managed daily job assignments"]
+      description: ["This is the folder where I store a copy of all of the Google Apps projects I've built. Feel free to check it out!"]
     }
   ]
 };
@@ -113,7 +109,9 @@ bio.display = function () {
   var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
   var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
   var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+  formattedEmail = formattedEmail.replace("%href%","mailto:"+bio.contacts.email);
   var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+  formattedGithub = formattedGithub.replace("%href%",bio.contacts.githubLink);
   var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
   var formattedBiopic = HTMLbioPic.replace("%data%",bio.biopic);
   var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
@@ -151,16 +149,16 @@ projects.display = function() {
 
     var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[x].title).replace('#',projects.projects[x].url);
     var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[x].dates);
-    var formattedImage = "";
+    var formattedImage = HTMLprojectImage.replace("%href%",projects.projects[x].url);
     for (var y=0;y<projects.projects[x].images.length;y++) {
       if (y === 0) {
-        formattedImage = HTMLprojectImage.replace("%small%",projects.projects[x].images[y]);
+        formattedImage = formattedImage.replace("%small%",projects.projects[x].images[y]);
       }
       else if (y === 1) {
-        formattedImage = HTMLprojectImage.replace("%med%",projects.projects[x].images[y]);
+        formattedImage = formattedImage.replace("%med%",projects.projects[x].images[y]);
       }
       else if (y === 2) {
-        formattedImage = HTMLprojectImage.replace("%large%",projects.projects[x].images[y]);
+        formattedImage = formattedImage.replace("%large%",projects.projects[x].images[y]);
       }
     }
     var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[x].description);
