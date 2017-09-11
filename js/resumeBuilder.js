1b7366c35000be3a9e94f50e9a42e37783946bfa
TobiasDi <tobifabi96@googlemail.com>
2017-09-11 15:56:43 +0200

    vcv
    cvc

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,1 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*This is empty on purpose! Your code to build the resume will go here. */var formattedName = HTMLheaderName.replace("%data%", "Tobias");var formattedRole = HTMLheaderRole.replace("%data%", "Entwickler");$("#header").append(formattedName);$("#header").prepend(formattedRole);
