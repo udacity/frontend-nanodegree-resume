1d1ea1db28e539a715165fe9f5a37942f759f1c1
kshitijgosavi <31984242+kshitijgosavi@users.noreply.github.com>
2017-09-18 08:54:07 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name= "Kshitij Gosavi";
+var role= "Web Developer"
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
