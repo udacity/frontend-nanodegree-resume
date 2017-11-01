117c1cdde9f4fc94243de2ca6e49bdea3a0c994d
8bit <lizardscruff@users.noreply.github.com>
2017-11-01 19:11:52 +0900

    Add files via upload
    feat: added name and job position

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+const name = 'Zachary Butterfield';
+const role = 'Front-End Developer';
+const awesomeThoughts = `I am ${name} and I am AWESOME!`;
+console.log(awesomeThoughts);
+
+const funThoughts = awesomeThoughts.replace(['AWESOME'], ['FUN']);
+console.log(funThoughts);
+$('#main').append(funThoughts);
+
+const formattedName = HTMLheaderName.replace('%data%', name);
+const formattedRole = HTMLheaderRole.replace('%data%', role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
