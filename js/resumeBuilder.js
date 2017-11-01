4f1c8a15f0a15f10110b0f63e3d01b4536b9050c
8bit <lizardscruff@users.noreply.github.com>
2017-11-01 20:02:01 +0900

    Add files via upload
    feat: some messing around

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,13 +5,18 @@ This is empty on purpose! Your code to build the resume will go here.
 const name = 'Zachary Butterfield';
 const role = 'Front-End Developer';
 const awesomeThoughts = `I am ${name} and I am AWESOME!`;
+const skills = ['HTML', 'JavaScript', 'Python', 'CSS', 'ES6', 'jQuery'];
 console.log(awesomeThoughts);
 
 const funThoughts = awesomeThoughts.replace(['AWESOME'], ['FUN']);
 console.log(funThoughts);
 $('#main').append(funThoughts);
+$('#main').append(skills);
 
 const formattedName = HTMLheaderName.replace('%data%', name);
 const formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+
+
+
