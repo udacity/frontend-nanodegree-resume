88a9822ce07651b1f19e25c842ef4dc714fe842e
8bit <lizardscruff@users.noreply.github.com>
2017-11-02 15:14:07 +0900

    Add files via upload
    feat: added some objects and moogle image

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,21 +2,48 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-const name = 'Zachary Butterfield';
-const role = 'Front-End Developer';
+const 	name = 'Zachary Butterfield',
+	  	role = 'Front-End Developer',
+	  	skills = ['HTML', 'JavaScript', 'Python', 'CSS', 'ES6', 'jQuery'],
+	  	bio = {
+				'name' : name,
+				'role' : role,
+				'contactInfo' : 'zbutterfield@gmail.com',
+				'bioPic' : 'images/moogle.jpg',
+				'welcomeMessage' : `Hey there! I'm ${name}. Nice to meet you!`,
+				'age' : 31,
+				'skills' : skills
+			},
+		$main = $('#main'),
+		$header = $('#header'),
+		formattedName = HTMLheaderName.replace('%data%', name),
+	  	formattedRole = HTMLheaderRole.replace('%data%', role);
+	  	//bioPic = HTMLbioPic.replace('%data%', bio.pictureURL);
+
+
+$header.prepend(formattedRole);
+$header.prepend(formattedName);
+$main.append(bio.name);
+$main.append(bio.role);
+$main.append(bio.contactInfo);
+$main.append(bio.bioPic);
+$main.append(bio.welcomeMessage);
+$main.append(bio.skills);
+
+
+/*
 const awesomeThoughts = `I am ${name} and I am AWESOME!`;
-const skills = ['HTML', 'JavaScript', 'Python', 'CSS', 'ES6', 'jQuery'];
-console.log(awesomeThoughts);
+$('#main').append(bio.name);
+	  
 
 const funThoughts = awesomeThoughts.replace(['AWESOME'], ['FUN']);
-console.log(funThoughts);
 $('#main').append(funThoughts);
 $('#main').append(skills);
 
-const formattedName = HTMLheaderName.replace('%data%', name);
-const formattedRole = HTMLheaderRole.replace('%data%', role);
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+*/
+
 
 
 
